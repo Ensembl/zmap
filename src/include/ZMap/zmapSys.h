75c85d1fb4ea66a14c4462ace6727c7344f5b682
edgrif <edgrif>
2004-03-12 15:10:39 +0000

    Move some macros into zmapUtils.h

--- src/include/ZMap/zmapSys.h
+++ src/include/ZMap/zmapSys.h
@@ -26,9 +26,9 @@
  * Description: 
  * Exported functions: See XXXXXXXXXXXXX.h
  * HISTORY:
- * Last edited: Aug  6 10:37 2003 (edgrif)
+ * Last edited: Mar 12 14:06 2004 (edgrif)
  * Created: Thu Jul 24 14:36:17 2003 (edgrif)
- * CVS info:   $Id: zmapSys.h,v 1.1 2004-03-03 12:51:52 edgrif Exp $
+ * CVS info:   $Id: zmapSys.h,v 1.2 2004-03-12 15:10:39 edgrif Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_SYS_H
@@ -37,41 +37,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <glib.h>
-
-
-/* Debugging messages, TRUE for on... */
-extern gboolean zmap_debug_G ;
-
-/* Use this one like this:   ZMAP_DEBUG(("format string" lots, of, args)) ; */
-#define ZMAP_DEBUG(ALL_ARGS_WITH_BRACKETS) \
-do { \
-     if (zmap_debug_G) \
-       printf ALL_ARGS_WITH_BRACKETS ; \
-   } while (0)
-
-
-
-/* Zmap error messages. */
-/* I'm defining this for now, I don't really want to use the whole messXXXX() thing
- * unless it gets chopped out into a file on its own.... */
-
-/* Use this one like this:   ZMAPERR("complete error text") ; */
-#define ZMAPERR(TEXT) \
-do { \
-     fprintf(stderr, "%s (line %d), error:: \"%s\"\n", \
-             __FILE__, __LINE__, TEXT) ; \
-     exit(EXIT_FAILURE) ; \
-   } while (0)
-
-
-/* Use this one like this: ZMAPSYSERR(errno_value, "complete error text") ;   */
-#define ZMAPSYSERR(CODE, TEXT) \
-do { \
-     fprintf(stderr, "%s (line %d), error:: \"%s\" (errno = \"%s\")\n", \
-             __FILE__, __LINE__, TEXT, g_strerror(CODE)) ; \
-     exit(EXIT_FAILURE) ; \
-   } while (0)
-
 
 
 /* I would like the data passed to be ZMapWinConn but this produces circularities
