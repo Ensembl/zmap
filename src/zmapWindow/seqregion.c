c0471d9b055f534bdd64be5840acc6811281a535
rnc <rnc>
2004-05-14 10:28:41 +0000

    Folded in window-splitting code: major changes to the structures in zmapcontrol.h

--- src/zmapWindow/seqregion.c
+++ src/zmapWindow/seqregion.c
@@ -1,3 +1,4 @@
+/*  Last edited: Apr 13 14:21 2004 (rnc) */
 /*  file: seqregion.c
  *  Author: Simon Kelley (srk@sanger.ac.uk)
  *  Copyright (c) Sanger Institute, 2003
@@ -24,157 +25,68 @@
  *	Simon Kelley (Sanger Institute, UK) srk@sanger.ac.uk
  */
 
-#include <wh/acedb.h>
-#include <wh/regular.h>
-#include <wh/a.h>
-#include <wh/lex.h>
-#include <wh/smap.h>
-#include <wzmap/stringbucket.h>
-#include <wzmap/seqregion.h>
+#include <seqregion.h>
 
 
+/* srInvarCoord ***************************************************/
+/* Returns a coordinate in the absolute direction. */
 
-InvarCoord srInvarCoord(SeqRegion *region, Coord coord)
+InvarCoord srInvarCoord(ZMapRegion *zMapRegion, Coord coord)
 {
-  if (region->rootIsReverse)
-    return coord - region->length + 1;
+  if (zMapRegion->rootIsReverse)
+    return coord - zMapRegion->length + 1;
   else
     return coord;
 }
 
-Coord srCoord(SeqRegion *region, InvarCoord coord)
+/* srCoord ********************************************************/
+/* Returns a coordinate in the current direction. */
+
+Coord srCoord(ZMapRegion *zMapRegion, InvarCoord coord)
 {
-   if (region->rootIsReverse)
-    return coord - region->length + 1;
+   if (zMapRegion->rootIsReverse)
+    return coord - zMapRegion->length + 1;
   else
     return coord;
 }
 
-BOOL srActivate(char *seqspec, SeqRegion *region, Coord *r1, Coord *r2)
+
+
+/* srCreate ******************************************************/
+/* Creates a new, empty ZMapRegion structure. */
+
+ZMapRegion *srCreate(STORE_HANDLE handle)
 {
-  
-  int x1, x2;
-  KEY key;
+  ZMapRegion *zMapRegion = halloc(sizeof(ZMapRegion), handle);
 
-  if (!lexClassKey(seqspec, &key) || 
-      !sMapTreeRoot(key, 1, 0, &region->rootKey, &x1, &x2))
-    return FALSE;
-  
-  region->length = sMapLength(region->rootKey);
-  
-  /* If the key is reversed wrt to the root, record that we need to 
-     make the sMap reversed, and calculate the coords that the key
-     has on the reversed sMap. */
-  
-  if (x1 > x2)
-    {
-      *r1 = region->length - x1 + 1;
-      *r2 = region->length - x2 + 1;
-      region->rootIsReverse = 1;
-    }
-  else
-    {
-      region->rootIsReverse = 0;
-      *r1 = x1;
-      *r2 = x2;
-    }
-
-  return TRUE;
+  zMapRegion->area1 = zMapRegion->area2 = 0;
+  zMapRegion->methods = NULL;
+  zMapRegion->oldMethods = NULL;
+  zMapRegion->dna = NULL;
+  return zMapRegion;
 }
 
-static void calcSeqRegion(SeqRegion *region, 
-			  Coord x1, Coord x2,
-			  BOOL isReverse)
+/* srMethodFromID ***********************************************/
+/* Retrieves a method structure from the methods array based on
+ * the ID it receives.  Knows nothing of AceDB, which is why it's
+ * here, but could as well be in zmapcalls.c with it's sisters. */
+
+srMeth *srMethodFromID(ZMapRegion *zMapRegion, methodID id)
 {
-  if (x1 < 1) x1 = 1;
-  if (x2 > region->length) x2 = region->length;
-  
-  /* Note: this currently throws everything away and starts again.
-     It's designed by capable of incrementally adding to existing structures.
-  */
-  
-  if (region->smap)
-    sMapDestroy(region->smap);
-  
-  if (region->dna)
-    arrayDestroy(region->dna);
+  int i;
+
+  if (zMapRegion && zMapRegion->methods)
+    for (i=0; i<arrayMax(zMapRegion->methods); i++)
+      {
+	srMeth *meth = arrp(zMapRegion->methods, i, srMeth);
+	if (meth->id == id)
+	  return meth;
+      }
+  return NULL;
+}
+
  
-  if (region->bucket)
-    sbDestroy(region->bucket);
-  
-  region->oldMethods = region->methods;
-  region->methods = NULL;
-  /* currently just put in new area - might need these to see what we have 
-     already got. */
-
-  
-  region->area1 = x1;
-  region->area2 = x2;
-  region->rootIsReverse = isReverse;
-  
-  region->bucket = sbCreate(region->handle);
-  if (region->rootIsReverse)
-    region->smap = sMapCreateEx(region->handle, region->rootKey, 
-				0, region->length, 
-				region->area1, region->area2, NULL);
-  else
-    region->smap = sMapCreateEx(region->handle, region->rootKey, 
-				region->length, 1, 
-				region->area1, region->area2, NULL);
-  
-  seqRegionConvert(region);
-  arrayDestroy(region->oldMethods);
-  
-}
-
-BOOL srGetDNA(SeqRegion *region)
-{
-  
-  /* TODO handle callback stuff */
-  if (!region->dna)
-    region->dna = sMapDNA(region->smap, region->handle, 0);
-  
-  return region->dna != NULL;
-}
-
-void srCalculate(SeqRegion *region, Coord x1, Coord x2)
-{
-  calcSeqRegion(region, x1, x2, region->rootIsReverse);
-}
-
-void srRevComp(SeqRegion *region)
-{
-  calcSeqRegion(region, 
-		region->length - region->area2 + 1,
-		region->length - region->area1 + 1,
-		!region->rootIsReverse);
-}
-
-SeqRegion *srCreate(STORE_HANDLE handle)
-{
-  /* Get out own handle */
-  
-  STORE_HANDLE newHandle = handleHandleCreate(handle);
-  
-  SeqRegion *region = halloc(sizeof(SeqRegion), newHandle);
-  
-  region->handle = newHandle;
-  region->smap = NULL;
-  region->dna = NULL;
-  region->bucket = NULL;
-  region->area1 = region->area2 = 0;
-  region->methods = NULL;
-  region->oldMethods = NULL;
-  region->idc = 1;
-  return region;
-}
-
-void seqRegionDestroy(SeqRegion *region)
-{
-  handleDestroy(region->handle);
-}
-
-/***********************************************************************/
+/********************** end of file **********************************/
   
 
 
