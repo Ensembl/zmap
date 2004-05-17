4b37a1f031480a7fecc91fc5ef9d70a81b518f89
edgrif <edgrif>
2004-05-17 16:22:45 +0000

    add parent widget parameter, window is now not top level widget.

--- src/include/ZMap/zmapWindow.h
+++ src/include/ZMap/zmapWindow.h
@@ -23,12 +23,13 @@
  * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk and
  *	Simon Kelley (Sanger Institute, UK) srk@sanger.ac.uk
  *
- * Description: 
- * Exported functions: See XXXXXXXXXXXXX.h
+ * Description: Defines interface to code that creates/handles a
+ *              window displaying genome data.
+ *              
  * HISTORY:
- * Last edited: Mar  3 16:23 2004 (edgrif)
+ * Last edited: May 17 16:29 2004 (edgrif)
  * Created: Thu Jul 24 15:21:56 2003 (edgrif)
- * CVS info:   $Id: zmapWindow.h,v 1.2 2004-03-12 15:12:13 edgrif Exp $
+ * CVS info:   $Id: zmapWindow.h,v 1.3 2004-05-17 16:22:45 edgrif Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_WINDOW_H
@@ -36,16 +37,15 @@
 
 #include <ZMap/zmapSys.h>				    /* For callback funcs... */
 
-/* Window stuff, callbacks.... */
-typedef enum {ZMAP_WINDOW_INIT, ZMAP_WINDOW_LOAD,
-	      ZMAP_WINDOW_STOP, ZMAP_WINDOW_QUIT} ZmapWindowCmd ;
-
 
 /* Opaque type, represents an individual ZMap window. */
 typedef struct _ZMapWindowStruct *ZMapWindow ;
 
+/* Window stuff, callbacks, will need changing.... */
+typedef enum {ZMAP_WINDOW_INIT, ZMAP_WINDOW_LOAD,
+	      ZMAP_WINDOW_STOP, ZMAP_WINDOW_QUIT} ZmapWindowCmd ;
 
-ZMapWindow zMapWindowCreate(char *sequence, char *zmap_id,
+ZMapWindow zMapWindowCreate(GtkWidget *parent_widget, char *sequence,
 			    zmapVoidIntCallbackFunc app_routine, void *app_data) ;
 void zMapWindowDisplayData(ZMapWindow window, void *data) ;
 void zMapWindowReset(ZMapWindow window) ;
