e4ec1d944da3b021363ddf2828db0d86ed06f6e6
edgrif <edgrif>
2004-05-27 13:41:29 +0000

    Add a new view button.

--- src/zmapControl/zmapControlWindowButtons.c
+++ src/zmapControl/zmapControlWindowButtons.c
@@ -26,15 +26,16 @@
  * Description: 
  * Exported functions: See XXXXXXXXXXXXX.h
  * HISTORY:
- * Last edited: May 19 15:07 2004 (edgrif)
+ * Last edited: May 20 15:40 2004 (edgrif)
  * Created: Thu Jul 24 14:36:27 2003 (edgrif)
- * CVS info:   $Id: zmapControlWindowButtons.c,v 1.2 2004-05-20 14:10:44 edgrif Exp $
+ * CVS info:   $Id: zmapControlWindowButtons.c,v 1.3 2004-05-27 13:41:29 edgrif Exp $
  *-------------------------------------------------------------------
  */
 
 #include <string.h>
 #include <ZMap_P.h>
 
+static void newCB(GtkWidget *widget, gpointer cb_data) ;
 static void loadCB(GtkWidget *widget, gpointer cb_data) ;
 static void stopCB(GtkWidget *widget, gpointer cb_data) ;
 static void quitCB(GtkWidget *widget, gpointer cb_data) ;
@@ -43,7 +44,7 @@ static void quitCB(GtkWidget *widget, gpointer cb_data) ;
 GtkWidget *zmapControlWindowMakeButtons(ZMap zmap)
 {
   GtkWidget *frame ;
-  GtkWidget *hbox,  *load_button, *stop_button, *quit_button ;
+  GtkWidget *hbox, *new_button, *load_button, *stop_button, *quit_button ;
 
   frame = gtk_frame_new(NULL);
   gtk_container_border_width(GTK_CONTAINER(frame), 5);
@@ -52,7 +53,7 @@ GtkWidget *zmapControlWindowMakeButtons(ZMap zmap)
   gtk_container_border_width(GTK_CONTAINER(hbox), 5);
   gtk_container_add(GTK_CONTAINER(frame), hbox);
 
-  load_button = gtk_button_new_with_label("Load Data") ;
+  load_button = gtk_button_new_with_label("Load") ;
   gtk_signal_connect(GTK_OBJECT(load_button), "clicked",
 		     GTK_SIGNAL_FUNC(loadCB), (gpointer)zmap) ;
   gtk_box_pack_start(GTK_BOX(hbox), load_button, FALSE, FALSE, 0) ;
@@ -62,14 +63,19 @@ GtkWidget *zmapControlWindowMakeButtons(ZMap zmap)
 		     GTK_SIGNAL_FUNC(stopCB), (gpointer)zmap) ;
   gtk_box_pack_start(GTK_BOX(hbox), stop_button, FALSE, FALSE, 0) ;
 
+  new_button = gtk_button_new_with_label("New") ;
+  gtk_signal_connect(GTK_OBJECT(new_button), "clicked",
+		     GTK_SIGNAL_FUNC(newCB), (gpointer)zmap) ;
+  gtk_box_pack_start(GTK_BOX(hbox), new_button, FALSE, FALSE, 0) ;
+
   quit_button = gtk_button_new_with_label("Quit") ;
   gtk_signal_connect(GTK_OBJECT(quit_button), "clicked",
 		     GTK_SIGNAL_FUNC(quitCB), (gpointer)zmap) ;
   gtk_box_pack_start(GTK_BOX(hbox), quit_button, FALSE, FALSE, 0) ;
 
-  /* default button is quit...probably not the most sensible.... */
-  GTK_WIDGET_SET_FLAGS(quit_button, GTK_CAN_DEFAULT) ;
-  gtk_window_set_default(GTK_WINDOW(zmap->toplevel), quit_button) ;
+  /* Make Load button the default, its probably what user wants to do mostly. */
+  GTK_WIDGET_SET_FLAGS(load_button, GTK_CAN_DEFAULT) ;
+  gtk_window_set_default(GTK_WINDOW(zmap->toplevel), load_button) ;
 
 
   return frame ;
@@ -82,7 +88,8 @@ GtkWidget *zmapControlWindowMakeButtons(ZMap zmap)
  *  ------------------- Internal functions -------------------
  */
 
-
+/* These callbacks simple make calls to routines in zmapControl.c, this is because I want all
+ * the state handling etc. to be in one file so that its easier to work on. */
 static void loadCB(GtkWidget *widget, gpointer cb_data)
 {
   ZMap zmap = (ZMap)cb_data ;
@@ -101,6 +108,16 @@ static void stopCB(GtkWidget *widget, gpointer cb_data)
   return ;
 }
 
+static void newCB(GtkWidget *widget, gpointer cb_data)
+{
+  ZMap zmap = (ZMap)cb_data ;
+
+  /* The string is for testing only, in the full code there should be a dialog box that enables
+   * the user to provide a whole load of info. */
+  zmapControlNewCB(zmap, "eds_sequence") ;
+
+  return ;
+}
 
 static void quitCB(GtkWidget *widget, gpointer cb_data)
 {
