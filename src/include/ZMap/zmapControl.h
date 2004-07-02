a7111da4ec48737319fdbbbbd0d57fd48806e417
rnc <rnc>
2004-07-02 13:27:45 +0000

    Moved pane-splitting code from zmapWindow to zmapControl

--- src/include/ZMap/zmapControl.h
+++ src/include/ZMap/zmapControl.h
@@ -25,9 +25,9 @@
  * Description: Interface for creating, controlling and destroying ZMaps.
  *              
  * HISTORY:
- * Last edited: Jul  1 10:07 2004 (edgrif)
+ * Last edited: Jul  2 10:50 2004 (rnc)
  * Created: Mon Nov 17 08:04:32 2003 (edgrif)
- * CVS info:   $Id: zmapControl.h,v 1.2 2004-07-01 09:24:36 edgrif Exp $
+ * CVS info:   $Id: zmapControl.h,v 1.3 2004-07-02 13:27:45 rnc Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_CONTROL_H
@@ -38,10 +38,55 @@
  * want to expose all the zmapview stuff I do want the opaque ZMapView type.
  * Think about this some more. */
 #include <ZMap/zmapView.h>
-
+#include <ZMap/zmapFeature.h>
+#include <ZMap/zmapWindow.h>
 
 /* Opaque type, represents an instance of a ZMap. */
 typedef struct _ZMapStruct *ZMap ;
+
+typedef struct zMapColumn ZMapColumn;
+
+/* callback function prototypes********************************
+ * These must be here as they're referred to in zMapColumn below
+ */
+typedef void (*colDrawFunc)  (ZMapPane pane, ZMapColumn *col,
+			      float *offset, int frame);
+typedef void (*colConfFunc)  (void);
+typedef void (*colInitFunc)  (ZMapPane pane, ZMapColumn *col);
+typedef void (*colSelectFunc)(ZMapPane pane, ZMapColumn *col,
+			      void *seg, int box, 
+			      double x, double y,
+			      gboolean isSelect);
+
+/**************************************************************/
+
+struct zMapColumn {
+  ZMapPane      pane;
+  colInitFunc   initFunc;
+  colDrawFunc   drawFunc;
+  colConfFunc   configFunc;
+  colSelectFunc selectFunc;
+  gboolean          isFrame;
+  float         priority;
+  char         *name;
+  float         startx, endx; /* filled in by drawing code */
+  methodID      meth;         /* method */
+  ZMapFeatureType        type;
+  void         *private;
+};
+
+
+struct ZMapColDefs {
+  colInitFunc   initFunc;
+  colDrawFunc   drawFunc;
+  colConfFunc   configFunc;
+  colSelectFunc selectFunc;
+  gboolean          isFrame;
+  float         priority; /* only for default columns. */
+  char         *name;
+  ZMapFeatureType        type;
+}; 
+
 
 
 /* Applications can register functions that will be called back with their own
@@ -63,6 +108,26 @@ char *zMapGetZMapID(ZMap zmap) ;
 char *zMapGetZMapStatus(ZMap zmap) ;
 gboolean zMapReset(ZMap zmap) ;
 gboolean zMapDestroy(ZMap zmap) ;
+
+GPtrArray   *zMapPaneGetCols           (ZMapPane pane);
+void         zMapPaneNewBox2Col        (ZMapPane pane, int elements);
+ZMapColumn  *zMapPaneGetBox2Col        (ZMapPane pane, int index);
+GArray      *zMapPaneSetBox2Col        (ZMapPane pane, ZMapColumn *col, int index);
+void         zMapPaneFreeBox2Col       (ZMapPane pane);
+void         zMapPaneNewBox2Seg        (ZMapPane pane, int elements);
+ZMapFeature zMapPaneGetBox2Seg        (ZMapPane pane, int index);
+GArray      *zMapPaneSetBox2Seg        (ZMapPane pane, ZMapColumn *seg, int index);
+void         zMapPaneFreeBox2Seg       (ZMapPane pane);
+ZMapRegion  *zMapPaneGetZMapRegion     (ZMapPane pane);
+FooCanvasItem *zMapPaneGetGroup        (ZMapPane pane);
+ZMapWindow   zMapPaneGetZMapWindow     (ZMapPane pane);
+FooCanvas   *zMapPaneGetCanvas         (ZMapPane pane);
+int          zMapPaneGetDNAwidth       (ZMapPane pane);
+void         zMapPaneSetDNAwidth       (ZMapPane pane, int width);
+void         zMapPaneSetStepInc        (ZMapPane pane, int incr);
+int          zMapPaneGetHeight         (ZMapPane pane);
+InvarCoord   zMapPaneGetCentre         (ZMapPane pane);
+float        zMapPaneGetBPL            (ZMapPane pane);
 
 
 #endif /* !ZMAP_CONTROL_H */
