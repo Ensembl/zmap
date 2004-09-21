1cf1fd0f8ded2d6246a1d79f97d572e6f2180c1a
rnc <rnc>
2004-09-21 13:13:33 +0000

    Fix scrolling to selected feature when zoomed in; convert c++ to c style comments

--- src/zmapWindow/zmapWindowList.c
+++ src/zmapWindow/zmapWindowList.c
@@ -28,9 +28,9 @@
  *              
  * Exported functions: 
  * HISTORY:
- * Last edited: Sep 16 15:52 2004 (rnc)
+ * Last edited: Sep 21 12:18 2004 (rnc)
  * Created: Thu Sep 16 10:17 2004 (rnc)
- * CVS info:   $Id: zmapWindowList.c,v 1.1 2004-09-16 15:44:32 rnc Exp $
+ * CVS info:   $Id: zmapWindowList.c,v 1.2 2004-09-21 13:13:33 rnc Exp $
  *-------------------------------------------------------------------
  */
 
@@ -53,7 +53,6 @@ static void quitListCB   (GtkWidget *window, gpointer data);
 void zMapWindowCreateListWindow(ParamStruct *params, ZMapFeatureSet feature_set)
 {
   GtkWidget *treeView = gtk_tree_view_new();
-  GtkRequisition req;
   GtkWidget *featureList;
   GtkTreeStore *list = gtk_tree_store_new(N_COLUMNS,
 					  G_TYPE_STRING,
@@ -64,29 +63,25 @@ void zMapWindowCreateListWindow(ParamStruct *params, ZMapFeatureSet feature_set)
   GtkTreeSelection *select;
 
 
-  // first set up the top level window
+  /* first set up the top level window */
   params->window->featureListWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  req.width = 1000;
-  req.height = 1000;
-  gtk_widget_size_request(params->window->featureListWindow, &req);
-  //  gtk_container_add(GTK_CONTAINER(params->window->featureListWindow), treeView);
   
-  // Build and populate the list
+  /* Build and populate the list */
   g_datalist_foreach(&(feature_set->features),addItemToList , list) ;
 
   sort_model = gtk_tree_model_sort_new_with_model (GTK_TREE_MODEL(list));
 
   featureList = gtk_tree_view_new_with_model(GTK_TREE_MODEL(sort_model));
 
-  // sort the list on the start coordinate
+  /* sort the list on the start coordinate */
   gtk_tree_sortable_set_sort_column_id(GTK_TREE_SORTABLE(sort_model),
 				       START,
 				       GTK_SORT_ASCENDING);
 
-  // finished with list now
+  /* finished with list now */
   g_object_unref(G_OBJECT(list));
 
-  // render the columns
+  /* render the columns */
   renderer = gtk_cell_renderer_text_new ();
   column = gtk_tree_view_column_new_with_attributes ("Name",
 						     renderer,
@@ -110,7 +105,6 @@ void zMapWindowCreateListWindow(ParamStruct *params, ZMapFeatureSet feature_set)
   g_signal_connect(GTK_OBJECT(params->window->featureListWindow), "destroy",
 		   GTK_SIGNAL_FUNC(quitListCB), featureList);
 
-  //  gtk_container_add(GTK_CONTAINER(treeView), featureList);
   gtk_container_add(GTK_CONTAINER(params->window->featureListWindow), featureList);
 
   gtk_widget_show_all(params->window->featureListWindow);
@@ -141,20 +135,32 @@ static int tree_selection_changed_cb (GtkTreeSelection *selection, gpointer data
   GtkTreeIter iter;
   GtkTreeModel *model;
   int x, y, start = 0;
+  gchar *name;
+  static gboolean FIRSTTIME = TRUE;
 
-  // retrieve user's selection
-  if (gtk_tree_selection_get_selected (selection, &model, &iter))
-      gtk_tree_model_get (model, &iter, START, &start, -1);
-
-  if (start)
+  if (FIRSTTIME)  /* when we first display the list, don't scroll to selected feature. */
+    FIRSTTIME = FALSE;
+  else
     {
-      // get current position on canvas
-      foo_canvas_get_scroll_offsets(params->thisCanvas, &x, &y);
+      // retrieve user's selection
+      if (gtk_tree_selection_get_selected (selection, &model, &iter))
+	{
+	  gtk_tree_model_get (model, &iter, NAME, &name, START, &start, -1);
+	}
       
-      // scroll up or down to user's selection
-      foo_canvas_scroll_to(params->thisCanvas, x, start * params->magFactor);
+      if (start)
+	{
+	  /* get current position on canvas */
+	  foo_canvas_get_scroll_offsets(params->thisCanvas, &x, &y);
+      
+	  /* scroll up or down to user's selection */
+	  zmapDisplayText(FOO_CANVAS_GROUP(params->feature_group), name, 
+			  "brown", x, start * params->magFactor); 
+	  g_free(name); 
+	  foo_canvas_scroll_to(params->thisCanvas, x, start * params->magFactor * params->window->zoom_factor);
+	  gtk_widget_show_all(GTK_WIDGET(params->window->parent_widget));
+	}
     }
-
   return TRUE;
 }
 
