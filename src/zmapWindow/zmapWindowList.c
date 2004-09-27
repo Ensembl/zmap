1ee2dd35fad544abdbdaf4d5115ec17b33c7ef1c
rnc <rnc>
2004-09-27 09:23:13 +0000

    Highlight the feature the user selected in the selectable features list

--- src/zmapWindow/zmapWindowList.c
+++ src/zmapWindow/zmapWindowList.c
@@ -28,9 +28,9 @@
  *              
  * Exported functions: 
  * HISTORY:
- * Last edited: Sep 21 12:18 2004 (rnc)
+ * Last edited: Sep 27 10:12 2004 (rnc)
  * Created: Thu Sep 16 10:17 2004 (rnc)
- * CVS info:   $Id: zmapWindowList.c,v 1.2 2004-09-21 13:13:33 rnc Exp $
+ * CVS info:   $Id: zmapWindowList.c,v 1.3 2004-09-27 09:23:13 rnc Exp $
  *-------------------------------------------------------------------
  */
 
@@ -38,8 +38,9 @@
 #include <glib.h>
 #include <zmapWindow_P.h>
 #include <ZMap/zmapWindowDrawFeatures.h>
+#include <ZMap/zmapFeature.h>
 
-enum { NAME, START, N_COLUMNS };
+enum { NAME, START, TYPE, N_COLUMNS };
 
 
 
@@ -50,21 +51,26 @@ static void quitListCB   (GtkWidget *window, gpointer data);
 
 
 /* functions *************************************************************/
-void zMapWindowCreateListWindow(ParamStruct *params, ZMapFeatureSet feature_set)
+void zMapWindowCreateListWindow(ZMapCanvasDataStruct *canvasData, ZMapFeatureSet feature_set)
 {
   GtkWidget *treeView = gtk_tree_view_new();
   GtkWidget *featureList;
   GtkTreeStore *list = gtk_tree_store_new(N_COLUMNS,
 					  G_TYPE_STRING,
-					  G_TYPE_INT);
+					  G_TYPE_INT,
+					  G_TYPE_STRING);
   GtkTreeModel *sort_model;
   GtkCellRenderer *renderer;
   GtkTreeViewColumn *column;
   GtkTreeSelection *select;
 
 
-  /* first set up the top level window */
-  params->window->featureListWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  /* set up the top level window */
+  canvasData->window->featureListWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_container_border_width(GTK_CONTAINER(canvasData->window->featureListWindow), 5) ;
+  gtk_window_set_title(GTK_WINDOW(canvasData->window->featureListWindow), feature_set->source) ;
+  gtk_window_set_default_size(GTK_WINDOW(canvasData->window->featureListWindow), -1, 200);
+
   
   /* Build and populate the list */
   g_datalist_foreach(&(feature_set->features),addItemToList , list) ;
@@ -94,20 +100,36 @@ void zMapWindowCreateListWindow(ParamStruct *params, ZMapFeatureSet feature_set)
 						     "text", START,
 						     NULL);
   gtk_tree_view_append_column (GTK_TREE_VIEW (featureList), column);
+  /*
+  column = gtk_tree_view_column_new_with_attributes ("End",
+						     renderer,
+						     "text", END,
+						     NULL);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (featureList), column);
+
+  gtk_tree_view_column_set_visible(column, FALSE);
+  */ 
+  column = gtk_tree_view_column_new_with_attributes ("Type",
+						     renderer,
+						     "text", TYPE,
+						     NULL);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (featureList), column);
+
+  gtk_tree_view_column_set_visible(column, FALSE);
   
   /* Setup the selection handler */
   select = gtk_tree_view_get_selection (GTK_TREE_VIEW (featureList));
   gtk_tree_selection_set_mode (select, GTK_SELECTION_SINGLE);
   g_signal_connect (G_OBJECT (select), "changed",
 		    G_CALLBACK (tree_selection_changed_cb),
-		    params);
+		    canvasData);
 
-  g_signal_connect(GTK_OBJECT(params->window->featureListWindow), "destroy",
+  g_signal_connect(GTK_OBJECT(canvasData->window->featureListWindow), "destroy",
 		   GTK_SIGNAL_FUNC(quitListCB), featureList);
 
-  gtk_container_add(GTK_CONTAINER(params->window->featureListWindow), featureList);
+  gtk_container_add(GTK_CONTAINER(canvasData->window->featureListWindow), featureList);
 
-  gtk_widget_show_all(params->window->featureListWindow);
+  gtk_widget_show_all(canvasData->window->featureListWindow);
 
   return;
 }
@@ -120,10 +142,12 @@ static void addItemToList(GQuark key_id, gpointer data, gpointer user_data)
   GtkTreeStore *list    = (GtkTreeStore*)user_data;
   GtkTreeIter   iter1;
 
+
   gtk_tree_store_append(GTK_TREE_STORE(list), &iter1, NULL);
   gtk_tree_store_set   (GTK_TREE_STORE(list), &iter1, 
-			NAME, feature->name,
+			NAME , feature->name,
 			START, feature->x1,
+			TYPE , feature->method_name,
 			-1 );
 
   return;
@@ -131,11 +155,15 @@ static void addItemToList(GQuark key_id, gpointer data, gpointer user_data)
 
 static int tree_selection_changed_cb (GtkTreeSelection *selection, gpointer data)
 {
-  ParamStruct *params = (ParamStruct*)data;
+  ZMapCanvasDataStruct *canvasData = (ZMapCanvasDataStruct*)data;
   GtkTreeIter iter;
   GtkTreeModel *model;
   int x, y, start = 0;
-  gchar *name;
+  double wx, wy;
+  gchar *name, *type;
+  FooCanvasItem *item;
+  GdkColor outline;
+  GdkColor foreground;
   static gboolean FIRSTTIME = TRUE;
 
   if (FIRSTTIME)  /* when we first display the list, don't scroll to selected feature. */
@@ -145,21 +173,30 @@ static int tree_selection_changed_cb (GtkTreeSelection *selection, gpointer data
       // retrieve user's selection
       if (gtk_tree_selection_get_selected (selection, &model, &iter))
 	{
-	  gtk_tree_model_get (model, &iter, NAME, &name, START, &start, -1);
+	  gtk_tree_model_get (model, &iter, NAME, &name, START, &start, TYPE, &type, -1);
 	}
       
       if (start)
 	{
-	  /* get current position on canvas */
-	  foo_canvas_get_scroll_offsets(params->thisCanvas, &x, &y);
-      
 	  /* scroll up or down to user's selection */
-	  zmapDisplayText(FOO_CANVAS_GROUP(params->feature_group), name, 
-			  "brown", x, start * params->magFactor); 
-	  g_free(name); 
-	  foo_canvas_scroll_to(params->thisCanvas, x, start * params->magFactor * params->window->zoom_factor);
-	  gtk_widget_show_all(GTK_WIDGET(params->window->parent_widget));
+	  foo_canvas_scroll_to(canvasData->canvas, 0.0, 
+			       start * canvasData->magFactor * canvasData->window->zoom_factor);
+
+	  foo_canvas_c2w(canvasData->canvas, 0.0, 
+			 start * canvasData->magFactor * canvasData->window->zoom_factor, &wx, &wy);
+
+	  item = foo_canvas_get_item_at(canvasData->canvas, canvasData->x+1.0, wy+7.0);
+
+	  canvasData->thisType = (ZMapFeatureTypeStyle)g_datalist_get_data(&(canvasData->types), type) ;
+
+	  zmapHighlightObject(item, canvasData);
+	  zMapFeatureClickCB(canvasData, canvasData->feature); /* show feature details on info_panel  */
+
+	  gtk_widget_show_all(GTK_WIDGET(canvasData->window->parent_widget));
 	}
+
+      if (name)      
+	g_free(name); 
     }
   return TRUE;
 }
