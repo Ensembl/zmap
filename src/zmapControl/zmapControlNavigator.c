14e0e1e9ae6b83947c183f837f1f8eeebcbfa924
edgrif <edgrif>
2004-12-20 10:54:00 +0000

    add second navigator bar to show user how big their scrolling window is.

--- src/zmapControl/zmapControlNavigator.c
+++ src/zmapControl/zmapControlNavigator.c
@@ -29,9 +29,9 @@
  *              
  * Exported functions: See zmapControl_P.h
  * HISTORY:
- * Last edited: Nov 11 13:51 2004 (rnc)
+ * Last edited: Dec 16 16:07 2004 (edgrif)
  * Created: Thu Jul  8 12:54:27 2004 (edgrif)
- * CVS info:   $Id: zmapControlNavigator.c,v 1.18 2004-11-12 13:32:29 rnc Exp $
+ * CVS info:   $Id: zmapControlNavigator.c,v 1.19 2004-12-20 10:54:00 edgrif Exp $
  *-------------------------------------------------------------------
  */
 
@@ -41,34 +41,90 @@
 #define TOPTEXT_NO_SCALE "<no data>"
 #define BOTTEXT_NO_SCALE ""
 
-void zmapControlNavigatorCreate(ZMap zmap, GtkWidget *frame)
+
+/* AGGGGHHHHH, THIS SHOULD BE AN "OBJECT" IN ITS OWN RIGHT.....LETS TRY AND MAKE IT BETTER */
+
+ZMapNavigator zmapControlNavigatorCreate(GtkWidget **top_widg_out)
 {
+  ZMapNavigator navigator ;
+  GtkWidget *pane ;
   GtkObject *adjustment ;
 
-  zmap->navigator = g_new0(ZMapNavStruct, 1);
+  GtkWidget *vbox, *label, *scroll ;
+
+  navigator = g_new0(ZMapNavStruct, 1);
+
+  pane = gtk_hpaned_new() ;
+  gtk_widget_set_size_request(pane, 100, -1) ;
+
+
+  /* Construct the region locator. */
 
   /* Need a vbox so we can add a label with sequence size at the bottom later,
    * we set it to a fixed width so that the text is always visible. */
-  zmap->navigator->navVBox = gtk_vbox_new(FALSE, 0);
-  gtk_widget_set_size_request(zmap->navigator->navVBox, 100, -1); 
+  navigator->navVBox = gtk_vbox_new(FALSE, 0);
+  gtk_paned_add1(GTK_PANED(pane), navigator->navVBox) ;
 
-  zmap->navigator->topLabel = gtk_label_new(TOPTEXT_NO_SCALE) ;
-  gtk_box_pack_start(GTK_BOX(zmap->navigator->navVBox), zmap->navigator->topLabel, FALSE, TRUE, 0);
+
+  navigator->topLabel = gtk_label_new(TOPTEXT_NO_SCALE) ;
+  gtk_box_pack_start(GTK_BOX(navigator->navVBox), navigator->topLabel, FALSE, TRUE, 0);
 
 
   /* Make the navigator with a default, "blank" adjustment obj. */
   adjustment = gtk_adjustment_new(0.0, 0.0, 0.0, 0.0, 0.0, 0.0) ;
 
-  zmap->navigator->navVScroll = gtk_vscrollbar_new(GTK_ADJUSTMENT(adjustment)) ;
-  gtk_box_pack_start(GTK_BOX(zmap->navigator->navVBox), zmap->navigator->navVScroll, TRUE, TRUE, 0) ;
+  navigator->navVScroll = gtk_vscrollbar_new(GTK_ADJUSTMENT(adjustment)) ;
+  gtk_box_pack_start(GTK_BOX(navigator->navVBox), navigator->navVScroll, TRUE, TRUE, 0) ;
 
   /* Note how we pack the label at the end of the vbox and set "expand" to FALSE so that it
    * remains small and the vscale expands to fill the rest of the box. */
-  zmap->navigator->botLabel = gtk_label_new(BOTTEXT_NO_SCALE) ;
-  gtk_box_pack_end(GTK_BOX(zmap->navigator->navVBox), zmap->navigator->botLabel, FALSE, TRUE, 0);
+  navigator->botLabel = gtk_label_new(BOTTEXT_NO_SCALE) ;
+  gtk_box_pack_end(GTK_BOX(navigator->navVBox), navigator->botLabel, FALSE, TRUE, 0);
 
 
-  return;
+  /* Construct the window locator. */
+  navigator->wind_vbox = gtk_vbox_new(FALSE, 0);
+  gtk_paned_add2(GTK_PANED(pane), navigator->wind_vbox) ;
+
+  navigator->wind_top_label = gtk_label_new(TOPTEXT_NO_SCALE) ;
+  gtk_box_pack_start(GTK_BOX(navigator->wind_vbox), navigator->wind_top_label, FALSE, TRUE, 0);
+
+
+  /* Make the navigator with a default, "blank" adjustment obj. */
+  adjustment = gtk_adjustment_new(0.0, 0.0, 0.0, 0.0, 0.0, 0.0) ;
+
+  navigator->wind_scroll = gtk_vscrollbar_new(GTK_ADJUSTMENT(adjustment)) ;
+  gtk_box_pack_start(GTK_BOX(navigator->wind_vbox), navigator->wind_scroll, TRUE, TRUE, 0) ;
+
+  /* Note how we pack the label at the end of the vbox and set "expand" to FALSE so that it
+   * remains small and the vscale expands to fill the rest of the box. */
+  navigator->wind_bot_label = gtk_label_new(BOTTEXT_NO_SCALE) ;
+  gtk_box_pack_end(GTK_BOX(navigator->wind_vbox), navigator->wind_bot_label, FALSE, TRUE, 0);
+
+  navigator->wind_top = navigator->wind_bot = 0.0 ;
+
+
+  /* Set left hand (region view) pane closed. */
+  gtk_paned_set_position(GTK_PANED(pane), 0) ;
+
+  *top_widg_out = pane ;
+
+  return navigator ;
+}
+
+
+void zmapControlNavigatorSetWindowPos(ZMapNavigator navigator, double top_pos, double bot_pos)
+{
+  GtkObject *window_adjuster ;
+
+  window_adjuster = (GtkObject *)gtk_range_get_adjustment(GTK_RANGE(navigator->wind_scroll)) ;
+
+  GTK_ADJUSTMENT(window_adjuster)->value = top_pos ;
+  GTK_ADJUSTMENT(window_adjuster)->page_size = (gdouble)(abs(bot_pos - top_pos) + 1) ;
+
+  gtk_adjustment_changed(GTK_ADJUSTMENT(window_adjuster)) ;
+
+  return ;
 }
 
 
@@ -76,11 +132,11 @@ void zmapControlNavigatorCreate(ZMap zmap, GtkWidget *frame)
 /* updates size/range to coords of the new view. */
 void zmapControlNavigatorNewView(ZMapNavigator navigator, ZMapFeatureContext features)
 {
-  gchar *top_str, *bot_str ;
-  GtkObject *adjuster ;
+  GtkObject *region_adjuster, *window_adjuster ;
+  gchar *region_top_str, *region_bot_str, *window_top_str, *window_bot_str ;
 
-  adjuster = (GtkObject *)gtk_range_get_adjustment(GTK_RANGE(navigator->navVScroll)) ;
-
+  region_adjuster = (GtkObject *)gtk_range_get_adjustment(GTK_RANGE(navigator->navVScroll)) ;
+  window_adjuster = (GtkObject *)gtk_range_get_adjustment(GTK_RANGE(navigator->wind_scroll)) ;
 
   /* May be called with no sequence to parent mapping so must set default navigator for this. */
   if (features)
@@ -88,17 +144,29 @@ void zmapControlNavigatorNewView(ZMapNavigator navigator, ZMapFeatureContext fea
       navigator->parent_span = features->parent_span ;	    /* n.b. struct copy. */
       navigator->sequence_to_parent = features->sequence_to_parent ; /* n.b. struct copy. */
 
-      top_str = g_strdup_printf("%d", navigator->parent_span.x1) ;
-      bot_str = g_strdup_printf("%d", navigator->parent_span.x2) ;
+      region_top_str = g_strdup_printf("%d", navigator->parent_span.x1) ;
+      region_bot_str = g_strdup_printf("%d", navigator->parent_span.x2) ;
 
-      GTK_ADJUSTMENT(adjuster)->value = (((gdouble)(navigator->sequence_to_parent.p1
+      GTK_ADJUSTMENT(region_adjuster)->value = (((gdouble)(navigator->sequence_to_parent.p1
 						    + navigator->sequence_to_parent.p2)) / 2.0) ;
-      GTK_ADJUSTMENT(adjuster)->lower = (gdouble)navigator->parent_span.x1 ;
-      GTK_ADJUSTMENT(adjuster)->upper = (gdouble)navigator->parent_span.x2 ;
-      GTK_ADJUSTMENT(adjuster)->step_increment = 10.0 ;	    /* step incr, wild guess... */
-      GTK_ADJUSTMENT(adjuster)->page_increment = 1000 ;	    /* page incr, wild guess... */
-      GTK_ADJUSTMENT(adjuster)->page_size = (gdouble)(abs(navigator->sequence_to_parent.p2
+      GTK_ADJUSTMENT(region_adjuster)->lower = (gdouble)navigator->parent_span.x1 ;
+      GTK_ADJUSTMENT(region_adjuster)->upper = (gdouble)navigator->parent_span.x2 ;
+      GTK_ADJUSTMENT(region_adjuster)->step_increment = 10.0 ;	    /* step incr, wild guess... */
+      GTK_ADJUSTMENT(region_adjuster)->page_increment = 1000 ;	    /* page incr, wild guess... */
+      GTK_ADJUSTMENT(region_adjuster)->page_size = (gdouble)(abs(navigator->sequence_to_parent.p2
 							  - navigator->sequence_to_parent.p1) + 1) ;
+
+      window_top_str = g_strdup_printf("%d", navigator->sequence_to_parent.c1) ;
+      window_bot_str = g_strdup_printf("%d", navigator->sequence_to_parent.c2) ;
+
+      /* I assume here that we start at the top. */
+      GTK_ADJUSTMENT(window_adjuster)->value = (0.0) ;
+      GTK_ADJUSTMENT(window_adjuster)->lower = (gdouble)navigator->sequence_to_parent.c1 ;
+      GTK_ADJUSTMENT(window_adjuster)->upper = (gdouble)navigator->sequence_to_parent.c2 ;
+      GTK_ADJUSTMENT(window_adjuster)->step_increment = 0.0 ;
+      GTK_ADJUSTMENT(window_adjuster)->page_increment = 0.0 ;
+      GTK_ADJUSTMENT(window_adjuster)->page_size = (gdouble)(abs(navigator->sequence_to_parent.c2
+								 - navigator->sequence_to_parent.c1) + 1) ;
     }
   else
     {
@@ -107,24 +175,41 @@ void zmapControlNavigatorNewView(ZMapNavigator navigator, ZMapFeatureContext fea
       navigator->sequence_to_parent.p1 = navigator->sequence_to_parent.p2
 	= navigator->sequence_to_parent.c1 = navigator->sequence_to_parent.c2 = 0 ;
 
-      top_str = g_strdup(TOPTEXT_NO_SCALE) ;
-      bot_str = g_strdup(BOTTEXT_NO_SCALE) ;
+      region_top_str = g_strdup(TOPTEXT_NO_SCALE) ;
+      region_bot_str = g_strdup(BOTTEXT_NO_SCALE) ;
 
-      GTK_ADJUSTMENT(adjuster)->value = 0.0 ;
-      GTK_ADJUSTMENT(adjuster)->lower = 0.0 ;
-      GTK_ADJUSTMENT(adjuster)->upper = 0.0 ;
-      GTK_ADJUSTMENT(adjuster)->step_increment = 0.0 ;
-      GTK_ADJUSTMENT(adjuster)->page_increment = 0.0 ;
-      GTK_ADJUSTMENT(adjuster)->page_size = 0.0 ;
+      GTK_ADJUSTMENT(region_adjuster)->value = 0.0 ;
+      GTK_ADJUSTMENT(region_adjuster)->lower = 0.0 ;
+      GTK_ADJUSTMENT(region_adjuster)->upper = 0.0 ;
+      GTK_ADJUSTMENT(region_adjuster)->step_increment = 0.0 ;
+      GTK_ADJUSTMENT(region_adjuster)->page_increment = 0.0 ;
+      GTK_ADJUSTMENT(region_adjuster)->page_size = 0.0 ;
+
+      window_top_str = g_strdup(TOPTEXT_NO_SCALE) ;
+      window_bot_str = g_strdup(BOTTEXT_NO_SCALE) ;
+
+      GTK_ADJUSTMENT(window_adjuster)->value = 0.0 ;
+      GTK_ADJUSTMENT(window_adjuster)->lower = 0.0 ;
+      GTK_ADJUSTMENT(window_adjuster)->upper = 0.0 ;
+      GTK_ADJUSTMENT(window_adjuster)->step_increment = 0.0 ;
+      GTK_ADJUSTMENT(window_adjuster)->page_increment = 0.0 ;
+      GTK_ADJUSTMENT(window_adjuster)->page_size = 0.0 ;
     }
 
-  gtk_adjustment_changed(GTK_ADJUSTMENT(adjuster)) ;
+  gtk_adjustment_changed(GTK_ADJUSTMENT(region_adjuster)) ;
+  gtk_adjustment_changed(GTK_ADJUSTMENT(window_adjuster)) ;
 
-  gtk_label_set_text(GTK_LABEL(navigator->topLabel), top_str) ;
-  gtk_label_set_text(GTK_LABEL(navigator->botLabel), bot_str) ;
+  gtk_label_set_text(GTK_LABEL(navigator->topLabel), region_top_str) ;
+  gtk_label_set_text(GTK_LABEL(navigator->botLabel), region_bot_str) ;
 
-  g_free(top_str) ;
-  g_free(bot_str) ;
+  g_free(region_top_str) ;
+  g_free(region_bot_str) ;
+
+  gtk_label_set_text(GTK_LABEL(navigator->wind_top_label), window_top_str) ;
+  gtk_label_set_text(GTK_LABEL(navigator->wind_bot_label), window_bot_str) ;
+
+  g_free(window_top_str) ;
+  g_free(window_bot_str) ;
 
   return ;
 }
