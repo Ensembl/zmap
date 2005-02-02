46896eae9e0560049dae62432b5d563b8238fc34
edgrif <edgrif>
2005-02-02 15:03:51 +0000

    private feature header.

---
+++ src/zmapFeature/zmapFeature_P.h
@@ -0,0 +1,44 @@
+/*  File: zmapFeature_P.h
+ *  Author: Ed Griffiths (edgrif@sanger.ac.uk)
+ *  Copyright (c) Sanger Institute, 2004
+ *-------------------------------------------------------------------
+ * ZMap is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+ *-------------------------------------------------------------------
+ * This file is part of the ZMap genome database package
+ * originated by
+ * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
+ *      Rob Clack (Sanger Institute, UK) rnc@sanger.ac.uk
+ *
+ * Description: Internals for zmapFeature routines.
+ *
+ * HISTORY:
+ * Last edited: Nov 24 11:02 2004 (edgrif)
+ * Created: Wed Nov 24 11:01:24 2004 (edgrif)
+ * CVS info:   $Id: zmapFeature_P.h,v 1.1 2005-02-02 15:03:51 edgrif Exp $
+ *-------------------------------------------------------------------
+ */
+#ifndef ZMAP_FEATURE_P_H
+#define ZMAP_FEATURE_P_H
+
+#include <ZMap/zmapFeature.h>
+
+
+
+void zmapPrintFeatureContext(ZMapFeatureContext context) ;
+
+
+
+#endif /* !ZMAP_FEATURE_P_H */
