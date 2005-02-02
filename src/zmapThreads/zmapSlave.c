913312aa9177b1a1717b8fa1bb399a709eaea867
edgrif <edgrif>
2005-02-02 14:39:39 +0000

    remove all server specific code, replace with generalised thread handling code.

--- src/zmapThreads/zmapSlave.c
+++ src/zmapThreads/zmapSlave.c
@@ -19,20 +19,23 @@
  *-------------------------------------------------------------------
  * This file is part of the ZMap genome database package
  * and was written by
- *      Rob Clack (Sanger Institute, UK) rnc@sanger.ac.uk,
- * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk
+ * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
+ *      Rob Clack (Sanger Institute, UK) rnc@sanger.ac.uk
  *
- * Description: 
+ * Description: This code is called when a new thread is created by the
+ *              zmapThread code, it loops waiting for commands from
+ *              a master thread.
+ *              
  * Exported functions: See zmapConn_P.h
  * HISTORY:
- * Last edited: Dec 14 09:53 2004 (edgrif)
+ * Last edited: Feb  2 14:09 2005 (edgrif)
  * Created: Thu Jul 24 14:37:26 2003 (edgrif)
- * CVS info:   $Id: zmapSlave.c,v 1.22 2004-12-15 14:11:47 edgrif Exp $
+ * CVS info:   $Id: zmapSlave.c,v 1.23 2005-02-02 14:39:39 edgrif Exp $
  *-------------------------------------------------------------------
  */
 
-
 #include <strings.h>
+
 /* YOU SHOULD BE AWARE THAT ON SOME PLATFORMS (E.G. ALPHAS) THERE SEEMS TO BE SOME INTERACTION
  * BETWEEN pthread.h AND gtk.h SUCH THAT IF pthread.h COMES FIRST THEN gtk.h INCLUDES GET MESSED
  * UP AND THIS FILE WILL NOT COMPILE.... */
@@ -40,48 +43,15 @@
 #include <ZMap/zmapUtils.h>
 
 /* With some additional calls in the zmapConn code I could get rid of the need for
- * the private header here...... */
-#include <zmapConn_P.h>
-
+ * the private header here......check this out... */
+#include <zmapThreads_P.h>
 #include <zmapSlave_P.h>
 
 
-
-enum {ZMAP_SLAVE_REQ_BUFSIZE = 512} ;
-
-
-/* Some protocols have global init/cleanup functions that must only be called once, this type/list
- * allows us to do this. */
-typedef struct
-{
-  char *protocol ;
-  gboolean init_called ;
-  void *global_init_data ;
-  gboolean cleanup_called ;
-} ZMapProtocolInitStruct, *ZMapProtocolInit ;
-
-typedef struct
-{
-  pthread_mutex_t mutex ;				    /* Protects access to init/cleanup list. */
-  GList *protocol_list ;				    /* init/cleanup list. */
-} ZMapProtocolInitListStruct, *ZMapProtocolInitList ;
-
-
+enum {ZMAPTHREAD_SLAVE_REQ_BUFSIZE = 512} ;
 
 
 static void cleanUpThread(void *thread_args) ;
-static void protocolGlobalInitFunc(ZMapProtocolInitList protocols, char *protocol,
-				   void **global_init_data) ;
-static int findProtocol(gconstpointer list_protocol, gconstpointer protocol) ;
-
-
-
-/* Set up the list, note the special pthread macro that makes sure mutex is set up before
- * any threads can use it. */
-static ZMapProtocolInitListStruct protocol_init_G = {PTHREAD_MUTEX_INITIALIZER, NULL} ;
-
-
-
 
 
 /* This is the routine that is called by the pthread_create() function, in effect
@@ -93,197 +63,135 @@ static ZMapProtocolInitListStruct protocol_init_G = {PTHREAD_MUTEX_INITIALIZER, 
  * really does clean up properly. */
 void *zmapNewThread(void *thread_args)
 {
-  zmapThreadCB thread_cb ;
-  void *global_init_data ;
-  ZMapConnection connection = (ZMapConnection)thread_args ;
-  ZMapRequest thread_state = &(connection->request) ;
+  zmapThreadCB thread_cb = NULL ;
+  ZMapThread thread = (ZMapThread)thread_args ;
+  ZMapRequest thread_state = &(thread->request) ;
   TIMESPEC timeout ;
   ZMapThreadRequest signalled_state ;
-  ZMapServerSetContext context ;
 
-  ZMAP_THR_DEBUG(("%x: main thread routine starting....\n", connection->thread_id)) ;
+  ZMAPTHREAD_DEBUG(("%lu: main thread routine starting....\n", thread->thread_id)) ;
 
-
+  /* We set up the thread struct and then immediately set up our pthread clean up routine
+   * to catch errors, if we are cancelled before this then the clean up routine will not
+   * be called. */
   thread_cb = g_new0(zmapThreadCBstruct, 1) ;
-  thread_cb->connection = connection ;
+  thread_cb->thread = thread ;
   thread_cb->thread_died = FALSE ;
   thread_cb->initial_error = NULL ;
-  thread_cb->server = NULL ;
-
+  thread_cb->slave_data = NULL ;
 #ifdef ED_G_NEVER_INCLUDE_THIS_CODE
-  thread_cb->server_request = ZMAP_SERVERREQ_INVALID ;
-#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
+  thread_cb->server_request = ZMAPTHREAD_SERVERREQ_INVALID ;
 
-
-#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
   thread_cb->server_reply = NULL ;
 #endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
-
-
-
-  /* Only do the minimum before setting this up as this is the call that makes sure our
-   * cleanup routine is called. */
   pthread_cleanup_push(cleanUpThread, (void *)thread_cb) ;
 
-
-  /* Check if we need to call the global init function of the protocol, this is a
-   * function that should only be called once. */
-  protocolGlobalInitFunc(&protocol_init_G, connection->protocol, &global_init_data) ;
-
-
-  /* Create the connection block for this specific server connection. */
-  if (!zMapServerCreateConnection(&(thread_cb->server), global_init_data,
-				  connection->machine, connection->port,
-				  connection->protocol, connection->timeout, connection->version,
-				  "any", "any"))
-    {
-      thread_cb->thread_died = TRUE ;
-      thread_cb->initial_error = g_strdup_printf("%s - %s", ZMAPSLAVE_CONNCREATE,
-						 zMapServerLastErrorMsg(thread_cb->server)) ;
-      goto clean_up ;
-    }
-
-  if (zMapServerOpenConnection(thread_cb->server) != ZMAP_SERVERRESPONSE_OK)
-    {
-      thread_cb->thread_died = TRUE ;
-      thread_cb->initial_error = g_strdup_printf("%s - %s", ZMAPSLAVE_CONNOPEN,
-						 zMapServerLastErrorMsg(thread_cb->server)) ;
-      goto clean_up ;
-    }
-
-
-  /* Create a sequence context from the sequence and start/end data. */
-  context = g_new0(ZMapServerSetContextStruct, 1) ;
-  context->sequence = connection->sequence ;
-  context->start = connection->start ;
-  context->end = connection->end ;
-  context->types = connection->types ;
-
-  if (zMapServerSetContext(thread_cb->server, context)
-      != ZMAP_SERVERRESPONSE_OK)
-    {
-      thread_cb->thread_died = TRUE ;
-      thread_cb->initial_error = g_strdup_printf("%s - %s", ZMAPSLAVE_CONNCONTEXT,
-						 zMapServerLastErrorMsg(thread_cb->server)) ;
-      goto clean_up ;
-    }
-
-  g_free(context) ;
-
-
-  if (connection->initial_request)
-    {
-      ZMapServerResponseType server_response ;
-
-      if (zMapServerRequest(thread_cb->server, connection->initial_request) == ZMAP_SERVERRESPONSE_OK)
-	{
-	  /* Signal that we got some data. */
-	  zmapVarSetValueWithData(&(connection->reply), ZMAP_REPLY_GOTDATA, connection->initial_request) ;
-	}
-      else
-	{
-	  connection->initial_request = NULL ;		    /* make sure there is explicitly no
-							       data returned. */
-
-	  thread_cb->thread_died = TRUE ;
-	  thread_cb->initial_error = g_strdup_printf("%s - %s", ZMAPSLAVE_CONNREQUEST,
-						     zMapServerLastErrorMsg(thread_cb->server)) ;
-	  goto clean_up ;
-	}
-    }
-  
+ 
 
 #ifdef ED_G_NEVER_INCLUDE_THIS_CODE
   /* somehow doing this screws up the whole gui-slave communication bit...not sure how....
    * the slaves just die, almost all the time.... */
 
   /* Signal that we are ready and waiting... */
-  zMapConnSetReply(connection, ZMAP_REPLY_WAIT) ;
+  zMapConnSetReply(thread, ZMAPTHREAD_REPLY_WAIT) ;
 #endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
-
 
 
   while (1)
     {
       void *request ;
 
-      ZMAP_THR_DEBUG(("%x: about to do timed wait\n", connection->thread_id)) ;
+      ZMAPTHREAD_DEBUG(("%lu: about to do timed wait\n", thread->thread_id)) ;
 
       /* this will crap over performance...asking the time all the time !! */
       timeout.tv_sec = 5 ;				    /* n.b. interface seems to absolute time. */
       timeout.tv_nsec = 0 ;
       request = NULL ;
-      signalled_state = zmapCondVarWaitTimed(thread_state, ZMAP_REQUEST_WAIT, &timeout, TRUE,
+      signalled_state = zmapCondVarWaitTimed(thread_state, ZMAPTHREAD_REQUEST_WAIT, &timeout, TRUE,
 					     &request) ;
 
-      /* The whole request bit needs sorting out...into types and data... */
+      ZMAPTHREAD_DEBUG(("%lu: finished condvar wait, state = %s\n", thread->thread_id,
+			zMapThreadGetRequestString(signalled_state))) ;
 
-      ZMAP_THR_DEBUG(("%x: finished condvar wait, state = %s\n", connection->thread_id,
-		      zMapVarGetRequestString(signalled_state))) ;
-
-      if (signalled_state == ZMAP_REQUEST_TIMED_OUT)
+      if (signalled_state == ZMAPTHREAD_REQUEST_TIMED_OUT)
 	{
 	  continue ;
 	}
-      else if (signalled_state == ZMAP_REQUEST_GETDATA)
+      else if (signalled_state == ZMAPTHREAD_REQUEST_EXECUTE)
 	{
-	  static int failure = 0 ;
-	  char *server_command ;
-	  int reply_len = 0 ;
-	  ZMapServerResponseType server_response ;
+	  ZMapThreadReturnCode slave_response ;
+	  void *reply ;
+	  char *slave_error = NULL ;
 
 	  /* Must have a request at this stage.... */
 	  zMapAssert(request) ;
 
 	  /* this needs changing according to request.... */
-	  ZMAP_THR_DEBUG(("%x: servicing request....\n", connection->thread_id)) ;
+	  ZMAPTHREAD_DEBUG(("%lu: servicing request....\n", thread->thread_id)) ;
 
-	  server_response = zMapServerRequest(thread_cb->server, (ZMapProtocolAny)request) ;
 
-	  switch (server_response)
+	  /* Call the registered slave handler function. */
+	  slave_response = (*(thread->handler_func))(&(thread_cb->slave_data), request, &reply,
+						     &slave_error) ;
+
+	  /* The handling below is not now correct, if a call fails we need to kill the thread
+	   * we can't cope with dangling threads....we will need to set the thread_died flag */
+
+	  switch (slave_response)
 	    {
-	    case ZMAP_SERVERRESPONSE_OK:
+	    case ZMAPTHREAD_RETURNCODE_OK:
 	      {
-		ZMAP_THR_DEBUG(("%x: got all data....\n", connection->thread_id)) ;
+		ZMAPTHREAD_DEBUG(("%lu: got all data....\n", thread->thread_id)) ;
 
 		/* Signal that we got some data. */
-		zmapVarSetValueWithData(&(connection->reply), ZMAP_REPLY_GOTDATA, request) ;
+		zmapVarSetValueWithData(&(thread->reply), ZMAPTHREAD_REPLY_GOTDATA, request) ;
 		
 		request = NULL ;			    /* Reset, we don't free this data. */
 		break ;
 	      }
-	    case ZMAP_SERVERRESPONSE_TIMEDOUT:
-	    case ZMAP_SERVERRESPONSE_REQFAIL:
-	    case ZMAP_SERVERRESPONSE_BADREQ:
+	    case ZMAPTHREAD_RETURNCODE_TIMEDOUT:
 	      {
 		char *error_msg ;
 
-		ZMAP_THR_DEBUG(("%x: request failed....\n", connection->thread_id)) ;
+		ZMAPTHREAD_DEBUG(("%lu: request failed....\n", thread->thread_id)) ;
 
-		error_msg = g_strdup_printf("%s - %s", ZMAPSLAVE_CONNREQUEST,
-					    zMapServerLastErrorMsg(thread_cb->server)) ;
+		error_msg = g_strdup_printf("%s - %s", ZMAPTHREAD_SLAVEREQUEST, slave_error) ;
 
 		/* Signal that we failed. */
-		zmapVarSetValueWithError(&(connection->reply), ZMAP_REPLY_REQERROR, error_msg) ;
+		zmapVarSetValueWithError(&(thread->reply), ZMAPTHREAD_REPLY_REQERROR, error_msg) ;
 
 		break ;
 	      }
-	    case ZMAP_SERVERRESPONSE_SERVERDIED:
+	    case ZMAPTHREAD_RETURNCODE_REQFAIL:
+	    case ZMAPTHREAD_RETURNCODE_BADREQ:
+	    case ZMAPTHREAD_RETURNCODE_SERVERDIED:
 	      {
 		char *error_msg ;
 
-		ZMAP_THR_DEBUG(("%x: server died....\n", connection->thread_id)) ;
 
-		error_msg = g_strdup_printf("%s - %s", ZMAPSLAVE_CONNREQUEST,
-					    zMapServerLastErrorMsg(thread_cb->server)) ;
+#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
+		connection->initial_request = NULL ;	    /* make sure there is explicitly no
+							       data returned. */
+#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
+
+
+
+		ZMAPTHREAD_DEBUG(("%lu: server died....\n", thread->thread_id)) ;
+
+		error_msg = g_strdup_printf("%s - %s", ZMAPTHREAD_SLAVEREQUEST, slave_error) ;
+
+#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
+		/* not sure we need to do this...happens in the cleanup routine.... */
 
 		/* Signal that we failed. */
-		zmapVarSetValueWithError(&(connection->reply), ZMAP_REPLY_DIED, error_msg) ;
+		zmapVarSetValueWithError(&(thread->reply), ZMAPTHREAD_REPLY_DIED, error_msg) ;
+#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
+
 
 		thread_cb->thread_died = TRUE ;
 
-		thread_cb->initial_error = g_strdup_printf("%s - %s", ZMAPSLAVE_CONNREQUEST,
-							   zMapServerLastErrorMsg(thread_cb->server)) ;
+		thread_cb->initial_error = g_strdup_printf("%s - %s", ZMAPTHREAD_SLAVEREQUEST,
+							   error_msg) ;
 
 		goto clean_up ;
 	      }
@@ -314,7 +222,7 @@ void *zmapNewThread(void *thread_args)
   pthread_cleanup_pop(1) ;				    /* 1 => always call clean up routine */
 
 
-  ZMAP_THR_DEBUG(("%x: main thread routine exitting....\n", connection->thread_id)) ;
+  ZMAPTHREAD_DEBUG(("%lu: main thread routine exitting....\n", thread->thread_id)) ;
 
 
   return thread_args ;
@@ -328,115 +236,65 @@ void *zmapNewThread(void *thread_args)
  *
  * This means some care is needed in handling resources to be released, we may need to set flags
  * to remember which resources have been allocated.
- * In particular we DO NOT free the thread_cb->connection, this is done by the GUI thread
+ * In particular we DO NOT free the thread_cb->thread, this is done by the GUI thread
  * which created it.
  */
 static void cleanUpThread(void *thread_args)
 {
   zmapThreadCB thread_cb = (zmapThreadCB)thread_args ;
-  ZMapConnection connection = thread_cb->connection ;
+  ZMapThread thread = thread_cb->thread ;
   ZMapThreadReply reply ;
   gchar *error_msg = NULL ;
 
-  ZMAP_THR_DEBUG(("%x: thread clean-up routine starting....\n", connection->thread_id)) ;
+  zMapAssert(thread_args) ;
+
+  ZMAPTHREAD_DEBUG(("%lu: thread clean-up routine starting....\n", thread->thread_id)) ;
 
   if (thread_cb->thread_died)
     {
-      reply = ZMAP_REPLY_DIED ;
+      reply = ZMAPTHREAD_REPLY_DIED ;
       if (thread_cb->initial_error)
 	error_msg = g_strdup(thread_cb->initial_error) ;
     }
   else
-    reply = ZMAP_REPLY_CANCELLED ;
+    reply = ZMAPTHREAD_REPLY_CANCELLED ;
 
+#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
   if (thread_cb->server)
     {
+      /* this suggests we may need a "kill" function registered for a slave.... */
+      /* It also suggests that perhaps we need something like an acedb handle...to just
+       * chuck away all allocated resoures... */
+
       if (zMapServerCloseConnection(thread_cb->server)
 	  && zMapServerFreeConnection(thread_cb->server))
 	{
 	  thread_cb->server = NULL ;
 	}
       else
-	ZMAP_THR_DEBUG(("%x: Unable to close connection to server cleanly\n",
+	ZMAPTHREAD_DEBUG(("%lu: Unable to close connection to server cleanly\n",
 			connection->thread_id)) ;
+
+
 
       /* this should be an error message now as well as a debug message..... */
     }
+#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
 
 
   g_free(thread_cb) ;
 
   if (!error_msg)
-    zmapVarSetValue(&(connection->reply), reply) ;
+    zmapVarSetValue(&(thread->reply), reply) ;
   else
-    zmapVarSetValueWithError(&(connection->reply), reply, error_msg) ;
+    zmapVarSetValueWithError(&(thread->reply), reply, error_msg) ;
 
 
-  ZMAP_THR_DEBUG(("%x: thread clean-up routine exitting because %s....\n",
-		  connection->thread_id,
-		  zMapVarGetReplyString(reply))) ;
+  ZMAPTHREAD_DEBUG(("%lu: thread clean-up routine exitting because %s....\n",
+		    thread->thread_id, zMapThreadGetReplyString(reply))) ;
 
   return ;
 }
 
 
 
-/* Static/global list of protocols and whether their global init/cleanup functions have been
- * called. These are functions that must only be called once. */
-static void protocolGlobalInitFunc(ZMapProtocolInitList protocols, char *protocol,
-				   void **global_init_data_out)
-{
-  int status ;
-  GList *curr_ptr ;
-  ZMapProtocolInit init ;
-
-  if ((status = pthread_mutex_lock(&(protocols->mutex))) != 0)
-    {
-      zMapLogFatalSysErr(status, "%s", "protocolGlobalInitFunc() mutex lock") ;
-    }
-
-  /* If we don't find the protocol in the list then add it, initialised to FALSE. */
-  if (!(curr_ptr = g_list_find_custom(protocols->protocol_list, protocol, findProtocol)))
-    {
-      init = (ZMapProtocolInit)g_new0(ZMapProtocolInitStruct, 1) ;
-      init->protocol = g_strdup(protocol) ;
-      init->init_called = init->cleanup_called = FALSE ;
-      init->global_init_data = NULL ;
-
-      protocols->protocol_list = g_list_prepend(protocols->protocol_list, init) ;
-    }
-  else
-    init = curr_ptr->data ;
-
-  /* Call the init routine if its not been called yet and either way return the global_init_data. */
-  if (!init->init_called)
-    {
-      if (!zMapServerGlobalInit(protocol, &(init->global_init_data)))
- 	zMapLogFatal("Initialisation call for %s protocol failed.", protocol) ;
-
-      init->init_called = TRUE ;
-    }
-
-  *global_init_data_out = init->global_init_data ;
-
-  if ((status = pthread_mutex_unlock(&(protocols->mutex))) != 0)
-    {
-      zMapLogFatalSysErr(status, "%s", "protocolGlobalInitFunc() mutex unlock") ;
-    }
-
-  return ;
-}
-
-/* Compare a protocol in the list with the supplied one. Just a straight string
- * comparison currently. */
-static int findProtocol(gconstpointer list_data, gconstpointer custom_data)
-{
-  int result ;
-  char *list_protocol = ((ZMapProtocolInit)list_data)->protocol, *protocol = (char *)custom_data ;
-
-  result = strcasecmp(list_protocol, protocol) ;
-
-  return result ;
-}
-  
- 
