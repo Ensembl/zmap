95996437d4692c11e4c33f81ff9998bbdeba9440
edgrif <edgrif>
2005-04-05 14:50:20 +0000

    make this work simply off a foocanvas item, this routine still needs a lot of work.

--- src/zmapWindow/zmapWindowList.c
+++ src/zmapWindow/zmapWindowList.c
@@ -1,4 +1,4 @@
-/*  File: zmapList.c
+/*  File: zmapWindowList.c
  *  Author: Rob Clack (rnc@sanger.ac.uk)
  *  Copyright (c) Sanger Institute, 2004
  *-------------------------------------------------------------------
@@ -27,18 +27,24 @@
  *              
  * Exported functions: 
  * HISTORY:
- * Last edited: Jan 20 15:09 2005 (edgrif)
+ * Last edited: Mar 31 14:53 2005 (edgrif)
  * Created: Thu Sep 16 10:17 2004 (rnc)
- * CVS info:   $Id: zmapWindowList.c,v 1.24 2005-01-24 11:45:54 edgrif Exp $
+ * CVS info:   $Id: zmapWindowList.c,v 1.25 2005-04-05 14:50:20 edgrif Exp $
  *-------------------------------------------------------------------
  */
-
 
 #include <glib.h>
 #include <zmapWindow_P.h>
 #include <ZMap/zmapUtils.h>
 
-enum { NAME, START, END, ID, TYPE, FEATURE_TYPE, X_COORD, N_COLUMNS };
+
+
+/* I have changed this function quite a lot and it could do with more tidying up still,
+ * there is much that is not very tidy about it at all. EG */
+
+
+
+enum { NAME, START, END, ID, TYPE, FEATURE_TYPE, X_COORD, FEATURE_ITEM, N_COLUMNS };
 
 
 /* ListColStruct structure declaration
@@ -47,13 +53,13 @@ enum { NAME, START, END, ID, TYPE, FEATURE_TYPE, X_COORD, N_COLUMNS };
  * of feature details through the g_datalist_foreach() call 
  * and into the addItemToList() function where the list is loaded.
  */
-typedef struct _ListColStruct {
-  ZMapStrand    strand;                      /*!< up or down strand (enum). */
-  double        x_coord;                     /*!< X coordinate of selected column. */
-  GtkTreeStore *list;                        /*!< list of features in selected column. */
-  GString      *source;                      /*!< type/source/method */
-} ListColStruct,
-  *ListCol;                                  /*!< pointer to ListColStruct. */
+typedef struct
+{
+  ZMapWindow window ;
+  ZMapStrand    strand;					    /*!< up or down strand (enum). */
+  double        x_coord;				    /*!< X coordinate of selected column. */
+  GtkTreeStore *list;					    /*!< list of features in selected column. */
+} ListColStruct, *ListCol ;				    /*!< pointer to ListColStruct. */
 
 
 
@@ -65,25 +71,37 @@ static void quitListCB                (GtkWidget *window, gpointer data);
 
 
 /* functions *************************************************************/
-/** \Brief Displays a list of selectable features
+/* Displays a list of selectable features
  *
  * All the features the chosen column are displayed in ascending start-coordinate
  * sequence.  When the user selects one, the main display is scrolled to that feature
  * and the selected item hightlighted.
  */
 
-void zMapWindowCreateListWindow(ZMapWindow zmapWindow, ZMapFeatureItem featureItem)
+
+void zMapWindowCreateListWindow(ZMapWindow zmapWindow, FooCanvasItem *item)
 {
   GtkWidget *window, *featureList, *scrolledWindow;
   GtkTreeModel *sort_model;
   GtkCellRenderer *renderer;
   GtkTreeViewColumn *column;
   GtkTreeSelection *select;
-  ListCol listCol = g_new0(ListColStruct, 1);
-  ZMapFeatureTypeStyle thisType;
+  ListCol listCol ;
+  ZMapFeature feature ;
   double x1, y1, x2, y2;
+  GData *feature_sets ;
 
-  foo_canvas_item_get_bounds(featureItem->canvasItem, &x1, &y1, &x2, &y2); /* world coords */
+
+  listCol = g_new0(ListColStruct, 1) ;
+
+  listCol->window = zmapWindow ;
+
+  /* Get hold of the feature corresponding to this item. */
+  feature = g_object_get_data(G_OBJECT(item), "feature");  
+  zMapAssert(feature) ;
+
+
+  foo_canvas_item_get_bounds(item, &x1, &y1, &x2, &y2);	    /* world coords */
 
   listCol->list = gtk_tree_store_new(N_COLUMNS,
 				     G_TYPE_STRING,
@@ -92,19 +110,16 @@ void zMapWindowCreateListWindow(ZMapWindow zmapWindow, ZMapFeatureItem featureIt
 				     G_TYPE_INT,
 				     G_TYPE_STRING,
 				     G_TYPE_INT,
-				     G_TYPE_DOUBLE);
+				     G_TYPE_DOUBLE,
+				     G_TYPE_POINTER) ;
 
+  /* I have no idea why this x coord is kept.... */
   listCol->x_coord = x1;
-  listCol->source = g_string_new(featureItem->feature_set->source);
-  listCol->source = g_string_ascii_down(listCol->source); /* does this in place */
-
-  thisType = (ZMapFeatureTypeStyle)g_datalist_get_data(&(zmapWindow->types), 
-						       listCol->source->str);
-  zMapAssert(thisType);
 
   /* need to know whether the column is up or down strand, 
    * so we load the right set of features */
-  listCol->strand = featureItem->strand;
+  listCol->strand = feature->strand ;
+
 
   /* set up the top level window */
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -112,7 +127,7 @@ void zMapWindowCreateListWindow(ZMapWindow zmapWindow, ZMapFeatureItem featureIt
   g_ptr_array_add(zmapWindow->featureListWindows, (gpointer)window);
 
   gtk_container_border_width(GTK_CONTAINER(window), 5) ;
-  gtk_window_set_title(GTK_WINDOW(window), featureItem->feature_set->source) ;
+  gtk_window_set_title(GTK_WINDOW(window), g_quark_to_string(feature->style)) ;
   gtk_window_set_default_size(GTK_WINDOW(window), -1, 600); 
 
   scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
@@ -121,7 +136,8 @@ void zMapWindowCreateListWindow(ZMapWindow zmapWindow, ZMapFeatureItem featureIt
 				 GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
   
   /* Build and populate the list of features */
-  g_datalist_foreach(&(featureItem->feature_set->features),addItemToList , listCol) ;
+  feature_sets = zMapFeatureFindSetInContext(zmapWindow->feature_context, feature->style) ;
+  g_datalist_foreach(&feature_sets, addItemToList, listCol) ;
 
   sort_model = gtk_tree_model_sort_new_with_model (GTK_TREE_MODEL(listCol->list));
 
@@ -134,8 +150,8 @@ void zMapWindowCreateListWindow(ZMapWindow zmapWindow, ZMapFeatureItem featureIt
 
   /* finished with list now */
   g_object_unref(G_OBJECT(listCol->list));
-  g_string_free(listCol->source, FALSE);
-  g_free(listCol);
+  g_free(listCol) ;
+
 
   /* render the columns. */
   renderer = gtk_cell_renderer_text_new ();
@@ -191,6 +207,14 @@ void zMapWindowCreateListWindow(ZMapWindow zmapWindow, ZMapFeatureItem featureIt
 
   gtk_tree_view_column_set_visible(column, FALSE);
    
+  column = gtk_tree_view_column_new_with_attributes ("Feature Item",
+						     renderer,
+						     "text", FEATURE_ITEM,
+						     NULL);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (featureList), column);
+
+  gtk_tree_view_column_set_visible(column, FALSE);
+   
   /* Setup the selection handler */
   select = gtk_tree_view_get_selection (GTK_TREE_VIEW (featureList));
   gtk_tree_selection_set_mode (select, GTK_SELECTION_SINGLE);
@@ -202,7 +226,9 @@ void zMapWindowCreateListWindow(ZMapWindow zmapWindow, ZMapFeatureItem featureIt
   g_signal_connect(GTK_OBJECT(window), "destroy",
 		   GTK_SIGNAL_FUNC(quitListCB), featureList);
 
+
   gtk_container_add(GTK_CONTAINER(scrolledWindow), featureList);
+
 
   gtk_widget_show_all(window);
 
@@ -251,18 +277,26 @@ static void addItemToList(GQuark key_id, gpointer data, gpointer user_data)
 
   if (listCol->strand == feature->strand)
     {
+      FooCanvasItem *item ;
+
+      item = zmapWindowFToIFindItem(listCol->window->feature_to_item,
+				    feature->style, feature->unique_id) ;
+      zMapAssert(item) ;
+
       gtk_tree_store_append(GTK_TREE_STORE(listCol->list), &iter1, NULL);
-      gtk_tree_store_set   (GTK_TREE_STORE(listCol->list), &iter1, 
-			    NAME , feature->name,
-			    START, feature->x1,
-			    END  , feature->x2,
-			    ID   , key_id,
-			    TYPE , listCol->source->str,
-			    FEATURE_TYPE, feature->type,
-			    X_COORD, listCol->x_coord,
-			    -1 );
+      gtk_tree_store_set(GTK_TREE_STORE(listCol->list), &iter1, 
+			 NAME, (char *)g_quark_to_string(feature->original_id),
+			 START, feature->x1,
+			 END, feature->x2,
+			 ID, key_id,
+			 TYPE, g_quark_to_string(feature->style),
+			 FEATURE_TYPE, feature->type,
+			 X_COORD, listCol->x_coord,
+			 FEATURE_ITEM, item,
+			 -1) ;
     }
-  return;
+
+  return ;
 }
 
 
@@ -281,36 +315,28 @@ static int tree_selection_changed_cb (GtkTreeSelection *selection, gpointer data
   double x_coord;
   gchar *name, *type;
   GQuark id;
-
-  /* At present, when we display the list, it will scroll the main display
-  ** to the first feature on the list.  This is not good but it's not my
-  ** top priority right now. */
+  FooCanvasItem *item ;
 
   /* retrieve user's selection */
   if (gtk_tree_selection_get_selected (selection, &model, &iter))
     {
       gtk_tree_model_get (model, &iter, NAME, &name, START, &start, END, &end, 
 			  ID, &id, TYPE, &type, FEATURE_TYPE, &feature_type, 
-			  X_COORD, &x_coord, -1);
+			  X_COORD, &x_coord, FEATURE_ITEM, &item, -1) ;
     }
 
-  if (start)
-    {
-      if (zMapWindowScrollToItem(window, type, id))
-	gtk_widget_show_all(GTK_WIDGET(window->parent_widget));
-      else
-	zMapShowMsg(ZMAP_MSG_CRASH, "Quark %d of type %s not found in list of known features\n", id, type);
-    }
-  
-  if (name) g_free(name); 
-  if (type) g_free(type);
+  zMapWindowScrollToItem(window, item) ;
     
-  return TRUE;
+  return TRUE ;
 }
 
 
 static void quitListCB(GtkWidget *window, gpointer data)
 {
+
+  /* YES AND WHAT ABOUT THE ENTRY IN THE ARRAY OF THESE WINDOWS ROB ?????????
+   * WHEN DOES THAT GO AWAY....?????? */
+
   gtk_widget_destroy(GTK_WIDGET(window));
 
   return;
