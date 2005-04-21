164c15a937c7af4152005855f7b94a8d0a37bbca
rds <rds>
2005-04-21 13:34:37 +0000

    build the module

--- src/perl/build.sh
+++ src/perl/build.sh
@@ -11,11 +11,11 @@ ZMAP_INC="$SRC/include"
 ZMAP_LIB="$SRC/build/alpha/lib"
 #==========================================
 # chose a perl version
-PERL=/usr/local/bin/perl
-
+    PERL_EXE=/usr/local/bin/perl
+   PERL_ARCH=`$PERL_EXE -MConfig -e 'print $Config{archname}'`
+PERL_VERSION=`$PERL_EXE -MConfig -e 'print $Config{version}'`
 #==========================================
 #==========================================
-
 
 cd X11-XRemote-0.01/X11/XRemote
 
@@ -24,8 +24,10 @@ if [ $1 ]; then
 else
     PREFIX=`pwd`/PREFIX
 fi
+ADD_PERL5LIB=$PREFIX/lib/perl5/site_perl/$PERL_VERSION/$PERL_ARCH:\
+$PREFIX/lib/site_perl/$PERL_VERSION/$PERL_ARCH
 
-$PERL Makefile.PL PREFIX=$PREFIX \
+$PERL_EXE Makefile.PL PREFIX=$PREFIX \
 --with-zmap-inc "-I$ZMAP_INC" \
 --with-zmap-libs "-L$ZMAP_LIB" \
 --with-symbols 
@@ -33,9 +35,13 @@ $PERL Makefile.PL PREFIX=$PREFIX \
 make
 make install
 
-PERL5LIB=$PREFIX:$PERL5LIB
+PERL5LIB=$ADD_PERL5LIB:$PERL5LIB
 export PERL5LIB
-echo "A nice Tk window should appear. If not something failed."
-./tk_test.pl
-echo "close the Tk window please..."
-echo "Make sure $PREFIX is in your PERL5LIB"
+
+echo 
+echo "Testing installation..."
+echo "=================================="
+./debug_test.pl
+echo "=================================="
+echo
+echo "Make sure $ADD_PERL5LIB is in your PERL5LIB"
