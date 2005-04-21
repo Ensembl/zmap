b046814e9f4380546d8a3029fa3ca10a51d71220
rds <rds>
2005-04-21 13:38:45 +0000

    this file should not be here, but for now... I've changed it not to have use lib statements...

--- src/perl/X11-XRemote-0.01/X11/XRemote/tk_test.pl
+++ src/perl/X11-XRemote-0.01/X11/XRemote/tk_test.pl
@@ -1,17 +1,7 @@
 #!/usr/local/bin/perl -w
 
-#!/nfs/team71/analysis/rds/tmp/DEBUG_PERL/bin/perl -w
-
-
-
-
 use strict;
-use lib '/nfs/team71/analysis/rds/workspace/tk';
-use lib './PREFIX/lib/site_perl/5.6.1/alpha-dec_osf';
-#use lib './PREFIX/lib/perl5/site_perl/5.8.0/alpha-dec_osf-thread-multi-ld';
-#use lib './PREFIX/lib/perl5/site_perl/5.8.6/alpha-dec_osf-thread-multi';
-
-# make a TK window
+use warnings;
 use CanvasWindow::MainWindow;
 use CanvasWindow::XRemoteTest;
 
