0f66d0e6770ad0e25de6237954a1a2ccec2b306b
acedb <acedb>
2005-07-19 09:52:04 +0000

    add build scripts

---
+++ scripts/setup_libs_otter.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+base=/nfs/disk100/acedb/ZMap/prefix
+script=`basename $0`
+
+OSTYPE=`uname`
+if [ "$OSTYPE" = 'OSF1' ]; then
+    osbin='ALPHA/bin'
+    LD_LIBRARY_PATH="$base/ALPHA/lib:/usr/local/gtk2/lib"
+    myBuildDir=/nfs/team71/analysis/rds/workspace/ZMap/src/build/alpha
+    myAceDir=/nfs/team71/acedb/edgrif/acedb/CODE/acedb/bin.ALPHA_5
+#/nfs/disk100/acedb/RELEASE.SUPPORTED/bin.ALPHA_5
+    export LD_LIBRARY_PATH
+    echo $LD_LIBRARY_PATH
+elif [ "$OSTYPE" = 'Linux' ]; then
+    osbin='LINUX/bin'
+    myBuildDir=/nfs/team71/analysis/rds/workspace/ZMap/src/build/linux
+    myAceDir=/nfs/disk100/acedb/RELEASE.SUPPORTED/bin.LINUX_4
+else
+    echo "Unknown OSTYPE '$OSTYPE'"
+fi
+
+if [ -e $myBuildDir ]; then
+   PATH="$myBuildDir:$myAceDir:$PATH"
+   echo "I've set path to: \n $PATH"
+fi
+
+      PERL_EXE=/usr/local/bin/perl
+     PERL_ARCH=`$PERL_EXE -MConfig -e 'print $Config{archname}'`
+  PERL_VERSION=`$PERL_EXE -MConfig -e 'print $Config{version}'`
+
+OTTER_HOME=$base/otter
+export OTTER_HOME
+
+PERL5LIB=\
+$OTTER_HOME/tk:\
+$OTTER_HOME/PerlModules:\
+$OTTER_HOME/ensembl-ace:\
+$OTTER_HOME/ensembl-otter/modules:\
+$OTTER_HOME/ensembl-pipeline/modules:\
+$OTTER_HOME/ensembl/modules:\
+$OTTER_HOME/bioperl-0.7.2:\
+$OTTER_HOME/bioperl-1.2:\
+$base/CPAN/lib/perl5/site_perl/$PERL_VERSION/$PERL_ARCH:\
+$base/CPAN/lib/perl5/site_perl/$PERL_VERSION:\
+$base/CPAN/lib/perl5/$PERL_VERSION
+export PERL5LIB
+
+export PATH
+
+exec $OTTER_HOME/tk/$script "$@"
