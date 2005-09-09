fec08731691fad631c39564aaeaf997beb564ea5
rds <rds>
2005-09-09 11:06:41 +0000

    remove glib 2.4 function

--- src/zmapXML/zmapXMLElement.c
+++ src/zmapXML/zmapXMLElement.c
@@ -27,9 +27,9 @@
  *
  * Exported functions: See XXXXXXXXXXXXX.h
  * HISTORY:
- * Last edited: Sep  8 12:09 2005 (rds)
+ * Last edited: Sep  9 12:06 2005 (rds)
  * Created: Fri Aug  5 14:33:49 2005 (rds)
- * CVS info:   $Id: zmapXMLElement.c,v 1.2 2005-09-08 17:49:52 rds Exp $
+ * CVS info:   $Id: zmapXMLElement.c,v 1.3 2005-09-09 11:06:41 rds Exp $
  *-------------------------------------------------------------------
  */
 
@@ -129,7 +129,7 @@ zmapXMLElement zMapXMLElement_getChildByPath(zmapXMLElement parent,
   zmapXMLElement ele = NULL, pele = NULL;
   gchar **names = NULL;
   gboolean exist = TRUE;
-  names = g_strsplit_set(path, ".", -1);
+  names = g_strsplit(path, ".", -1);
   int i = 0;
   pele = parent;
   while(exist)
