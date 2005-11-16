7946edecb3bce734cfc2a6e39a1159b80cdc628c
rds <rds>
2005-11-16 10:37:10 +0000

    modified / renamed g_list_foreach_reverse
    - my_g_list_foreach_reverse -> zMap_g_list_foreach_directional
    - no longer g_list_last so works from current list member.
    - takes extra argument to control the direction (FORWARD/REVERSE)!

--- src/zmapUtils/zmapGLibUtils.c
+++ src/zmapUtils/zmapGLibUtils.c
@@ -26,20 +26,35 @@
  *
  * Exported functions: See ZMap/zmapGLibUtils.h
  * HISTORY:
- * Last edited: Oct 13 15:27 2005 (edgrif)
+ * Last edited: Nov 14 11:48 2005 (rds)
  * Created: Thu Oct 13 15:22:35 2005 (edgrif)
- * CVS info:   $Id: zmapGLibUtils.c,v 1.1 2005-11-03 16:14:29 edgrif Exp $
+ * CVS info:   $Id: zmapGLibUtils.c,v 1.2 2005-11-16 10:37:10 rds Exp $
  *-------------------------------------------------------------------
  */
 
 #include <ZMap/zmapGLibUtils.h>
 
-
+#ifdef UTILISE_SINGLE_FUNCTION
 /* Go through a list in reverse calling the supplied GFunc.
  * This was lifted from the glib code of g_list. */
-void my_g_list_foreach_reverse(GList *list, GFunc func, gpointer user_data)
+
+/* changed this slightly to be the g_list_foreach_directional
+ * function, as it was working differently to g_list_foreach, by
+ * the addition of the g_list_last.  g_list_foreach doesn't do a
+ * g_list_first, but then it "shouldn't" need to as the all their
+ * function return the first item of the list again. Here we need to 
+ * get to the end of the list to do the reverse! but the only place
+ * it's getting called from at the moment is zmapWindowDrawFeatures.c 
+ * positionColumns, where the list is the item list of the
+ * foocanvasgroup.  the group holds a pointer to the end of the list, 
+ * so we can use that instead and the other function below... 
+ * However, in the dna highlighting it's better not to go all the way 
+ * to the end of the list... Maybe we need 2 functions as it is nice
+ * to have the g_list_last encapsulated in reverse function...
+ */
+void zMap_g_list_foreach_reverse(GList *list, GFunc func, gpointer user_data)
 {
-  list = g_list_last(list) ;
+  list = g_list_last(list) ;    
 
   while (list)
     {
@@ -51,3 +66,19 @@ void my_g_list_foreach_reverse(GList *list, GFunc func, gpointer user_data)
 
   return ;
 }
+#endif
+
+void zMap_g_list_foreach_directional(GList   *list, 
+                                     GFunc    func,
+                                     gpointer user_data,
+                                     ZMapGListDirection forward)
+{
+  while (list)
+    {
+      GList *next = (forward == ZMAP_GLIST_FORWARD ? list->next : list->prev);
+      (*func) (list->data, user_data);
+      list = next;
+    }
+  return ;
+}
+
