d9c42f52492186ee026f1e915ad9ef5deed567ea
edgrif <edgrif>
2005-11-18 11:06:28 +0000

    Fix formatScore func, fix code to have only 3 feature types.

--- src/zmapFeature/zmapFeatureFormatInput.c
+++ src/zmapFeature/zmapFeatureFormatInput.c
@@ -20,22 +20,23 @@
  * This file is part of the ZMap genome database package
  * originated by
  * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
- *      Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk,
- *      Rob Clack (Sanger Institute, UK) rnc@sanger.ac.uk
+ *      Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk
  *
- * Description: 
+ * Description: Routines to handle converting from Feature to Type and
+ *              vice-versa.
  *
- * Exported functions: See XXXXXXXXXXXXX.h
+ * Exported functions: See ZMap/zmapFeature.h
  * HISTORY:
- * Last edited: Sep 27 16:56 2005 (edgrif)
+ * Last edited: Nov 17 10:16 2005 (edgrif)
  * Created: Thu Sep 15 12:01:30 2005 (rds)
- * CVS info:   $Id: zmapFeatureFormatInput.c,v 1.2 2005-09-30 07:29:01 edgrif Exp $
+ * CVS info:   $Id: zmapFeatureFormatInput.c,v 1.3 2005-11-18 11:06:28 edgrif Exp $
  *-------------------------------------------------------------------
  */
 
 #include <string.h>
 #include <ZMap/zmapUtils.h>
 #include <zmapFeature_P.h>
+
 
 /* Type must equal something that the code understands. In GFF v1 this is unregulated and
  * could be anything. By default unrecognised terms are not converted.
@@ -59,57 +60,30 @@ gboolean zMapFeatureFormatType(gboolean SO_compliant, gboolean default_to_basic,
 
 
   /* Is feature_type a SO term, note that I do case-independent compares, hope this is correct. */
-  if (g_ascii_strcasecmp(feature_type, "trans_splice_acceptor_site") == 0)
-    {
-      type = ZMAPFEATURE_BOUNDARY ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "transposable_element_insertion_site") == 0
-	   || g_ascii_strcasecmp(feature_type, "deletion") == 0)
-    {
-      type = ZMAPFEATURE_VARIATION ;
-    }
-  if (g_ascii_strcasecmp(feature_type, "region") == 0)
-    {
-      type = ZMAPFEATURE_BASIC ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "virtual_sequence") == 0)
-    {
+
+#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
+  /* I don't know what this is about, how to represent it or anything........... */
+  /*  else if (g_ascii_strcasecmp(feature_type, "virtual_sequence") == 0)
+      {
       type = ZMAPFEATURE_SEQUENCE ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "reagent") == 0
-	   || g_ascii_strcasecmp(feature_type, "oligo") == 0
-	   || g_ascii_strcasecmp(feature_type, "PCR_product") == 0
-	   || g_ascii_strcasecmp(feature_type, "RNAi_reagent") == 0
-	   || g_ascii_strcasecmp(feature_type, "clone") == 0
-	   || g_ascii_strcasecmp(feature_type, "clone_end") == 0
-	   || g_ascii_strcasecmp(feature_type, "clone_end") == 0)
+      }
+      else */
+#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
+
+  /* There are usually many more alignments/exons than anything else in a GFF dump so do them first. */
+  if (g_ascii_strcasecmp(feature_type, "nucleotide_match") == 0
+      || g_ascii_strcasecmp(feature_type, "expressed_sequence_match") == 0
+      || g_ascii_strcasecmp(feature_type, "EST_match") == 0
+      || g_ascii_strcasecmp(feature_type, "cDNA_match") == 0
+      || g_ascii_strcasecmp(feature_type, "translated_nucleotide_match") == 0
+      || g_ascii_strcasecmp(feature_type, "protein_match") == 0)
     {
-      type = ZMAPFEATURE_BASIC ;
+      type = ZMAPFEATURE_ALIGNMENT ;
     }
-  else if (g_ascii_strcasecmp(feature_type, "UTR") == 0
-	   || g_ascii_strcasecmp(feature_type, "polyA_signal_sequence") == 0
-	   || g_ascii_strcasecmp(feature_type, "polyA_site") == 0)
-    {
-      /* these should in the end be part of a gene structure..... */
-      type = ZMAPFEATURE_BASIC ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "operon") == 0)
-    {
-      /* this should in the end be part of a gene group, not sure how we display that..... */
-      type = ZMAPFEATURE_BASIC ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "pseudogene") == 0)
-    {
-      /* In SO terms this is a region but we don't have a basic "region" type that includes
-       * exons like structure...suggests we need to remodel our feature struct.... */
-      type = ZMAPFEATURE_TRANSCRIPT ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "experimental_result_region") == 0
-	   || g_ascii_strcasecmp(feature_type, "chromosomal_structural_element") == 0)
-    {
-      type = ZMAPFEATURE_BASIC ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "transcript") == 0
+  else if (g_ascii_strcasecmp(feature_type, "exon") == 0
+	   || g_ascii_strcasecmp(feature_type, "intron") == 0
+	   ||g_ascii_strcasecmp(feature_type, "pseudogene") == 0
+	   || g_ascii_strcasecmp(feature_type, "transcript") == 0
 	   || g_ascii_strcasecmp(feature_type, "protein_coding_primary_transcript") == 0
 	   || g_ascii_strcasecmp(feature_type, "CDS") == 0
 	   || g_ascii_strcasecmp(feature_type, "mRNA") == 0
@@ -117,51 +91,65 @@ gboolean zMapFeatureFormatType(gboolean SO_compliant, gboolean default_to_basic,
     {
       type = ZMAPFEATURE_TRANSCRIPT ;
     }
-  else if (g_ascii_strcasecmp(feature_type, "exon") == 0)
-    {
-      type = ZMAPFEATURE_EXON ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "intron") == 0)
-    {
-      type = ZMAPFEATURE_INTRON ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "nucleotide_match") == 0
-	   || g_ascii_strcasecmp(feature_type, "expressed_sequence_match") == 0
-	   || g_ascii_strcasecmp(feature_type, "EST_match") == 0
-	   || g_ascii_strcasecmp(feature_type, "cDNA_match") == 0
-	   || g_ascii_strcasecmp(feature_type, "translated_nucleotide_match") == 0
-	   || g_ascii_strcasecmp(feature_type, "protein_match") == 0)
-    {
-      type = ZMAPFEATURE_HOMOL ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "repeat_region") == 0
+  else if (g_ascii_strcasecmp(feature_type, "reagent") == 0
+	   || g_ascii_strcasecmp(feature_type, "oligo") == 0
+	   || g_ascii_strcasecmp(feature_type, "PCR_product") == 0
+	   || g_ascii_strcasecmp(feature_type, "RNAi_reagent") == 0
+	   || g_ascii_strcasecmp(feature_type, "clone") == 0
+	   || g_ascii_strcasecmp(feature_type, "clone_end") == 0
+	   || g_ascii_strcasecmp(feature_type, "clone_end") == 0
+	   || g_ascii_strcasecmp(feature_type, "trans_splice_acceptor_site") == 0
+	   || g_ascii_strcasecmp(feature_type, "transposable_element_insertion_site") == 0
+	   || g_ascii_strcasecmp(feature_type, "deletion") == 0
+	   || g_ascii_strcasecmp(feature_type, "region") == 0
+	   || g_ascii_strcasecmp(feature_type, "UTR") == 0
+	   || g_ascii_strcasecmp(feature_type, "polyA_signal_sequence") == 0
+	   || g_ascii_strcasecmp(feature_type, "polyA_site") == 0
+	   || g_ascii_strcasecmp(feature_type, "operon") == 0
+	   || g_ascii_strcasecmp(feature_type, "experimental_result_region") == 0
+	   || g_ascii_strcasecmp(feature_type, "chromosomal_structural_element") == 0
+	   || g_ascii_strcasecmp(feature_type, "repeat_region") == 0
 	   || g_ascii_strcasecmp(feature_type, "inverted_repeat") == 0
 	   || g_ascii_strcasecmp(feature_type, "tandem_repeat") == 0
-	   || g_ascii_strcasecmp(feature_type, "transposable_element") == 0)
-    {
-      type = ZMAPFEATURE_BASIC ;
-    }
-  else if (g_ascii_strcasecmp(feature_type, "SNP") == 0
+	   || g_ascii_strcasecmp(feature_type, "transposable_element") == 0
+	   || g_ascii_strcasecmp(feature_type, "SNP") == 0
 	   || g_ascii_strcasecmp(feature_type, "sequence_variant") == 0
 	   || g_ascii_strcasecmp(feature_type, "substitution") == 0)
     {
-      type = ZMAPFEATURE_VARIATION ;
+      type = ZMAPFEATURE_BASIC ;
     }
 
 
+  /* Currently there will be lots of non-SO terms in any GFFv2 dump. */
   if (!SO_compliant)
     {
-      if (g_ascii_strcasecmp(feature_type, "SL1_acceptor_site") == 0
-	  || g_ascii_strcasecmp(feature_type, "SL2_acceptor_site") == 0)
+      if (g_ascii_strcasecmp(feature_type, "transcript") == 0
+	  || g_ascii_strcasecmp(feature_type, "protein-coding_primary_transcript") == 0
+	  /* N.B. "protein-coding_primary_transcript" is a typo in wormDB currently, should
+	   * be all underscores. */
+	  || g_ascii_strcasecmp(feature_type, "protein_coding_primary_transcript") == 0
+	  || g_ascii_strcasecmp(feature_type, "miRNA_primary_transcript") == 0
+	  || g_ascii_strcasecmp(feature_type, "snRNA_primary_transcript") == 0
+	  || g_ascii_strcasecmp(feature_type, "snoRNA_primary_transcript") == 0
+	  || g_ascii_strcasecmp(feature_type, "tRNA_primary_transcript") == 0
+	  || g_ascii_strcasecmp(feature_type, "rRNA_primary_transcript") == 0
+	  || g_ascii_strcasecmp(feature_type, "Pseudogene") == 0
+	  || g_ascii_strcasecmp(feature_type, "coding_exon") == 0
+	  || g_ascii_strcasecmp(feature_type, "exon") == 0
+	  || g_ascii_strcasecmp(feature_type, "intron") == 0)
 	{
-	  type = ZMAPFEATURE_BOUNDARY ;
+	  type = ZMAPFEATURE_TRANSCRIPT ;
 	}
-      else if (g_ascii_strcasecmp(feature_type, "Clone_right_end") == 0)
+      else if (g_ascii_strcasecmp(feature_type, "similarity") == 0
+	       || g_ascii_strcasecmp(feature_type, "transcription") == 0)
 	{
-	  type = ZMAPFEATURE_BASIC ;
+	  type = ZMAPFEATURE_ALIGNMENT ;
 	}
       else if (g_ascii_strcasecmp(feature_type, "Clone") == 0
 	       || g_ascii_strcasecmp(feature_type, "Clone_left_end") == 0
+	       || g_ascii_strcasecmp(feature_type, "Clone_right_end") == 0
+	       || g_ascii_strcasecmp(feature_type, "SL1_acceptor_site") == 0
+	       || g_ascii_strcasecmp(feature_type, "SL2_acceptor_site") == 0
 	       || g_ascii_strcasecmp(feature_type, "utr") == 0
 	       || g_ascii_strcasecmp(feature_type, "experimental") == 0
 	       || g_ascii_strcasecmp(feature_type, "reagent") == 0
@@ -169,61 +157,27 @@ gboolean zMapFeatureFormatType(gboolean SO_compliant, gboolean default_to_basic,
 	       || g_ascii_strcasecmp(feature_type, "structural") == 0
 	       || g_ascii_strcasecmp(feature_type, "contig") == 0
 	       || g_ascii_strcasecmp(feature_type, "supercontig") == 0
-	       || g_ascii_strcasecmp(feature_type, "misc_feature") == 0)
+	       || g_ascii_strcasecmp(feature_type, "misc_feature") == 0
+	       || g_ascii_strcasecmp(feature_type, "SNP") == 0
+	       || g_ascii_strcasecmp(feature_type, "complex_change_in_nucleotide_sequence") == 0
+	       || g_ascii_strcasecmp(feature_type, "trans-splice_acceptor") == 0)
 	{
 	  type = ZMAPFEATURE_BASIC ;
 	}
-      else if (g_ascii_strcasecmp(feature_type, "Pseudogene") == 0)
-	{
-	  /* REALLY NOT SURE ABOUT THIS CLASSIFICATION......SHOULD IT BE A TRANSCRIPT ? */
-	  type = ZMAPFEATURE_TRANSCRIPT ;
-	}
-      else if (g_ascii_strcasecmp(feature_type, "SNP") == 0
-	       || g_ascii_strcasecmp(feature_type, "complex_change_in_nucleotide_sequence") == 0)
-	{
-	  type = ZMAPFEATURE_VARIATION ;
-	}
-      else if (g_ascii_strcasecmp(feature_type, "Sequence") == 0)
-	{
-	  type = ZMAPFEATURE_SEQUENCE ;
-	}
-      else if (g_ascii_strcasecmp(feature_type, "transcript") == 0
-	       || g_ascii_strcasecmp(feature_type, "protein-coding_primary_transcript") == 0
-	       || g_ascii_strcasecmp(feature_type, "miRNA_primary_transcript") == 0
-	       || g_ascii_strcasecmp(feature_type, "snRNA_primary_transcript") == 0
-	       || g_ascii_strcasecmp(feature_type, "snoRNA_primary_transcript") == 0
-	       || g_ascii_strcasecmp(feature_type, "tRNA_primary_transcript") == 0
-	       || g_ascii_strcasecmp(feature_type, "rRNA_primary_transcript") == 0)
-	{
-	  /* N.B. "protein-coding_primary_transcript" is a typo in wormDB currently, should
-	   * be all underscores. */
-	  type = ZMAPFEATURE_TRANSCRIPT ;
-	}
-      else if (g_ascii_strcasecmp(feature_type, "similarity") == 0
-	       || g_ascii_strcasecmp(feature_type, "transcription") == 0)
-	{
-	  type = ZMAPFEATURE_HOMOL ;
-	}
-      else if (g_ascii_strcasecmp(feature_type, "trans-splice_acceptor") == 0)
-	{
-	  type = ZMAPFEATURE_BOUNDARY ;
-	}
-      else if (g_ascii_strcasecmp(feature_type, "coding_exon") == 0
-	       || g_ascii_strcasecmp(feature_type, "exon") == 0)
-	{
-	  type = ZMAPFEATURE_EXON ;
-	}
-      else if (g_ascii_strcasecmp(feature_type, "intron") == 0)
-	{
-	  type = ZMAPFEATURE_INTRON ;
-	}
+#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
+      /* I don't know what to do about this.... */
+      /*      else if (g_ascii_strcasecmp(feature_type, "Sequence") == 0)
+	      {
+	      type = ZMAPFEATURE_SEQUENCE ;
+	      } */
+#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
       else if (default_to_basic)
 	{
 	  /* If we allow defaulting of unrecognised features, the default is a "basic" feature. */
 	  type = ZMAPFEATURE_BASIC ;
 	}
     }
-
+ 
 
   if (type != ZMAPFEATURE_INVALID)
     {
@@ -235,27 +189,33 @@ gboolean zMapFeatureFormatType(gboolean SO_compliant, gboolean default_to_basic,
 }
 
 
-/* Score must either be the char '.' or a valid floating point number, currently
- * if the score is '.' we return 0, this may not be correct. */
-gboolean zMapFeatureFormatScore(char *score_str, gdouble *score_out)
+/* Score must either be the char '.' or a valid floating point number.
+ * Returns TRUE if the score is '.' or a valid floating point number, FALSE otherwise.
+ * If the score is '.' then we return has_score = FALSE and score_out is not set,
+ * otherwise has_score = TRUE and score_out contains the score. */
+gboolean zMapFeatureFormatScore(char *score_str, gboolean *has_score, gdouble *score_out)
 {
   gboolean result = FALSE ;
 
   if (strlen(score_str) == 1 && strcmp(score_str, ".") == 0)
     {
       result = TRUE ;
-      *score_out = 0 ;
+      *has_score = FALSE ;
     }
   else
     {
       gdouble score ;
 
       if ((result = zMapStr2Double(score_str, &score)))
-	*score_out = score ;
+	{
+	  *has_score = TRUE ;
+	  *score_out = score ;
+	}
     }
 
   return result ;
 }
+
 	
 
 /* Strand must be one of the chars '+', '-' or '.'. */
