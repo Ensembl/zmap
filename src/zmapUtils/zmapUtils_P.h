b5dfb54de659530269ce1797cf0cd9e3a09e0564
zmap <zmap>
2006-07-24 13:59:26 +0000

    /nfs/team71/acedb/zmap/prefix/scripts/versioner run by zmap

--- src/zmapUtils/zmapUtils_P.h
+++ src/zmapUtils/zmapUtils_P.h
@@ -26,7 +26,7 @@
  * HISTORY:
  * Last edited: May  3 13:22 2006 (edgrif)
  * Created: Wed Mar 31 11:53:45 2004 (edgrif)
- * CVS info:   $Id: zmapUtils_P.h,v 1.33 2006-07-24 13:54:40 zmap Exp $
+ * CVS info:   $Id: zmapUtils_P.h,v 1.34 2006-07-24 13:59:26 zmap Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_UTILS_P_H
