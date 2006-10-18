32468248b7122a1cc1c60cc8b0503465ce25ee94
edgrif <edgrif>
2006-10-18 10:39:03 +0000

    add file to hold references to web pages for zmap.

---
+++ src/include/ZMap/zmapWebPages.h
@@ -0,0 +1,46 @@
+/*  File: zmapWebPages.h
+ *  Author: Ed Griffiths (edgrif@sanger.ac.uk)
+ *  Copyright (c) Sanger Institute, 2006
+ *-------------------------------------------------------------------
+ * ZMap is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+ *-------------------------------------------------------------------
+ * This file is part of the ZMap genome database package
+ * originated by
+ * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
+ *      Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk
+ *
+ * Description: Contains references to zmap web pages for current
+ *              code release.
+ *
+ * HISTORY:
+ * Last edited: Oct 18 11:35 2006 (edgrif)
+ * Created: Wed Oct 18 11:19:12 2006 (edgrif)
+ * CVS info:   $Id: zmapWebPages.h,v 1.1 2006-10-18 10:39:03 edgrif Exp $
+ *-------------------------------------------------------------------
+ */
+#ifndef ZMAP_WEBPAGES_H
+#define ZMAP_WEBPAGES_H
+
+
+#define ZMAPWEB_URL "http://dev.acedb.org/ZMap"
+
+#define ZMAPWEB_RELEASE_NOTES_DIR "doc/Release_notes"
+
+#define ZMAPWEB_RELEASE_NOTES "release_notes.2006_10_18.shtml"
+
+
+
+#endif /* ZMAP_WEBPAGES_H */
