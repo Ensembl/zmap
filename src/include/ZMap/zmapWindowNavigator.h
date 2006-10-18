4ae218377c613ecc419ddf97dc5985d2b1b1eae4
rds <rds>
2006-10-18 15:29:40 +0000

    header for the pretty navigator

---
+++ src/include/ZMap/zmapWindowNavigator.h
@@ -0,0 +1,83 @@
+/*  File: zmapWindowNavigator.h
+ *  Author: Roy Storey (rds@sanger.ac.uk)
+ *  Copyright (c) Sanger Institute, 2006
+ *-------------------------------------------------------------------
+ * ZMap is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+ *-------------------------------------------------------------------
+ * This file is part of the ZMap genome database package
+ * originally written by:
+ *
+ * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
+ *      Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk
+ *
+ * Description: 
+ *
+ * Exported functions: See XXXXXXXXXXXXX.h
+ * HISTORY:
+ * Last edited: Oct 18 09:23 2006 (rds)
+ * Created: Thu Sep  7 09:10:32 2006 (rds)
+ * CVS info:   $Id: zmapWindowNavigator.h,v 1.1 2006-10-18 15:29:40 rds Exp $
+ *-------------------------------------------------------------------
+ */
+
+#ifndef ZMAP_WINDOW_NAVIGATOR_H
+#define ZMAP_WINDOW_NAVIGATOR_H 
+
+#include <ZMap/zmapWindow.h>
+
+#define ZMAP_NAVIGATOR_CLASS_DATA "ZMAP_WINDOW_NAV_CLASS_DATA"
+
+typedef void (*ZMapWindowNavigatorValueChanged)(void *user_data, double start, double end) ;
+
+typedef struct _ZMapWindowNavigatorStruct *ZMapWindowNavigator;
+
+typedef struct _ZMapNavigatorCallbackStruct
+{
+  ZMapWindowNavigatorValueChanged valueCB;
+} ZMapWindowNavigatorCallbackStruct, *ZMapWindowNavigatorCallback;
+
+/* unsure on publicness of this... */
+typedef struct _ZMapNavigatorClassDataStruct
+{
+  int width, height;
+  double text_height;
+  ZMapWindowNavigatorCallbackStruct callbacks;
+  gpointer user_data;
+  double container_width, container_height;
+} ZMapNavigatorClassDataStruct, *ZMapNavigatorClassData;
+
+
+
+ZMapWindowNavigator zMapWindowNavigatorCreate(GtkWidget *canvas_widget);
+void zMapWindowNavigatorSetCurrentWindow(ZMapWindowNavigator navigate, ZMapWindow window);
+void zMapWindowNavigatorMergeInFeatureSetNames(ZMapWindowNavigator navigate, 
+                                               GList *navigator_sets);
+
+void zMapWindowNavigatorDrawFeatures(ZMapWindowNavigator navigate, 
+                                     ZMapFeatureContext full_context);
+void zMapWindowNavigatorDrawLocator(ZMapWindowNavigator navigate,
+                                    double top, double bottom);
+void zMapWindowNavigatorDestroy(ZMapWindowNavigator navigate);
+
+
+
+/* Widget Functions */
+
+GtkWidget *zMapWindowNavigatorCreateCanvas(ZMapWindowNavigatorCallback callbacks, gpointer user_data);
+void zMapWindowNavigatorPackDimensions(GtkWidget *widget, double *width, double *height);
+
+
+#endif /*  ZMAP_WINDOW_NAVIGATOR_H  */
