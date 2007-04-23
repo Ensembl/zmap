386e89bbe4382d78ee6e0bc309aa3623f9504209
edgrif <edgrif>
2007-04-23 14:49:38 +0000

    reset version number.

--- src/zmapUtils/zmapUtils_P.h
+++ src/zmapUtils/zmapUtils_P.h
@@ -24,9 +24,9 @@
  *
  * Description: 
  * HISTORY:
- * Last edited: Feb 28 15:32 2007 (edgrif)
+ * Last edited: Apr 23 15:49 2007 (edgrif)
  * Created: Wed Mar 31 11:53:45 2004 (edgrif)
- * CVS info:   $Id: zmapUtils_P.h,v 1.61 2007-03-28 15:11:43 zmap Exp $
+ * CVS info:   $Id: zmapUtils_P.h,v 1.62 2007-04-23 14:49:38 edgrif Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_UTILS_P_H
@@ -40,7 +40,7 @@
 #define ZMAP_DESCRIPTION "A multi-threaded genome browser and annotation tool."
 #define ZMAP_VERSION 0
 #define ZMAP_RELEASE 1
-#define ZMAP_UPDATE 22
+#define ZMAP_UPDATE 21
 
 
 /* Make a single version number out of the version, release and update numbers. */
