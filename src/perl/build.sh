de74dc058c430d36853a5e54e62a8bd768ad4ea4
rds <rds>
2007-05-24 10:43:55 +0000

    make it work after some changes to the organisation to the directories...

--- src/perl/build.sh
+++ src/perl/build.sh
@@ -11,18 +11,15 @@ HERE=`pwd`
  SRC=$HERE/..
 ZMAP_INC="$SRC/include"
 ZMAP_LIB="$SRC/build/linux/lib"
+
 #==========================================
 # chose a perl version
-    PERL_EXE=/usr/local/bin/perl
-# That should be it
-#==========================================
-   PERL_ARCH=`$PERL_EXE -MConfig -e 'print $Config{archname}'`
-PERL_VERSION=`$PERL_EXE -MConfig -e 'print $Config{version}'`
-#PERL_INSTALL=`$PERL_EXE -MConfig -e 'print $Config{installstyle}'`
-#==========================================
-#==========================================
+PERL_EXE=/usr/local/bin/perl
 
-cd $HERE/X11-XRemote-0.01/X11/XRemote
+
+export CC=gcc
+
+cd $HERE/X11-XRemote-0.01
 
 if [ $1 ]; then
     PREFIX=$1
@@ -33,32 +30,18 @@ fi
 opsys=`uname`
 
 case $opsys in
-
   OSF1 )
         locals=/nfs/disk100/acedb/ZMap/prefix/ALPHA/lib/pkgconfig
-    PKG_CONFIG_PATH="/usr/local/gtk2/lib/pkgconfig"
-    ZMAP_LIB="$SRC/build/alpha/lib"
-    export PKG_CONFIG_PATH ;;
+        PKG_CONFIG_PATH="/usr/local/gtk2/lib/pkgconfig"
+        ZMAP_LIB="$SRC/build/alpha/lib"
+        export PKG_CONFIG_PATH ;;
 esac
 
-
-ADD_PERL5LIB=$PREFIX/lib/perl
-
 $PERL_EXE Makefile.PL PREFIX=$PREFIX \
---with-zmap-inc "-I$ZMAP_INC" \
---with-zmap-libs "-L$ZMAP_LIB" \
---with-symbols 
+--with-zmap-inc "$ZMAP_INC" \
+--with-zmap-libs "$ZMAP_LIB"
+--with-symbols || exit 1
 
-make
-make install
-
-PERL5LIB=$ADD_PERL5LIB:$PERL5LIB
-export PERL5LIB
-
-echo 
-echo "Testing installation..."
-echo "=================================="
-$PERL_EXE ./debug_test.pl
-echo "=================================="
-echo
-echo "Make sure $ADD_PERL5LIB is in your PERL5LIB"
+make         || exit 1
+make test    || exit 1
+make install || exit 1
