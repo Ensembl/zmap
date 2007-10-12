71d059fec92f14dbad128c27ae0ce2041f244b7c
edgrif <edgrif>
2007-10-12 11:10:12 +0000

    add code to store different types of stats, lists of col. stats.

--- src/zmapWindow/zmapWindowStats.c
+++ src/zmapWindow/zmapWindowStats.c
@@ -23,27 +23,137 @@
  *      Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk
  *
  * Description: Functions for window statistics, e.g. number of boxes drawn.
+ *              Note that individual feature stats are updated in the
+ *              item factory code which "knows" how many boxes get drawn
+ *              for each feature.
  *
  * Exported functions: See zmapWindow_P.h
  * HISTORY:
- * Last edited: Dec 15 07:32 2006 (edgrif)
+ * Last edited: Oct 12 11:53 2007 (edgrif)
  * Created: Tue Nov  7 10:10:25 2006 (edgrif)
- * CVS info:   $Id: zmapWindowStats.c,v 1.3 2006-12-15 09:20:33 edgrif Exp $
+ * CVS info:   $Id: zmapWindowStats.c,v 1.4 2007-10-12 11:10:12 edgrif Exp $
  *-------------------------------------------------------------------
  */
 
 #include <string.h>
+#include <ZMap/zmapUtils.h>
+#include <ZMap/zmapFeature.h>
 #include <zmapWindow_P.h>
 
 
+/* Stats are held in the Containers of the canvas window, this is because the stats capture
+ * not only the numbers of features but also the numbers of boxes drawn etc. */
+
+
+/* 
+ * Stats struct for each set of stats for each container.
+ * (Opaque pointer declared in zmapWindow_P.h).
+ */
+typedef struct _ZMapWindowStatsStruct
+{
+  /* The basic stats... */
+  int num_context_children ;				    /* e.g. how many blocks, feature sets etc. */
+  int num_canvas_children ;				    /* e.g. how many canvas items to
+							       represent context children. */
+
+  GQuark feature_id ;					    /* i.e. name of block/align etc. */
+
+  /* List of ZMapWindowStatsXXX structs, this is NULL if there are no features.
+   * Note that we need a list here if we are support multiple feature types in each column. */
+  GList *child_sets ;
+
+} ZMapWindowStatsStruct ;
+
+
+
+
+static void resetStats(gpointer data, gpointer user_data_unused) ;
+static void printStats(gpointer data, gpointer user_data_unused) ;
+static gint feature2StyleCompare(gconstpointer a, gconstpointer b) ;
+
+
+
+/* 
+ *                  External functions
+ */
+
+
+ZMapWindowStats zmapWindowStatsCreate(ZMapFeatureAny feature_any)
+{
+  ZMapWindowStats stats ;
+  int nbytes = sizeof(ZMapWindowStatsStruct) ;
+
+  stats = g_slice_alloc0(nbytes) ;
+
+  stats->feature_id = feature_any->original_id ;
+
+  return stats ;
+}
+
+
+/* Note that we only record extra stats for any features added, stuff like blocks/aligns etc
+ * do not get their own stats currently....... */
+ZMapWindowStatsAny zmapWindowStatsAddChild(ZMapWindowStats stats, ZMapFeatureAny feature_any)
+{
+  ZMapWindowStatsAny stats_any = NULL ;
+
+  /* Increment overall stats. */
+  stats->num_context_children++ ;
+  stats->num_canvas_children++ ;
+
+
+  /* If its a zmapfeature then some extra stats are recorded. */
+  if (feature_any->struct_type == ZMAPFEATURE_STRUCT_FEATURE)
+    {
+      GList *stats_list ;
+      ZMapFeature feature = (ZMapFeature)feature_any ;
+
+      zMapAssert(zMapFeatureIsValidFull((ZMapFeatureAny)feature, ZMAPFEATURE_STRUCT_FEATURE)) ;
+
+      if ((stats_list = g_list_find_custom(stats->child_sets, (ZMapFeature)feature_any, feature2StyleCompare)))
+	{
+	  stats_any = (ZMapWindowStatsAny)(stats_list->data) ;
+	}
+      else
+	{
+	  int num_bytes ;
+
+	  switch (feature->type)
+	    {
+	    case ZMAPFEATURE_BASIC:
+	    case ZMAPFEATURE_RAW_SEQUENCE:
+	    case ZMAPFEATURE_PEP_SEQUENCE:
+	      num_bytes = sizeof(ZMapWindowStatsBasicStruct) ;
+	      break ;
+	    case ZMAPFEATURE_ALIGNMENT:
+	      num_bytes = sizeof(ZMapWindowStatsAlignStruct) ;
+	      break ;
+	    case ZMAPFEATURE_TRANSCRIPT:
+	      num_bytes = sizeof(ZMapWindowStatsTranscriptStruct) ;
+	      break ;
+	    default:
+	      zMapAssertNotReached() ;
+	      break ;
+	    }
+
+	  stats_any = g_slice_alloc0(num_bytes) ;
+	  stats_any->feature_type = feature->type ;
+	  stats_any->style = feature->style ;
+
+	  stats->child_sets = g_list_append(stats->child_sets, stats_any) ;
+	}
+    }
+
+  return stats_any ;
+}
 
 
 void zmapWindowStatsReset(ZMapWindowStats stats)
 {
+  stats->num_context_children = 0 ;
+  stats->num_canvas_children = 0 ;
 
-  /* this is fine for now as struct only contains ints, when we have other data we will need to do
-     other stuff. */
-  memset(stats, 0, sizeof(ZMapWindowStatsStruct)) ;
+  g_list_foreach(stats->child_sets, resetStats, NULL) ;
 
   return ;
 }
@@ -53,10 +163,119 @@ void zmapWindowStatsReset(ZMapWindowStats stats)
 void zmapWindowStatsPrint(ZMapWindowStats stats)
 {
 
-  printf("\nMatches -  total: %d,  gapped: %d,  not perfect gapped: %d, ungapped: %d,\n"
-	 "Boxes   -  total: %d,  gapped boxes: %d,  ungapped boxes: %d, gapped boxes not drawn: %d\n\n",
-	 stats->total_matches, stats->gapped_matches, stats->not_perfect_gapped_matches, stats->ungapped_matches,
-	 stats->total_boxes, stats->gapped_boxes, stats->ungapped_boxes, stats->imperfect_boxes) ;
+  printf("%s:\tContext children: %d, canvas children: %d\n",
+	 g_quark_to_string(stats->feature_id), stats->num_context_children, stats->num_canvas_children) ;
+
+  g_list_foreach(stats->child_sets, printStats, NULL) ;
 
   return ;
 }
+
+
+
+void zmapWindowStatsDestroy(ZMapWindowStats stats)
+{
+  int nbytes = sizeof(ZMapWindowStatsStruct) ;
+
+  zMapAssert(stats) ;
+
+  g_slice_free1(nbytes, stats) ;
+
+  return ;
+}
+
+
+
+/* 
+ *                            Internal functions 
+ */
+
+
+static void resetStats(gpointer data, gpointer user_data_unused)
+{
+  ZMapWindowStatsAny any_stats = (ZMapWindowStatsAny)data ;
+  int num_bytes = 0 ;
+
+  switch (any_stats->feature_type)
+    {
+    case ZMAPFEATURE_BASIC:
+    case ZMAPFEATURE_RAW_SEQUENCE:
+    case ZMAPFEATURE_PEP_SEQUENCE:
+      num_bytes = sizeof(ZMapWindowStatsBasicStruct) ;
+      break ;
+    case ZMAPFEATURE_ALIGNMENT:
+      num_bytes = sizeof(ZMapWindowStatsAlignStruct) ;
+      break ;
+    case ZMAPFEATURE_TRANSCRIPT:
+      num_bytes = sizeof(ZMapWindowStatsTranscriptStruct) ;
+      break ;
+    default:
+      zMapAssertNotReached() ;
+      break ;
+    }
+
+  memset(any_stats, 0, num_bytes) ;
+
+  return ;
+}
+
+
+
+
+
+static void printStats(gpointer data, gpointer user_data_unused)
+{
+  ZMapWindowStatsAny any_stats = (ZMapWindowStatsAny)data ;
+
+  switch (any_stats->feature_type)
+    {
+    case ZMAPFEATURE_BASIC:
+    case ZMAPFEATURE_RAW_SEQUENCE:
+    case ZMAPFEATURE_PEP_SEQUENCE:
+      {
+	ZMapWindowStatsBasic basic = (ZMapWindowStatsBasic)any_stats ;
+
+	printf("Basic\tfeatures:%d\t boxes:%d\n", basic->features, basic->items) ;
+
+	break ;
+      }
+    case ZMAPFEATURE_ALIGNMENT:
+      {
+	ZMapWindowStatsAlign align = (ZMapWindowStatsAlign)any_stats ;
+	printf("Alignment\tfeatures:%d\tgapped:%d\tnot perfect gapped:%d\tungapped:%d\t"
+	       "boxes:%d\tgapped boxes:%d\tungapped boxes:%d\tgapped boxes not drawn:%d\n",
+	       align->total_matches, align->gapped_matches, align->not_perfect_gapped_matches, align->ungapped_matches,
+	       align->total_boxes, align->gapped_boxes, align->ungapped_boxes, align->imperfect_boxes) ;
+	break ;
+      }
+    case ZMAPFEATURE_TRANSCRIPT:
+      {
+	ZMapWindowStatsTranscript transcript = (ZMapWindowStatsTranscript)any_stats ;
+
+	printf("Transcript\tfeatures:%d\texons:%d,\tintrons:%d,\tcds:%d"
+	       "boxes:%d\texon_boxes:%d\tintron_boxes:%d\tcds_boxes:%d\n",
+	       transcript->transcripts, transcript->exons, transcript->introns, transcript->cds,
+	       transcript->items, transcript->exon_boxes, transcript->intron_boxes, transcript->cds_boxes) ;
+	break ;
+      }
+    default:
+      zMapAssertNotReached() ;
+      break ;
+    }
+
+  return ;
+}
+
+
+static gint feature2StyleCompare(gconstpointer a, gconstpointer b)
+{
+  gint result = -1 ;
+  ZMapWindowStatsAny stats = (ZMapWindowStatsAny)a ;
+  ZMapFeature feature = (ZMapFeature)b ;
+
+  if (zMapStyleGetUniqueID(stats->style) == zMapStyleGetUniqueID(feature->style))
+    result = 0 ;
+
+  return result ;
+}
+
