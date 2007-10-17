1cca42f1beb777ba683d31e4d51382d33d074b3d
edgrif <edgrif>
2007-10-17 15:45:40 +0000

    add public header for new IO package.

---
+++ src/include/ZMap/zmapIO.h
@@ -0,0 +1,53 @@
+/*  File: zmapIO.h
+ *  Author: Ed Griffiths (edgrif@sanger.ac.uk)
+ *  Copyright (c) 2007: Genome Research Ltd.
+ *-------------------------------------------------------------------
+ * ZMap is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+ *-------------------------------------------------------------------
+ * This file is part of the ZMap genome database package
+ * originally written by:
+ *
+ * 	Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
+ *      Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk
+ *
+ * Description: Package to IO to strings, sockets, files etc.
+ *
+ * HISTORY:
+ * Last edited: Oct 17 13:55 2007 (edgrif)
+ * Created: Mon Oct 15 13:53:39 2007 (edgrif)
+ * CVS info:   $Id: zmapIO.h,v 1.1 2007-10-17 15:45:40 edgrif Exp $
+ *-------------------------------------------------------------------
+ */
+#ifndef ZMAP_IO_H
+#define ZMAP_IO_H
+
+#include <glib.h>
+
+typedef struct ZMapIOOutStruct_ *ZMapIOOut ;
+
+
+
+gboolean zMapOutValid(ZMapIOOut out) ;
+ZMapIOOut zMapOutCreateStr(char *init_value, int size) ;
+ZMapIOOut zMapOutCreateFD(int fd) ;
+char *zMapOutGetStr(ZMapIOOut out) ;
+gboolean zMapOutWrite(ZMapIOOut out, char *text) ;
+gboolean zMapOutWriteFormat(ZMapIOOut out, char *format, ...) ;
+void zMapOutDestroy(ZMapIOOut out) ;
+
+
+
+#endif /* !ZMAP_IO_H */
