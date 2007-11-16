baa15d302a9ba7b840a3f68ab79ad45ca1c37cbc
zmap <zmap>
2007-11-16 12:15:56 +0000

    helpful messages

--- gtk/patches/jpegsrc.v6b.sh
+++ gtk/patches/jpegsrc.v6b.sh
@@ -11,10 +11,12 @@ PACKAGE=$2
 . $BASE_DIR/build_functions.sh
 
 # copy the libtool files from $PREFIX
+build_message_out "Copying config.sub and config.guess to libjpeg directory"
 cp $PREFIX/share/libtool/config.sub $BASE_DIR/$BUILD_DIR/$PACKAGE/   || build_message_exit "Copy config.sub Failed"
 cp $PREFIX/share/libtool/config.guess $BASE_DIR/$BUILD_DIR/$PACKAGE/ || build_message_exit "Copy config.guess Failed"
 
 # some reason install won't create these!
+build_message_out "Making man directories in $PREFIX... (do you need to sudo here?)"
 mkdir -p $PREFIX/man/man{1,2,3,4,5,6,7,8,9} || build_message_exit "Man dir creation failed!"
 
 exit 0;
