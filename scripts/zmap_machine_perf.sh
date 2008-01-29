0580ea85c20d844c582c8e96c8bb6f51b439cf53
zmap <zmap>
2008-01-29 08:35:04 +0000

    script to report machine loading details

---
+++ scripts/zmap_machine_perf.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+########################
+# Find out where BASE is
+SCRIPT_NAME=$(basename $0)
+INITIAL_DIR=$(pwd)
+ SCRIPT_DIR=$(dirname $0)
+   BASE_DIR=$INITIAL_DIR/$SCRIPT_DIR
+
+. $BASE_DIR/zmap_functions.sh
+
+
+# name of the output file
+MACHINE_INFO_FILE="machine_info.txt"
+
+
+
+# message to user to say hi
+
+zmap_message_out "Creating machine info file..."
+
+# create the output file
+zmap_create_file $MACHINE_INFO_FILE
+
+# hostname
+this_host=`hostname`
+zmap_message_file $MACHINE_INFO_FILE "ZMap machine info file for : $this_host"
+
+# OS etc...
+zmap_message_file $MACHINE_INFO_FILE "Kernel/Arch/OS:"
+zmap_message_file $MACHINE_INFO_FILE "   " `uname -a`
+
+# CPU (important bits)
+zmap_message_file $MACHINE_INFO_FILE "CPU:"
+zmap_message_file $MACHINE_INFO_FILE "   " `cat /proc/cpuinfo | grep -i 'model name'`
+zmap_message_file $MACHINE_INFO_FILE "   " `cat /proc/cpuinfo | grep -i 'mhz'`
+zmap_message_file $MACHINE_INFO_FILE "   " `cat /proc/cpuinfo | grep -i 'cache size'`
+
+# Memory (important bits)
+zmap_message_file $MACHINE_INFO_FILE "Memory:"
+zmap_message_file $MACHINE_INFO_FILE "   " `cat /proc/meminfo | grep -i 'memtotal'`
+zmap_message_file $MACHINE_INFO_FILE "   " `cat /proc/meminfo | grep -i 'memfree'`
+zmap_message_file $MACHINE_INFO_FILE "   " `cat /proc/meminfo | grep -i 'vmalloc'`
+
+# This user's processes
+zmap_message_file $MACHINE_INFO_FILE "Processes:"
+ps uwx >>$MACHINE_INFO_FILE
+
+# How long has this machine been on?
+# What's its load average?
+zmap_message_file $MACHINE_INFO_FILE "Lifestyle:"
+zmap_message_file $MACHINE_INFO_FILE "   " `uptime`
+
+# finished... Now cat the output to terminal
+
+zmap_message_out "Finished creating file."
+zmap_message_out $MACHINE_INFO_FILE "contains:"
+zmap_message_out "--------------------------------------------------------------"
+
+cat $MACHINE_INFO_FILE
+
+zmap_message_out "--------------------------------------------------------------"
+
+zmap_message_out "Mailing file to zmap developers"
+
+mailx -s "[$SCRIPT_NAME] $this_host machine info" zmapdev < $MACHINE_INFO_FILE || zmap_message_exit "Failed to mail information to developers"
+
+rm -f $MACHINE_INFO_FILE
