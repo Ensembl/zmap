3d5654f6bd9155ac2b814d0f7957fa5182d47d6c
rds <rds>
2008-02-19 12:27:36 +0000

    added cvs directory navigation

--- scripts/zmap_functions.sh
+++ scripts/zmap_functions.sh
@@ -75,7 +75,12 @@ function zmap_cd
     if [ "x$SILENT_CD" != "x$ZMAP_TRUE" ]; then
 	zmap_message_out "cd $1"
     fi
+
     cd $1 || zmap_message_exit "Failed to cd to $1"
+
+    if [ "x$SILENT_CD" != "x$ZMAP_TRUE" ]; then
+	zmap_message_out "Now in $(pwd)"
+    fi
 }
 
 function zmap_mkdir
@@ -104,6 +109,28 @@ function zmap_pathmunge
         else
             PATH=$PATH:$1
         fi
+    fi
+}
+
+# Usage: zmap_goto_cvs_module_root
+function zmap_goto_cvs_module_root
+{
+    if [ -f CVS/Repository ]; then 
+	LOCATION=$(cat CVS/Repository)
+	current=$(pwd)
+	zmap_message_out "Navigating out of $LOCATION"
+	while [ "x$LOCATION" != "x." ];
+	  do
+	  CVS_MODULE=$(basename $LOCATION)
+	  LOCATION=$(dirname $LOCATION)
+	  zmap_message_out "Leaving $CVS_MODULE."
+	  CVS_MODULE_LOCAL=$(basename $current)
+	  current=$(dirname $current)
+	  zmap_cd $current
+	done
+	zmap_cd $CVS_MODULE_LOCAL
+    else
+	zmap_message_exit "Failed to find Repository file in CVS directory."
     fi
 }
 
