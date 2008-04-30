b081404cf0257e5bd61acb0eb391f9c92a74f84d
zmap <zmap>
2008-04-30 08:07:42 +0000

    /var/tmp/zmap.5013/ZMap/scripts/versioner run by zmap

--- src/zmapUtils/zmapUtils_P.h
+++ src/zmapUtils/zmapUtils_P.h
@@ -26,7 +26,7 @@
  * HISTORY:
  * Last edited: Apr 23 15:49 2007 (edgrif)
  * Created: Wed Mar 31 11:53:45 2004 (edgrif)
- * CVS info:   $Id: zmapUtils_P.h,v 1.88 2008-04-29 08:15:07 zmap Exp $
+ * CVS info:   $Id: zmapUtils_P.h,v 1.89 2008-04-30 08:07:42 zmap Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_UTILS_P_H
@@ -40,7 +40,7 @@
 #define ZMAP_DESCRIPTION "A multi-threaded genome browser and annotation tool."
 #define ZMAP_VERSION 0
 #define ZMAP_RELEASE 1
-#define ZMAP_UPDATE 47
+#define ZMAP_UPDATE 48
 
 
 /* Make a single version number out of the version, release and update numbers. */
