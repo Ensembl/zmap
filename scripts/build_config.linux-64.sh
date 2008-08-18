c6e0d070f3259491025b415a71030d72f1e1cdfa
rds <rds>
2008-08-18 18:53:43 +0000

    64bit compile

--- scripts/build_config.linux-64.sh
+++ scripts/build_config.linux-64.sh
@@ -6,6 +6,8 @@
 # its install path to the search dirs
 ACLOCAL_FLAGS="-I /usr/share/aclocal"
 
+# almost everything of interest is in here.
+PATH=$GTK_PREFIX/bin:$PATH
 
 # This shouldn't get overwritten.
 PKG_CONFIG=$GTK_PREFIX/bin/pkg-config
