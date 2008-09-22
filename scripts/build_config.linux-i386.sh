9d07f030fcff2cfd83228eda3ec37a3549e96875
rds <rds>
2008-09-22 09:50:22 +0000

    *** empty log message ***

--- scripts/build_config.linux-i386.sh
+++ scripts/build_config.linux-i386.sh
@@ -9,4 +9,4 @@ ACECONN_PREFIX=~zmap/prefix/LINUX
 PKG_CONFIG=$GTK_PREFIX/bin/pkg-config
 
 # autotools are also installed in here!
-PATH=$ACECONN_PREFIX/bin:$PATH
+PATH=$ACECONN_PREFIX/bin:$GTK_PREFIX/bin:$PATH
