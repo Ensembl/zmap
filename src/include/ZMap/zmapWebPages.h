abff716918fd1dbd1f14a9422feec3e5d10c71d3
zmap <zmap>
2009-11-30 12:52:23 +0000

    update release notes file to release_notes.2009_11_30.shtml

--- src/include/ZMap/zmapWebPages.h
+++ src/include/ZMap/zmapWebPages.h
@@ -28,7 +28,7 @@
  * HISTORY:
  * Last edited: Jul 18 10:14 2008 (edgrif)
  * Created: Wed Oct 18 11:19:12 2006 (edgrif)
- * CVS info:   $Id: zmapWebPages.h,v 1.40 2009-10-23 12:51:51 zmap Exp $
+ * CVS info:   $Id: zmapWebPages.h,v 1.41 2009-11-30 12:52:23 zmap Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_WEBPAGES_H
@@ -48,7 +48,7 @@
 
 /* This line is parsed/updated by ZMap/scripts/zmapreleasenotes, do not alter its format without
  * updating that script as well. */
-#define ZMAPWEB_RELEASE_NOTES "release_notes.2009_10_23.shtml"
+#define ZMAPWEB_RELEASE_NOTES "release_notes.2009_11_30.shtml"
 
 #define ZMAPWEB_HELP_DOC  "user_interface.shtml"
 #define ZMAPWEB_HELP_ALIGNMENT_SECTION "alignment_display"
