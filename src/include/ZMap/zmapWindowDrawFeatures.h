5dbb17a90477b10afdc22d67602bdc553db2950e
mh17 <mh17>
2010-02-23 10:58:36 +0000

    Removed private header (NB:file is not used)

--- src/include/ZMap/zmapWindowDrawFeatures.h
+++ src/include/ZMap/zmapWindowDrawFeatures.h
@@ -6,12 +6,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -28,7 +28,7 @@
  * HISTORY:
  * Last edited: Nov  8 11:50 2004 (rnc)
  * Created: Fri Aug 13 (rnc)
- * CVS info:   $Id: zmapWindowDrawFeatures.h,v 1.9 2006-11-08 09:23:33 edgrif Exp $
+ * CVS info:   $Id: zmapWindowDrawFeatures.h,v 1.10 2010-02-23 10:58:36 mh17 Exp $
  *-------------------------------------------------------------------
  */
 #ifndef ZMAP_WINDOW_DRAWFEATURES_H
@@ -36,7 +36,6 @@
 
 #include <ZMap/zmapSys.h>		       /* For callback funcs... */
 #include <ZMap/zmapFeature.h>
-#include <zmapWindow_P.h>
 
 
 
