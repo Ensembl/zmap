b543e978b0cedff62ca25a3f90b41bf5542a1e56
mh17 <mh17>
2010-03-04 15:35:32 +0000

    added copyright header

--- src/include/ZMap/zmapUrl.h
+++ src/include/ZMap/zmapUrl.h
@@ -1,33 +1,36 @@
-/*  Last edited: 2009-11-26 12:52:29 (mgh) */
-/*  Last edited: Aug 31 11:05 2005 (rds) */
-/* Declarations for url.c.
-   Copyright (C) 1995, 1996, 1997 Free Software Foundation, Inc.
-
-This file is part of GNU Wget.
-
-GNU Wget is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-GNU Wget is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Wget; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-In addition, as a special exception, the Free Software Foundation
-gives permission to link the code of its release of Wget with the
-OpenSSL projects "OpenSSL" library (or with modified versions of it
-that use the same license as the "OpenSSL" library), and distribute
-the linked executables.  You must obey the GNU General Public License
-in all respects for all of the code used other than "OpenSSL".  If you
-modify this file, you may extend this exception to your version of the
-file, but you are not obligated to do so.  If you do not wish to do
-so, delete this exception statement from your version.  */
+/*  File: zmapUrl.h
+ *  Author: Roy Story (rds@sanger.ac.uk)
+ *  Copyright (c) 2006-2010: Genome Research Ltd.
+ *-------------------------------------------------------------------
+ * ZMap is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+ *-------------------------------------------------------------------
+ * This file is part of the ZMap genome database package
+ * originally written by:
+ *
+ *     Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
+ *      Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk
+ *
+ * Description:
+ *
+ * Exported functions: See XXXXXXXXXXXXX.h
+ * HISTORY:
+ * Created: Thu Nov 26 10:30:21 2009 (mh17)
+ * CVS info:   $Id: zmapUrl.h,v 1.6 2010-03-04 15:35:32 mh17 Exp $
+ *-------------------------------------------------------------------
+ */
 
 #ifndef ZMAPURL_H
 #define ZMAPURL_H
@@ -35,11 +38,11 @@ so, delete this exception statement from your version.  */
 
 /* Default port definitions */
 #define DEFAULT_HTTP_PORT 80
-#define DEFAULT_FTP_PORT 21      
+#define DEFAULT_FTP_PORT 21
 #define DEFAULT_FILE_PORT 0      /* There is no port */
 #define DEFAULT_PIPE_PORT 0      /* There is no port */
 #define DEFAULT_ACEDB_PORT 23100 /* default sgifaceserver port */
-#define DEFAULT_MYSQL_PORT 3306  
+#define DEFAULT_MYSQL_PORT 3306
 #define DEFAULT_HTTPS_PORT 443
 
 /* Note: the ordering here is related to the order of elements in
