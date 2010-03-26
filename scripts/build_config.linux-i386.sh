114c3e52bdebf3673f34b1d0b5092744db098a7d
edgrif <edgrif>
2010-03-26 09:17:03 +0000

    use the aceconn installed in /software/acedb

--- scripts/build_config.linux-i386.sh
+++ scripts/build_config.linux-i386.sh
@@ -1,10 +1,6 @@
 #!/bin/echo dot script please source
 
 
-# For some reason aceconn isn't installed in GTK_PREFIX
-# This should change asap. Production foocanvas is!
-ACECONN_PREFIX=~zmap/prefix/LINUX
-
 # This shouldn't get overwritten.
 PKG_CONFIG=$GTK_PREFIX/bin/pkg-config
 
