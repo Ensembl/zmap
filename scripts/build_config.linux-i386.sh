0027a8d2bc6e9eebaf7ba996c65d130dc58d70d5
edgrif <edgrif>
2010-03-31 15:38:16 +0000

    add some defines for gtk and c compiler which are generally needed.

--- scripts/build_config.linux-i386.sh
+++ scripts/build_config.linux-i386.sh
@@ -1,6 +1,14 @@
 #!/bin/echo dot script please source
 
 
+# Required to get backtraces otherwise many symbols are not passed to ELF linker.
+CFLAGS_args="$CFLAGS_args -rdynamic"
+
+
+# I think we need /software/acedb/gtk as the default.
+GTK_PREFIX=/software/acedb/gtk
+
+
 # This shouldn't get overwritten.
 PKG_CONFIG=$GTK_PREFIX/bin/pkg-config
 
