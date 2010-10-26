45278500d74ea7f5fcdce836ac39ffe0b6bd8445
edgrif <edgrif>
2010-10-26 14:31:21 +0000

    add defines to make sure right aclocal macros and right autotools picked up.

--- scripts/build_config.tviewsrv.sh
+++ scripts/build_config.tviewsrv.sh
@@ -16,3 +16,9 @@ TOTALVIEW_PREFIX=/usr/toolworks/totalview.8.3.0-0
 # For testing alternative versions of gtk+
 GTK_PREFIX=/software/acedb/gtk
 # GTK_PREFIX=/software/acedb/gtk+-2.14.7
+
+# Make sure we look in the right place of autoconf macros...
+ACLOCAL_FLAGS="-I $GTK_PREFIX/share/aclocal"
+
+# autotools are also installed in here!
+PATH=$GTK_PREFIX/bin:$PATH
