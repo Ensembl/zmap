4c2b98079895164faef89fdf81523076c4c2e164
edgrif <edgrif>
2010-11-01 11:23:47 +0000

    try and fix autoconf refs etc.

--- scripts/build_config.linux-64.sh
+++ scripts/build_config.linux-64.sh
@@ -1,14 +1,21 @@
 #!/bin/echo dot script please source
 
 
-# I think I missed a trick installing the autotools
-# aclocal doesn't add the system path as well as
-# its install path to the search dirs
-ACLOCAL_FLAGS="-I /usr/share/aclocal"
+# For testing alternative versions of gtk+
+GTK_PREFIX=/software/acedb/gtk
 
-# almost everything of interest is in here.
-PATH=$GTK_PREFIX/bin:$PATH
 
 # This shouldn't get overwritten.
 PKG_CONFIG=$GTK_PREFIX/bin/pkg-config
+
+
+# I think I missed a trick installing the autotools
+# aclocal doesn't add the system path as well as
+# its install path to the search dirs
+ACLOCAL_FLAGS="-I $GTK_PREFIX/share/aclocal"
+
+
+# autotools in /usr is newer.
+#PATH=$GTK_PREFIX/bin:$PATH
+PATH=$PATH:$GTK_PREFIX/bin
 
