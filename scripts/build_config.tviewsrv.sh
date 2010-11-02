7ccfeb9b4c0db934d493bba40bef14c7bb707100
edgrif <edgrif>
2010-11-02 10:43:10 +0000

    remove tviewsrv specials, not needed currently.

--- scripts/build_config.tviewsrv.sh
+++ scripts/build_config.tviewsrv.sh
@@ -6,7 +6,7 @@
 #FOOCANVAS_PREFIX=/nfs/team71/acedb/edgrif/CHECKOUT/foocanvas_test/linux
 
 # Required to get backtraces otherwise many symbols are not passed to ELF linker.
-CFLAGS_args="$CFLAGS_args -rdynamic"
+#CFLAGS_args="$CFLAGS_args -rdynamic"
 
 TOTALVIEW_PREFIX=/usr/toolworks/totalview.8.3.0-0
 
@@ -14,12 +14,12 @@ TOTALVIEW_PREFIX=/usr/toolworks/totalview.8.3.0-0
 # LDFLAGS="-L$TOTALVIEW_PREFIX/linux-x86/lib -ldbfork"
 
 # For testing alternative versions of gtk+
-GTK_PREFIX=/software/acedb/gtk
-# GTK_PREFIX=/software/acedb/gtk+-2.14.7
+#GTK_PREFIX=/software/acedb/gtk
+#GTK_PREFIX=/usr
+
 
 # Make sure we look in the right place of autoconf macros...
-ACLOCAL_FLAGS="-I $GTK_PREFIX/share/aclocal"
+#ACLOCAL_FLAGS="-I $GTK_PREFIX/share/aclocal"
 
 # autotools are also installed in here!
 #PATH=$GTK_PREFIX/bin:$PATH
-PATH=$PATH:$GTK_PREFIX/bin
