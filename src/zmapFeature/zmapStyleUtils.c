fee91e134af7ee7d97b57ad69a68e5a80ec363e1
edgrif <edgrif>
2011-05-06 12:06:41 +0000

    move to single sequence mode.

--- src/zmapFeature/zmapStyleUtils.c
+++ src/zmapFeature/zmapStyleUtils.c
@@ -22,25 +22,20 @@
  *
  *      Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
  *        Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk,
- *     Malcolm Hinsley (Sanger Institute, UK) mh17@sanger.ac.uk
+ *   Malcolm Hinsley (Sanger Institute, UK) mh17@sanger.ac.uk
  *
  * Description:
  *
  * Exported functions: See ZMap/zmapStyle.h
  *
  * HISTORY:
- * Last edited: Jul 29 09:53 2009 (edgrif)
+ * Last edited: May  6 13:06 2011 (edgrif)
  * Created: Thu Oct 30 10:24:35 2008 (edgrif)
- * CVS info:   $Id: zmapStyleUtils.c,v 1.20 2010-10-13 09:00:37 mh17 Exp $
+ * CVS info:   $Id: zmapStyleUtils.c,v 1.21 2011-05-06 12:06:41 edgrif Exp $
  *-------------------------------------------------------------------
  */
 
 #include <ZMap/zmap.h>
-
-
-
-
-
 
 #include <string.h>
 #include <unistd.h>
@@ -307,8 +302,7 @@ void zMapStylePrint(ZMapIOOut dest, ZMapFeatureTypeStyle style, char *prefix, gb
     {
     case ZMAPSTYLE_MODE_INVALID:
     case ZMAPSTYLE_MODE_BASIC:
-    case ZMAPSTYLE_MODE_RAW_SEQUENCE:
-    case ZMAPSTYLE_MODE_PEP_SEQUENCE:
+    case ZMAPSTYLE_MODE_SEQUENCE:
     case ZMAPSTYLE_MODE_TEXT:
     case ZMAPSTYLE_MODE_META:
       {
