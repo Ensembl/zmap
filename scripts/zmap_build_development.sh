1102bad3b5948ba01dc51fe49ea9a62b6bc4bd15
edgrif <edgrif>
2011-05-11 14:44:07 +0000

    fix up build dir again.

--- scripts/zmap_build_development.sh
+++ scripts/zmap_build_development.sh
@@ -159,7 +159,7 @@ rm -f root_checkout.sh     || exit 1;   \
 cat - > root_checkout.sh   || exit 1;   \
 chmod 755 root_checkout.sh || _rm_exit; \
 : Change the variables in next line   ; \
-./root_checkout.sh -t -u RELEASE_LOCATION='$OUTPUT' ZMAP_MASTER_RT_TO_CVS=no || _rm_exit; \
+./root_checkout.sh -t -u ZMAP_RELEASES_DIR='$BUILDS_DIR' ZMAP_MASTER_RT_TO_CVS=no || _rm_exit; \
 :                                     ; \
 rm -f root_checkout.sh || exit 1;   \
 "' > $GLOBAL_LOG 2>&1
