46f51164add7ff223df0d47485a38c1ef99b3750
edgrif <edgrif>
2011-05-13 13:00:22 +0000

    fix up feature dir location.

--- scripts/zmap_build_release.sh
+++ scripts/zmap_build_release.sh
@@ -159,7 +159,7 @@ rm -f root_checkout.sh     || exit 1;   \
 cat - > root_checkout.sh   || exit 1;   \
 chmod 755 root_checkout.sh || _rm_exit; \
 : Change the variables in next line   ; \
-./root_checkout.sh -d -t -u  RELEASE_LOCATION='$OUTPUT' ZMAP_MASTER_RT_RELEASE_NOTES=yes ZMAP_MASTER_FORCE_RELEASE_NOTES=yes  ZMAP_MASTER_BUILD_DIST=yes || _rm_exit; \
+./root_checkout.sh -d -t -u ZMAP_MASTER_RT_RELEASE_NOTES=yes ZMAP_MASTER_FORCE_RELEASE_NOTES=yes  ZMAP_MASTER_BUILD_DIST=yes || _rm_exit; \
 :                                     ; \
 rm -f root_checkout.sh || exit 1;   \
 "' > $GLOBAL_LOG 2>&1
