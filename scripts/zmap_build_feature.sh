f7135fb4c3b50ea6cef61bde31d62c81961b74d2
edgrif <edgrif>
2011-05-13 09:46:10 +0000

    fix up feature dir location.

--- scripts/zmap_build_feature.sh
+++ scripts/zmap_build_feature.sh
@@ -130,8 +130,9 @@ cd /var/tmp                || exit 1;   \
 rm -f root_checkout.sh     || exit 1;   \
 cat - > root_checkout.sh   || exit 1;   \
 chmod 755 root_checkout.sh || _rm_exit; \
+export ZMAP_MASTER_BUILD_COPY_DIR='$SRC_DIR' \ 
 : Change the variables in next line             ; \
-./root_checkout.sh RELEASE_LOCATION='$OUTPUT' ZMAP_MASTER_BUILD_COPY_DIR='$SRC_DIR' || _rm_exit; \
+./root_checkout.sh RELEASE_LOCATION='$OUTPUT' || _rm_exit; \
 :                                               ; \
 rm -f root_checkout.sh     || exit 1;   \
 "' > $GLOBAL_LOG 2>&1
