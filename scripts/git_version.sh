96f9adf1e3494d0dde7106d93cf6e2e59ad02eb6
Ed Griffiths <edgrif@sanger.ac.uk>
2011-05-24 12:12:53 +0100

    "add script to retrieve git branch/tag/version information."

---
+++ scripts/git_version.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Get the git branch and tag/change info. for current branch.
+#
+#
+# Note that all error output must be sent to stderr so as not to interfer
+# with version information which is returned on stdout.
+#
+
+# Usage: message_err Your Message
+function message_err
+{
+    now=$(date +%H:%M:%S)
+    echo "[$SCRIPT_NAME ($now)] $*" >&2
+}
+
+
+
+RC=0
+
+SCRIPT_NAME=$(basename $0)
+
+
+git_info=''
+
+git_version=`git describe` || { message_err 'git describe failed' ; exit 1 ; }
+
+
+# git branch lists branches like this (* shows current branch):
+#
+# tviewsrv[edgrif]59: git branch
+# * develop
+#   master
+#
+# code finds the asterisk and then outputs the current branch
+#
+git_branch=`git branch | 
+while read first second
+  do
+
+  if [ "$first" == '*' ] ; then
+      echo $second
+      break
+  fi
+
+  done` || { message_err 'git branch failed' ; exit 1 ; }
+
+
+if [ -n $git_version ] && [ -n $git_branch ] ; then
+    echo "$git_branch-$git_version"
+else
+   message_err 'Could not find git version and branch.' ; exit 1 ; 
+fi
+
+exit $RC 
+
