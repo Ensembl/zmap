c820298376f0f88cab4013b1a3f2242aa207e9cd
Ed Griffiths <edgrif@sanger.ac.uk>
2011-05-24 15:43:50 +0100

    "tidy up."

--- scripts/zmap_build_one_off.sh
+++ scripts/zmap_build_one_off.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-# ================= README ==================
-
+#
 # Build zmap for all architectures specified in $ZMAP_BUILD_MACHINES.
 #
 # Builds/logs are in ONE_OFF.BUILD file/directory.
@@ -13,7 +11,8 @@
 #                           Docs created    no
 #                        Docs checked in    no
 #
-
+# Error reporting gets done by the build_run.
+#
 
 RC=0
 
@@ -23,7 +22,6 @@ OUTPUT=$BUILD_PREFIX
 ERROR_RECIPIENT='edgrif@sanger.ac.uk'
 
 
-# Error reporting gets done by the build_run.
 ./build_run.sh -a $ERROR_RECIPIENT -e -l $OUTPUT $BUILD_PREFIX || RC=1
 
 
