fb8f9c02c2cf39756ed403f593f8d44dbc3c0220
Ed Griffiths <edgrif@sanger.ac.uk>
2011-05-26 16:46:10 +0100

    "try to fix up common function loading."

--- scripts/build_run.sh
+++ scripts/build_run.sh
@@ -229,6 +229,7 @@ fi
 
 
 
+
 #
 # OK, off we go......
 #
@@ -294,7 +295,7 @@ rm -f root_checkout.sh     || exit 1;   \
 cat - > root_checkout.sh   || exit 1;   \
 chmod 755 root_checkout.sh || _rm_exit; \
 : Change the variables in next line   ; \
-./root_checkout.sh '$CMD_OPTIONS' || _rm_exit; \
+BASE_DIR='$SCRIPT_DIR' ./root_checkout.sh '$CMD_OPTIONS' || _rm_exit; \
 :                                     ; \
 rm -f root_checkout.sh || exit 1;   \
 "' >> $GLOBAL_LOG 2>&1
