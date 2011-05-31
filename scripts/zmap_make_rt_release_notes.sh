92181fd5271ca2aba8d0de0668815942946f461e
Ed Griffiths <edgrif@sanger.ac.uk>
2011-05-31 12:14:01 +0100

    "fix up git log format."

--- scripts/zmap_make_rt_release_notes.sh
+++ scripts/zmap_make_rt_release_notes.sh
@@ -491,7 +491,7 @@ cat $TMP_CHANGES_FILE > /dev/null || zmap_message_exit "$TMP_CHANGES_FILE doesn'
 # process using perl one-liner
 #perl -lne "s!.*\*!  </li>\n  <li>!; print if !/$CVS_YEAR/" $TMP_CHANGES_FILE >> $RELEASE_NOTES_OUTPUT
 #perl -nle 'print qq(</li>\n\n<li>\n$1) if /^[[:blank:]]+([^[:space:]].*)$/;'  $TMP_CHANGES_FILE >> $RELEASE_NOTES_OUTPUT
-perl -nle 'print qq(</li>\n\n<li>\n$_)'  $TMP_CHANGES_FILE >> $RELEASE_NOTES_OUTPUT
+perl -nle 'print qq(</li>\n\n<li>\n$*)'  $TMP_CHANGES_FILE >> $RELEASE_NOTES_OUTPUT
 
 
 rm -f $TMP_CHANGES_FILE  || zmap_message_exit "Couldn't remove $TMP_CHANGES_FILE! Odd considering rm -f use."
@@ -591,7 +591,7 @@ EOF
     
     # process using perl one-liner (Jeremy Henty's suggestion):
     #perl -nle 'print qq(</li>\n\n<li>\n$1) if /^[[:blank:]]+([^[:space:]].*)$/;'  $TMP_CHANGES_FILE >> $RELEASE_NOTES_OUTPUT
-    perl -nle 'print qq(</li>\n\n<li>\n$_)'  $TMP_CHANGES_FILE >> $RELEASE_NOTES_OUTPUT
+    perl -nle 'print qq(</li>\n\n<li>\n$*)'  $TMP_CHANGES_FILE >> $RELEASE_NOTES_OUTPUT
 
     rm -f $TMP_CHANGES_FILE  || zmap_message_exit "Couldn't remove $TMP_CHANGES_FILE! Odd considering rm -f use."
 
