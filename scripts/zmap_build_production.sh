ae1adc7abd0075b9fe67156fa522598d2291c6cd
Ed Griffiths <edgrif@sanger.ac.uk>
2011-08-01 15:57:49 +0100

    add production build script.

---
+++ scripts/zmap_build_production.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Build zmap for all architectures specified in $ZMAP_BUILD_MACHINES.
+#
+# Builds/logs are in ONE_OFF.BUILD file/directory.
+#
+# Main Build Parameters:
+#
+# Version incremented        no
+#        Docs created        no
+#     Docs checked in        no
+#  
+# Error reporting gets done by the build_run.
+#
+
+RC=0
+
+
+BUILD_PREFIX='PRODUCTION'
+ERROR_ID='edgrif@sanger.ac.uk'
+
+
+./build_run.sh -a $ERROR_ID -b production -d -m $BUILD_PREFIX || RC=1
+
+
+exit $RC
