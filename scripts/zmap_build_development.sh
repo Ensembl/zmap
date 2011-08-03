fc4c934140fb45f0124c46f7b5ed22da24b3e75b
Ed Griffiths <edgrif@sanger.ac.uk>
2011-08-03 09:19:21 +0100

    "tmp change to error message recipient for testing."

--- scripts/zmap_build_development.sh
+++ scripts/zmap_build_development.sh
@@ -17,11 +17,9 @@ RC=0
 
 
 BUILD_PREFIX='DEVELOPMENT'
-#SUB_DIR="$BUILD_PREFIX"
-ERROR_ID='zmapdev@sanger.ac.uk'
+ERROR_ID='edgrif@sanger.ac.uk'
 
 
-#./build_run.sh -a $ERROR_ID -d -g -m -n -t -z $SUB_DIR $BUILD_PREFIX || RC=1
 ./build_run.sh -a $ERROR_ID -d -g -m -n $BUILD_PREFIX || RC=1
 
 
