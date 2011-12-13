bf955e57b3fe1be309a80f45b36fb07df9a54fb6
Ed Griffiths <edgrif@sanger.ac.uk>
2011-12-13 10:31:16 +0000

    "redo state stuff, clear up/rationalise structs."

--- src/zmapRemoteControl/zmapRemoteControl_.h
+++ src/zmapRemoteControl/zmapRemoteControl_.h
@@ -27,7 +27,7 @@
  * Description: Private header for remote control package.
  *
  * HISTORY:
- * Last edited: Jun 28 13:03 2011 (edgrif)
+ * Last edited: Dec  1 15:58 2011 (edgrif)
  * Created: Fri Sep 24 14:44:59 2010 (edgrif)
  * CVS info:   $Id$
  *-------------------------------------------------------------------
@@ -40,241 +40,170 @@
 
 
 
-#define DATA_RECEIVING_ATOM "REMOTECONTROL_DATA_ATOM"
-
-
-
 /* Using the XXX_LIST macros to declare enums means we get lots of enum functions for free (see zmapEnum.h). */
 
-/* Is the request functioning as a client or server ? */
+
+/* Is the remote object idle, a client or a server ? */
 #define REMOTE_TYPE_LIST(_)                                                                                 \
-_(REMOTE_TYPE_INVALID,            , "invalid"           , "invalid type."                             , "") \
-_(REMOTE_TYPE_CLIENT,             , "client"            , "Request client."                           , "") \
-_(REMOTE_TYPE_SERVER,             , "server"            , "Request server."                           , "")
+_(REMOTE_TYPE_INVALID,            , "invalid"           , "Remote is invalid !"                             , "") \
+_(REMOTE_TYPE_IDLE,               , "idle"              , "Remote is idle."                             , "") \
+_(REMOTE_TYPE_CLIENT,               , "client"              , "Remote is acting as client."    , "") \
+_(REMOTE_TYPE_SERVER,               , "server"              , "Remote is acting as server."         , "")
 
 ZMAP_DEFINE_ENUM(RemoteType, REMOTE_TYPE_LIST) ;
 
 
-/* Using this macro to declare the enum means we get lots of enum functions for free (see zmapEnum.h). */
-#define CLIENT_STATE_LIST(_)                                                                                 \
-_(CLIENT_STATE_INVALID,            , "invalid"           , "invalid mode "                             , "") \
-_(CLIENT_STATE_INACTIVE,           , "inactive"          , "Waiting to make or receive a request."     , "") \
-_(CLIENT_STATE_SENDING,            , "client-sending"    , "Peer is client and sending a request."     , "") \
-_(CLIENT_STATE_WAITING,	           , "client-waiting"    , "Peer is client and waiting for a response.", "") \
-_(CLIENT_STATE_RETRIEVING,         , "client-retrieving" , "Peer is client and retrieving a response." , "")
+/* Remote Control state. */
+#define REMOTE_STATE_LIST(_)						\
+_(REMOTE_STATE_INVALID,            ,					\
+  "invalid"           , "Invalid state !"                             , "") \
+_(REMOTE_STATE_IDLE,               ,				\
+  "idle"              , "Peer - not waiting for a request/not making a request."     , "") \
+_(REMOTE_STATE_CLIENT_WAIT_GET,     ,\
+  "client-waiting-for-get"    , "Peer/client - contacted server, waiting for server ask for request."     , "") \
+_(REMOTE_STATE_CLIENT_WAIT_REQ_ACK,	   ,\
+  "client-waiting-for-req-ack"    , "Peer/client - sent request waiting for server to acknowledge.", "") \
+_(REMOTE_STATE_CLIENT_WAIT_REPLY,  ,\
+  "client-waiting-for-reply" , "Peer/client - waiting for server to signal it has a reply to send." , "") \
+_(REMOTE_STATE_CLIENT_WAIT_SEND,  ,\
+  "client-waiting-for-send" , "Peer/client - asked for reply, waiting for server to send it." , "") \
+_(REMOTE_STATE_CLIENT_WAIT_REPLY_ACK,  ,\
+  "client-waiting-for-reply-ack" , "Peer/client - waiting for server to acknowledge end of transaction." , "") \
+_(REMOTE_STATE_SERVER_WAIT_SIGNAL_NEW,  ,\
+  "server-waiting" , "Peer/server - waiting for client to signal they have a new request."  , "") \
+_(REMOTE_STATE_SERVER_WAIT_REQ,  ,\
+  "server-waiting-for-request" , "Peer/server - waiting for client to send request."  , "") \
+_(REMOTE_STATE_SERVER_WAIT_REQ_ACK,  ,\
+  "server-waiting-for-req-ack" , "Peer/server - told client we have request, waiting for acknowledgement."  , "") \
+_(REMOTE_STATE_SERVER_REQ_PROCESS,     ,\
+  "server-processing"    , "Peer/server - Contacted client to say we have reply, now processing reply."    , "") \
+_(REMOTE_STATE_SERVER_WAIT_GET,     ,\
+  "server-waiting-for-get"    , "Peer/server - Sent reply, waiting for client to acknowledge they have it."    , "") \
+_(REMOTE_STATE_SERVER_WAIT_REPLY_ACK,     ,\
+  "server-waiting-for-reply-ack"    , "Peer/server - Sent reply, waiting for client to acknowledge they have it."    , "") \
+_(REMOTE_STATE_RESETTING,          ,\
+  "resetting"         , "Peer - Resetting to idle state after error."      , "") \
+_(REMOTE_STATE_DYING,              ,\
+  "dying"             , "Peer - Dying after error."                        , "")
 
-ZMAP_DEFINE_ENUM(ClientControlState, CLIENT_STATE_LIST) ;
+
+ZMAP_DEFINE_ENUM(RemoteControlState, REMOTE_STATE_LIST) ;
 
 
-/* Using this macro to declare the enum means we get lots of enum functions for free (see zmapEnum.h). */
-#define SERVER_STATE_LIST(_)                                                                                 \
-_(SERVER_STATE_INVALID,            , "invalid"           , "invalid mode "                             , "") \
-_(SERVER_STATE_INACTIVE,           , "inactive"          , "Waiting to make or receive a request."     , "") \
-_(SERVER_STATE_RETRIEVING,         , "server-retrieving" , "Peer is server and retrieving a request."  , "") \
-_(SERVER_STATE_PROCESSING,         , "server-processing" , "Peer is server and processing a request."  , "") \
-_(SERVER_STATE_SENDING,	           , "server-sending"    , "Peer is server and sending a response."    , "")
-
-ZMAP_DEFINE_ENUM(ServerControlState, SERVER_STATE_LIST) ;
 
 
-/* Describes either client or server requests. */
-typedef struct AnyRequestStructName
+/* Usual common struct, all the below structs must have the same members as these first. */
+typedef struct RemoteAnyStructName
 {
-  RemoteType request_type ;
-
-  ZMapRemoteControl remote_control ;			    /* Parent remote control object. */
-
-  /* The _current_ peers atom for receiving requests and responses, will be rewritten for
-   * each new request/response, may change or not depending if it's a new client. */
-  GdkAtom peer_atom ;
+  RemoteType remote_type ;
+} RemoteAnyStruct, *RemoteAny ;
 
 
-#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
-  GdkNativeWindow peer_window ;
-#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
-
-
-  gulong curr_req_time ;				    /* Time curr request initiated. */
-
-  gboolean has_timed_out ;				    /* Have we timed out ? */
-
-
-
-  char *request ;
-
-} AnyRequestStruct, *AnyRequest ;
-
-
-/* Data for client requests. */
-typedef struct ClientRequestStructName
+/* Remote is idle, i.e. not acting as a server or a client. */
+typedef struct RemoteIdleStructName
 {
-  /* Section common to all request structs (see AnyRequest). */
-
-  RemoteType request_type ;
-
-  ZMapRemoteControl remote_control ;			    /* Parent remote control object. */
-
-  /* The current peers atom for receiving requests and responses, will be rewritten for
-   * each new request/response, may change or not depending if it's a new client. */
-  GdkAtom peer_atom ;
-
-  GdkNativeWindow peer_window ;
-
-  gulong curr_req_time ;				    /* Time curr request initiated. */
-
-  gboolean has_timed_out ;				    /* Have we timed out ? */
-
-  char *request ;
+  RemoteType remote_type ;
+} RemoteIdleStruct, *RemoteIdle ;
 
 
-  /* Client specific section. */
 
-  ClientControlState state ;				    /* Current state of this peer. */
-
-} ClientRequestStruct, *ClientRequest ;
-
-
-/* Data for individual requests. */
-typedef struct ServerRequestStructName
+/* Control block for when acting as a server, i.e. we have received a request from the peer. */
+typedef struct RemoteSelfStructName
 {
-  /* Section common to all request structs (see AnyRequest). */
+  RemoteType remote_type ;
 
-  RemoteType request_type ;				    /* Must be first to match AnyRequest. */
+  /* Our atom for receiving peer requests and returning a response, stays the same for
+   * the life of this control struct. */
+  GdkAtom our_atom ;
+  char *our_atom_string ;				    /* Cached because it's a pain to get the string. */
+  GtkClipboard *our_clipboard ;
 
-  ZMapRemoteControl remote_control ;			    /* Parent remote control object. */
+  guint32 timeout_ms ;					    /* timeout in milliseconds. */
 
-  /* The _current_ peers atom for receiving requests and responses, will be rewritten for
-   * each new request/response, may change or not depending if it's a new client. */
-  GdkAtom peer_atom ;
-
-  GdkNativeWindow peer_window ;
-
-  gulong curr_req_time ;				    /* Time curr request initiated. */
-
-  gboolean has_timed_out ;				    /* Have we timed out ? */
-
-  char *request ;
+  /* Our record of the two way traffic. */
+  char *their_request ;
+  char *our_reply ;
 
 
-  /* Server specific section. */
+  /* Callback functions specified by caller to receive requests and subsequent replies/errors. */
+  ZMapRemoteControlRequestHandlerFunc request_func ;
+  gpointer request_func_data ;
 
-  ServerControlState state ;				    /* Current state of this peer. */
+  ZMapRemoteControlTimeoutHandlerFunc timeout_func ;
+  gpointer timeout_func_data ;
 
-} ServerRequestStruct, *ServerRequest ;
-
+} RemoteSelfStruct, *RemoteSelf ;
 
 
 
+/* Control block for when acting as a client, i.e. we are making a request. */
+typedef struct RemotePeerStructName
+{
+  RemoteType remote_type ;
 
-/* The main remote control struct. */
+  /* Their 'request' atom for receiving our requests and giving us responses, stays the same for
+   * the life of this control struct. */
+  GdkAtom their_atom ;
+  char *their_atom_string ;				    /* Cached because it's a pain to get the string. */
+  GtkClipboard *their_clipboard ;
+
+  guint32 timeout_ms ;					    /* timeout in milliseconds. */
+
+  /* Our record of the two way traffic. */
+  char *our_request ;
+  char *their_reply ;
+
+  /* Callback functions specified by caller to receive requests and subsequent replies/errors. */
+  ZMapRemoteControlReplyHandlerFunc reply_func ;
+  gpointer reply_func_data ;
+
+  ZMapRemoteControlTimeoutHandlerFunc timeout_func ;
+  gpointer timeout_func_data ;
+
+} RemotePeerStruct, *RemotePeer ;
+
+
+
+
+/* The main remote control struct which contains the self and peer interfaces. */
 typedef struct ZMapRemoteControlStructName
 {
   /* We keep a magic pointer because these structs are handed to us by external callers
    * so we need to be able to check if they are valid. */
   ZMapMagic magic ;
 
-  /* Our callers "name", good for debug messages. */
+  /* Our current state. */
+  RemoteControlState state ;
+  RemoteAny curr_remote ;				    /* points to idle, self or peer. */
+
+  /* The idle, self (== when are acting as server) and peer (== when we are acting as client) interfaces. */
+  RemoteIdle idle ;
+  RemoteSelf self ;
+  RemotePeer peer ;
+
+
+  /* Applications "name", good for debug messages. */
   char *app_id ;
 
-  /* string prefix for atom names for init/requests. */
-  char *app_prefix ;
-
-  gboolean show_all_events ;				    /* If TRUE then print to stderr all
-							       events for our main window. */
-
-
-#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
-  /* NOT NEEDED NOW I THINK.... */
-  /* This is the widget which we attach all properties to. */
-  GtkWidget *our_widget ;
-  GdkNativeWindow our_window ;				    /* Cache this as it's a pain to derive
-							       from widget. */
-#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
+  /* atom representation of target type for data. */
+  GdkAtom target_atom ;
 
 
 
-  /* Atoms for communication. */
-
-  /* Our 'initiate' atom, this is the atom the client must first take ownership
-   * of to initiate any type of request. Stays the same for the life of this control struct. */
-  GdkAtom our_init_atom ;
-  char *our_init_atom_string ;				    /* Cached because it's a pain to get the string. */
-  GtkClipboard *our_init_clipboard ;
-
-
-  /* Our 'request' atom for receiving requests and responses from other peers, stays the same for
-   * the life of this control struct. */
-  GdkAtom our_request_atom ;
-  char *our_request_atom_string ;			    /* Cached because it's a pain to get the string. */
-  GtkClipboard *our_request_clipboard ;
-
-#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
-  /* NOT NEEDED NOW I THINK */
-  GdkAtom data_format_atom ;				    /* Still needed ???? */
-  gint data_format_bits ;
-#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
-
-
-
-  /* The current request, NULL when there isn't one. Can be client or server. */
-  AnyRequest curr_request ;
-
-
-
-
-  /* THESE ARE ALL GOING TO CHANGE.... */
-
-
-  /* Callback functions specified by caller to receive requests and subsequent replies/errors. */
-  ZMapRemoteControlRequestHandlerFunc request_func ;
-  gpointer request_data ;
-
-  ZMapRemoteControlReplyHandlerFunc reply_func ;
-  gpointer reply_data ;
-
-  guint32 timeout_ms ;					    /* timeout in milliseconds. */
-  ZMapRemoteControlTimeoutHandlerFunc timeout_func ;
-  gpointer timeout_data ;
-
-  ZMapRemoteControlErrorFunc err_func ;
+  /* where to send error messages, can be overridden by user. */
+  ZMapRemoteControlErrorReportFunc err_func ;
   gpointer err_data ;
 
-
-  /* internal callback function instance ids, ids are used to remove callbacks. */
-
-  gulong test_notify_id ;
-
-
-  /* general callbacks */
-  gulong all_events_id ;
   guint timer_source_id ;
-
-
-  /* Server callbacks */
-  gulong select_clear_id ;
-  gulong select_notify_id ;
-  gulong server_message_id ;
-
-
-  /* client callbacks */
-  gulong select_request_id ;
-  gulong client_message_id ;
-  gulong client_select_notify_id ;
 
 
 } ZMapRemoteControlStruct ;
 
 
 
+/* Function decs generated by macros, see zmapEnum.h for func. prototypes. */
 ZMAP_ENUM_AS_EXACT_STRING_DEC(remoteType2ExactStr, RemoteType) ;
-ZMAP_ENUM_AS_EXACT_STRING_DEC(clientState2ExactStr, ClientControlState) ;
-ZMAP_ENUM_AS_EXACT_STRING_DEC(serverState2ExactStr, ServerControlState) ;
-
-
-
-
-
-
+ZMAP_ENUM_AS_EXACT_STRING_DEC(remoteState2ExactStr, RemoteControlState) ;
 
 
 
