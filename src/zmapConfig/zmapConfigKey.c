1073ddb12679cbf8dd0b0da4bb52065f20b80e7a
Ed Griffiths <edgrif@sanger.ac.uk>
2012-03-16 10:34:56 +0000

    "formatting only."

--- src/zmapConfig/zmapConfigKey.c
+++ src/zmapConfig/zmapConfigKey.c
@@ -22,7 +22,7 @@
  *
  *      Ed Griffiths (Sanger Institute, UK) edgrif@sanger.ac.uk,
  *        Roy Storey (Sanger Institute, UK) rds@sanger.ac.uk,
- *     Malcolm Hinsley (Sanger Institute, UK) mh17@sanger.ac.uk
+ *   Malcolm Hinsley (Sanger Institute, UK) mh17@sanger.ac.uk
  *
  * Description:
  *
@@ -31,10 +31,6 @@
  */
 
 #include <ZMap/zmap.h>
-
-
-
-
 
 
 #include <string.h>		/* memset */
