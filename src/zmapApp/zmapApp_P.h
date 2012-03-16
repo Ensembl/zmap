01b986ccc8eee32b293288ef9484a99e049931cf
Ed Griffiths <edgrif@sanger.ac.uk>
2012-03-16 10:32:05 +0000

    "add new callbacks, tidy up."

--- src/zmapApp/zmapApp_P.h
+++ src/zmapApp/zmapApp_P.h
@@ -1,3 +1,4 @@
+/*  Last edited: Feb 29 16:15 2012 (edgrif) */
 /*  File: zmapApp.h
  *  Author: Ed Griffiths (edgrif@sanger.ac.uk)
  *  Copyright (c) 2006-2011: Genome Research Ltd.
@@ -70,43 +71,45 @@ typedef void (*ZMapAppCBFunc)(void *cb_data) ;
 
 /*                   NEW XREMOTE                                  */
 
+
 typedef struct _ZMapAppRemoteStruct
 {
   char *app_id ;					    /* zmap app name. */
   char *app_unique_id ;
 
-#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
-  char *peer_unique_id ;
-#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
-
-
   char *peer_name ;
   char *peer_clipboard ;
 
-
-#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
-  /* Used to provide a unique id for each request. */
-  int request_id_num ;
-  GString *request_id ;
-#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
-
+  /* Currently shutdown is the only command that needs to run _after_ we are sure
+   * the peer has received our reply.....if it turns out other commands need to do
+   * this then we will need to store their names/params here instead of this simple
+   * boolean. */
+  gboolean deferred_shutdown ;
 
   ZMapAppCBFunc exit_routine ;				    /* Called to exit application after we
 							       have signalled to peer we are going. */
 
   ZMapRemoteControl remote_controller ;
 
+  /* Default id to send commands to. Initially NULL meaning only app level commands
+   * will work, gets filled in with first zmap.view.window created. */
+  ZMapAppRemoteViewIDStruct default_view_id ;
 
-  /* Incoming request FROM a peer, remote_reply_func() is RemoteControl callback that gets called
+
+  /* Incoming request FROM a peer. */
+
+  /* remote_reply_func() is RemoteControl callback that gets called
    * when we receive a reply from a zmap sub-system to this request. */
-
   char *curr_command ;
   char *curr_request ;					    /* DO WE NEED TO CACHE THIS ?? */
 
-  ZMapRemoteControlCallWithReplyFunc remotecontrol_request_done_func ;
-  void *remotecontrol_request_done_data ;
+
+  ZMapAppRemoteViewIDStruct curr_view_id ;
 
 
+  /* Remote Control callback function that we call with our reply to a request. */
+  ZMapRemoteControlReturnReplyFunc return_reply_func ;
+  void *return_reply_func_data ;
 
 
   /* wrong ??? */
@@ -114,28 +117,12 @@ typedef struct _ZMapAppRemoteStruct
    * that gets called with curr_reply. */
   char *curr_reply ;
 
-  ZMapRemoteControlCallWithReplyFunc zmap_reply_func ;
-  void *zmap_reply_data ;
 
-
-
-
-  /* Outgoing request to a peer */
+  /* Outgoing request TO a peer */
 
   /* App function to be called with peer's reply to the request. */
   ZMapRemoteAppProcessReplyFunc process_reply_func ;
   gpointer process_reply_func_data ;
-
-
-  /* remote control function app needs to call to say it's processed the reply from the peer. */
-  ZMapRemoteControlCallReplyDoneFunc reply_done_func ;
-  void *reply_done_data ;
-
-
-  /* As we create new views we need to add/delete them, we keep a hash of views to zmap windows here. */
-  GHashTable *view2zmap ;
-
-
 
 } ZMapAppRemoteStruct, *ZMapAppRemote ;
 
@@ -187,13 +174,18 @@ typedef struct _ZMapAppContextStruct
    * to be executed once the app has an X Window, e.g. remote control. */
   gulong mapCB_id ;
 
+
   /* The new xremote. */
   ZMapAppRemote remote_control ;
+
 
   /* old xremote stuff... */
   gulong property_notify_event_id;
   ZMapXRemoteObj xremote_client ;			    /* The external program we are sending
 							       commands to. */
+  gboolean xremote_debug ;				    /* Turn on/off debugging for xremote connections. */
+
+
 
   gboolean show_mainwindow ;				    /* Should main window be displayed. */
 
@@ -206,9 +198,6 @@ typedef struct _ZMapAppContextStruct
   char *script_dir;					    /* where scripts are kept for the pipeServer module
 							     * can be set in [ZMap] or defaults to run-time directory
 							     */
-
-  gboolean xremote_debug ;				    /* Turn on/off debugging for xremote connections. */
-
 
 } ZMapAppContextStruct, *ZMapAppContext ;
 
@@ -247,12 +236,12 @@ ZMapRemoteAppMakeRequestFunc zmapAppRemoteControlGetRequestCB(void) ;
 gboolean zmapAppRemoteControlCreate(ZMapAppContext app_context, char *peer_name, char *peer_clipboard) ;
 gboolean zmapAppRemoteControlInit(ZMapAppContext app_context) ;
 gboolean zmapAppRemoteControlConnect(ZMapAppContext app_context) ;
-gboolean zmapAppRemoteControlDisconnect(ZMapAppContext app_context) ;
+gboolean zmapAppRemoteControlDisconnect(ZMapAppContext app_context, gboolean app_exit) ;
 gboolean zmapAppRemoteControlPing(ZMapAppContext app_context) ;
 void zmapAppRemoteControlSetExitRoutine(ZMapAppContext app_context, ZMapAppCBFunc exit_routine) ;
 gboolean zmapAppProcessAnyRequest(ZMapAppContext app_context, char *request,
 				  ZMapRemoteAppReturnReplyFunc replyHandlerFunc) ;
-gboolean zmapAppProcessAnyReply(ZMapAppContext app_context, char *reply) ;
+void zmapAppProcessAnyReply(ZMapAppContext app_context, char *reply) ;
 void zmapAppRemoteControlOurRequestEndedCB(void *user_data) ;
 void zmapAppRemoteControlTheirRequestEndedCB(void *user_data) ;
 void zmapAppRemoteControlDestroy(ZMapAppContext app_context) ;
