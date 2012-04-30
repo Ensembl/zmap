dd7d6058b6f51a4b0f5172cff9289c971dc19e2e
Gemma Barson <gb10@sanger.ac.uk>
2012-04-30 13:43:12 +0100

    Added hard-coded styles to methods2style script
    Removed -format argument, because we no longer support ?ZMap_Style in acedb

--- scripts/methods2style.pl
+++ scripts/methods2style.pl
@@ -103,7 +103,6 @@ use Data::Dumper;
       midblue => '#56b2de',
     } ;
 
-
     my $ACEDB_MAG_FACTOR = 6.0 ;
     my $ACEDB_MAG_FACTOR_ALIGN = 16.0 ;
 
@@ -113,7 +112,7 @@ use Data::Dumper;
         my ($self) = @_;
 
 #        my $txt = qq`Type\n{\n`;
-	my $txt ;
+	my $txt = '';
 
         # For transcript modes, the colours will be for the border, 
         # otherwise they will the fill colour (default).
@@ -132,7 +131,6 @@ use Data::Dumper;
             my $zmap  = $keyword_convert->{$txtprop} || next;
 
             my $value = lc($self->$txtprop())    || next;
-            $txt .= qq`[$value]\n`;
 
             # Determine the mode from the name.
             if ($self->name =~ m/halfwise/i ||
@@ -146,7 +144,8 @@ use Data::Dumper;
                 $self->name =~ m/transcript/i ||
                 $self->name =~ m/history/i ||
                 $self->name =~ m/jigsaw/i ||
-                $self->name =~ m/rnaseq/i ||
+                $self->name =~ m/rnaseq\./i ||
+                $self->name =~ m/rnaseq_hillier/i ||
                 $self->name =~ m/mgene/i ||
                 $self->name =~ m/genefinder/i ||
                 $self->name =~ m/fgenesh/i ||
@@ -155,8 +154,9 @@ use Data::Dumper;
                 $self->name =~ m/three_prime_utr/i ||
                 $self->name =~ m/pseudogene/i)
 	      {
-	      $txt .= qq`mode=transcript\n`;
-              $colour_type = 'border';
+                $txt .= qq`[$value]\n`;
+                $txt .= qq`mode=transcript\n`;
+                $colour_type = 'border';
 	      }
 	    elsif ($self->name =~ m/trembl/i ||
                    $self->name =~ m/swissprot/i ||
@@ -166,12 +166,37 @@ use Data::Dumper;
                    $self->name =~ m/ditags/i ||
                    $self->name =~ m/est/i)
 	      {
-	      $txt .= qq`mode=alignment\n`;
-              $mag_factor = $ACEDB_MAG_FACTOR_ALIGN;
+                $txt .= qq`[$value]\n`;
+                $txt .= qq`mode=alignment\n`;
+                $mag_factor = $ACEDB_MAG_FACTOR_ALIGN;
 	      }
+            elsif ($self->name =~ m/gf_splice/i ||
+                   $self->name =~ m/root/i ||
+                   $self->name =~ m/feat/i ||
+                   $self->name =~ m/DNA/i ||
+                   $self->name =~ m/Show Translation/i ||
+                   $self->name =~ m/3 Frame/i ||
+                   $self->name =~ m/3 Frame Translation/i ||
+                   $self->name =~ m/gene_finder_feat/i ||
+                   $self->name =~ m/GeneFinderFeatures/i ||
+                   $self->name =~ m/ATG/i ||
+                   $self->name =~ m/GF_ATG/i ||
+                   $self->name =~ m/GF_coding_seg/i ||
+                   $self->name =~ m/hexExon/i ||
+                   $self->name =~ m/hexExon_span/i ||
+                   $self->name =~ m/hexIntron/i ||
+                   $self->name =~ m/heatmap/i ||
+                   $self->name =~ m/histogram/i ||
+                   $self->name =~ m/homology-glyph/i ||
+                   $self->name =~ m/line/i ||
+                   $self->name =~ m/nc-splice-glyph/i)
+              {
+                return $txt; # inclued in hard-coded styles; nothing to do
+              }
             else
 	      {
-	      $txt .= qq`mode=basic\n`;
+                $txt .= qq`[$value]\n`;
+                $txt .= qq`mode=basic\n`;
 	      }
         }
 
@@ -280,203 +305,184 @@ use Data::Dumper;
 
         return $txt;
     }
-
-
-    # same as zmap_string but output in .ace format rather than ini-like format 
-    sub Hum::Ace::Method::zmap_string_ace{
-
-        my ($self) = @_;
-
-#        my $txt = qq`Type\n{\n`;
-	my $txt ;
-
-        # For transcript modes, the colours will be for the border, 
-        # otherwise they will the fill colour (default).
-        my $colour_type = 'fill';
-
-        # use a different magnification factor for alignments
-        my $mag_factor = $ACEDB_MAG_FACTOR;
-
-        # in the case of each of the following the arrays are lists of
-        # perl methods avaiable for the method.  These get translated 
-        # to be the correct zmap properties for the type.
-
-
-        foreach my $txtprop(qw(name)){
-
-            my $zmap  = $keyword_convert_ace->{$txtprop} || next;
-
-            my $value = lc($self->$txtprop())    || next;
-            $txt .= qq`ZMap_Style : "$value"\n`;
-
-
-            # Determine the mode from the name.
-            if ($self->name =~ m/halfwise/i ||
-                $self->name =~ m/genscan/i ||
-                $self->name =~ m/retained_intron/i ||
-                $self->name =~ m/cds/i ||
-                $self->name =~ m/estgene/i ||
-                $self->name =~ m/ensembl/i  ||
-                $self->name =~ m/coding/i ||
-                $self->name =~ m/curated/i ||
-                $self->name =~ m/transcript/i ||
-                $self->name =~ m/history/i ||
-                $self->name =~ m/jigsaw/i ||
-                $self->name =~ m/rnaseq/i ||
-                $self->name =~ m/mgene/i ||
-                $self->name =~ m/genefinder/i ||
-                $self->name =~ m/fgenesh/i ||
-                $self->name =~ m/genblastg/i ||
-                $self->name =~ m/twinscan/i ||
-                $self->name =~ m/three_prime_utr/i ||
-                $self->name =~ m/pseudogene/i)
-	      {
-	      $txt .= qq`mode "transcript"\n`;
-              $colour_type = 'border';
-	      }
-	    elsif ($self->name =~ m/trembl/i ||
-                   $self->name =~ m/swissprot/i ||
-                   $self->name =~ m/vertebrate_rna/i || 
-                   $self->name =~ m/refseq/i ||
-                   $self->name =~ m/blast/i ||
-                   $self->name =~ m/ditags/i ||
-                   $self->name =~ m/est/i)
-	      {
-	      $txt .= qq`mode "alignment"\n`;
-              $mag_factor = $ACEDB_MAG_FACTOR_ALIGN;
-	      }
-            else
-	      {
-	      $txt .= qq`mode "basic"\n`;
-	      }
-
-        }
-
-#        foreach my $txtprop(qw(strand_sensitive frame_sensitive show_up_strand)){
-#            my $zmap  = $keyword_convert_ace->{$txtprop} || next;
-#            $txt .= qq`$zmap = true\n`;
-#        }
-
-        foreach my $txtprop(qw(remark)){
-            my $zmap  = $keyword_convert_ace->{$txtprop} || next;
-            my $value = $self->$txtprop()    || next;
-            $txt .= qq`$zmap "$value"\n`;
-        }
-
-        foreach my $txtprop(qw(color cds_color)) {
-            my $zmap  = $keyword_convert_ace->{$txtprop};
-            my $index = $self->$txtprop();
-            my $value;
-
-            if ($index) {
-                $value = $colour_convert->{lc($index)};
-            }
-
-            if ($zmap && $value) {
-                $txt .= qq`$zmap normal $colour_type "$value"\n`;
-
-                # if setting the border colour, set the same border colour
-                # when selected, and set the fill to white.
-                if ($colour_type =~ m/border/i){
-                  $txt .= qq`$zmap selected border "$value"\n`;
-                  $txt .= qq'$zmap normal fill "#ffffff"\n';
-                }
-            }
-
-            if ($txtprop =~ m/^color/i && (!$value || $colour_type !~ m/border/i)){
-                # Set a default black border for any features that don't have it set
-              $txt .= qq'$zmap normal border "#000000"\n';
-              $txt .= qq'$zmap selected border "#000000"\n';
-            }
-                
-            # specify the fill colour when the feature is selected
-            # to do: what should selection colour be? I've hard-coded 
-            # values for now (light yellow). Note: only specify the
-            # selection colour for "cds_color" if the value exists 
-            # otherwise it is n/a, but for the general color property
-            # we must always set it.
-	    if ($txtprop =~ m/^color/i){
-		$txt .= qq`$zmap selected fill "#ffddcc"\n`; 
-	    }
-            elsif ($value) {
-                $txt .= qq`$zmap selected fill "#ffddcc"\n`; 
-            }
-        }
-
-        foreach my $numprop(qw(width)){
-            my $zmap  = $keyword_convert_ace->{$numprop} || next;
-            my $value = $self->$numprop()    || next;
-            $txt .= "$zmap " . ($self->$numprop * $mag_factor) . "\n";
-        }
-
-        foreach my $boolprop(qw(score_method)){
-            my $zmap = $keyword_convert_ace->{$boolprop} || next;
-            my $value = $self->$boolprop() || next;
-            $txt .= "$zmap score_by_width\n";
-        }
-
-        foreach my $arrprop(qw(score_bounds)){
-            my $zmap = $keyword_convert_ace->{$arrprop} || next;
-            if (my @values = $self->$arrprop()){
-                $txt .= "$zmap @values\n";
-            }
-        }
-
-        foreach my $boolprop(qw(strand_sensitive frame_sensitive show_up_strand)){
-            my $zmap = $keyword_convert_ace->{$boolprop} || next;
-
-	    if ($self->$boolprop){
-	        $txt .= qq`$zmap\n`;
-	    }
-        }
-
-        # Hack, stuff mode in for now...
-        $txt .= qq`bump_initial bump_mode unbump\n`;
-        $txt .= qq`bump_default bump_mode overlap\n`;
-
-#        $txt .= qq`}\n`;
-        $txt .= qq`\n`;
-
-        return $txt;
-    }
 }
 
 
 {
+    # Hard-coded predefined styles that we shove into the styles.ini file:
+    my $predefined_styles="    
+#
+# Extra acedb styles
+#
+
+[root]
+width=9.000000
+default-bump-mode=overlap
+colours=selected fill gold ; selected border black
+bump-spacing=3.000000
+bump-mode=unbump
+
+[feat]
+colours=normal fill lavender ; normal border #898994
+parent-style=root
+mode=basic
+bump-mode=unbump
+
+[DNA]
+colours=selected fill green
+
+[Show Translation]
+
+[3 Frame]
+
+[3 Frame Translation]
+colours=selected fill green
+
+[gene_finder_feat]
+width=11.000000
+parent-style=feat
+strand-specific=true
+frame-mode=only-3
+
+[GeneFinderFeatures]
+
+[ATG]
+colours=normal fill yellow2
+parent-style=gene_finder_feat
+
+[GF_ATG]
+colours=normal fill IndianRed
+parent-style=gene_finder_feat
+max-score=3.000000
+min-score=0.000000
+
+[GF_coding_seg]
+colours=normal fill gray
+parent-style=gene_finder_feat
+max-score=8.000000
+min-score=2.000000
+score-mode=width
+
+[GF_splice]
+width=30.000000
+mode=glyph
+strand-specific=true
+hide-forward-strand=true
+colours=normal fill grey
+max-score=4.000000
+glyph-mode=splice
+glyph-5=up-hook
+frame0-colours=normal fill blue; normal border blue; selected fill blue; selected border blue
+show-when-empty=false
+frame1-colours=normal fill green; normal border green; selected fill green; selected border green
+frame2-colours=normal fill red; normal border red; selected fill red; selected border red
+frame-mode=only-1
+show-reverse-strand=false
+min-score=-2.000000
+score-mode=width
+glyph-3=dn-hook
+bump-mode=unbump
+
+[hexExon]
+colours=normal fill orange
+parent-style=gene_finder_feat
+max-score=100.000000
+min-score=10.000000
+score-mode=width
+
+[hexExon_span]
+parent-style=gene_finder_feat
+
+[hexIntron]
+colours=normal fill DarkOrange
+parent-style=gene_finder_feat
+max-score=100.000000
+min-score=10.000000
+score-mode=width
+
+[heatmap]
+width=8
+graph-scale=log
+mode=graph
+graph-density-fixed=true
+strand-specific=true
+graph-density=true
+graph-mode=heatmap
+bump-style=line
+default-bump-mode=style
+colours=normal fill white; normal border black
+graph-baseline=0.000000
+max-score=1000.000000
+min-score=0.000000
+graph-density-stagger=9
+graph-density-min-bin=2
+
+[histogram]
+width=31.000000
+colours=normal fill MistyRose1 ; normal border MistyRose3
+max-mag=1000.000000
+graph-baseline=0.000000
+max-score=1000.000000
+mode=graph
+min-score=0.000000
+graph-mode=histogram
+
+[homology-glyph]
+mode=glyph
+glyph-alt-colours=normal fill YellowGreen; normal border #81a634
+colours=normal fill #e74343; normal border #dc2323
+glyph-3-rev=rev3-tri
+score-mode=alt
+glyph-3=fwd3-tri
+glyph-5=fwd5-tri
+glyph-5-rev=rev5-tri
+glyph-threshold=3
+
+[line]
+width=40.000000
+graph-scale=log
+mode=graph
+graph-density-fixed=true
+graph-density=true
+graph-mode=line
+colours=normal border blue
+graph-baseline=0.000000
+max-score=1000.000000
+min-score=0.000000
+graph-density-stagger=20
+graph-density-min-bin=4
+
+[nc-splice-glyph]
+glyph-strand=flip-y
+colours=normal fill yellow ; normal border blue
+mode=glyph
+glyph-3=up-tri
+glyph-5=dn-tri
+
+
+
+#
+# standard styles
+#
+";
+
     my $method_file = "methods.ace";
-    my $format = "keyfile";
 
     my $usage = sub { exit(exec('perldoc', $0)) };
 
     GetOptions('file=s' => \$method_file,
-               'format=s' => \$format,
                'help|h' => $usage) || $usage->();
     
     my $collection = Hum::Ace::MethodCollection->new_from_file($method_file);
     my $txt;
 
+    $txt .= qq`$predefined_styles\n`;
+
     foreach my $meth(@{$collection->get_all_Methods}){
 #        print STDERR "Found Method " . $meth->name . "\n";
-      if ($format =~ m/keyfile/i) {
-        $txt .= $meth->zmap_string;
+      $txt .= $meth->zmap_string;
       }
-      elsif ($format =~ m/ace/i) {
-        $txt .= $meth->zmap_string_ace;
-      }
-      else {
-        $txt = "Error: unrecognised format $format";
-        exit;
-      }
-  }
-
-  if ($format =~ m/ace/i) {
-    # Hack in DNA and 3ft stuff (required so that columns are shown in zmap)
-    $txt .= qq'ZMap_Style : "DNA"\n';
-    $txt .= qq'Sequence\n\n';
-    $txt .= qq'ZMap_Style : "3 Frame"\n\n';
-    $txt .= qq'ZMap_Style : "3 Frame Translation"\n\n';
-  }
-
+ 
   print $txt;
 }
 
@@ -497,11 +503,9 @@ __END__
 
 =head1 USAGE
 
- methods2style.pl -file <methods.ace> [-format keyfile|ace] -help
+ methods2style.pl -file <methods.ace> -help
 
- where <methods.ace> specifies full location of file to read and
- the -format argument indicates the type of output; 'keyfile' (default)
- for an ini-style file or 'ace' for a .ace file.
+ where <methods.ace> specifies full location of file to read
  -help shows this pod.
 
 =cut
