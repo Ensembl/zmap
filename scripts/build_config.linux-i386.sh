bab17af74058c85214f1d49c438cd17664bdb5ee
Ed Griffiths <edgrif@sanger.ac.uk>
2013-01-07 15:14:07 +0000

    "remove aceconn from external libs, now to be built in statically."

--- scripts/build_config.linux-i386.sh
+++ scripts/build_config.linux-i386.sh
@@ -12,7 +12,8 @@ CFLAGS_args="$CFLAGS_args -rdynamic"
 # This shouldn't get overwritten, note we need /software/acedb in the path
 # for libs like foocanvas, AceConn etc.
 PKG_CONFIG=$GTK_PREFIX/bin/pkg-config
-PKG_CONFIG_PATH=$GTK_PREFIX/lib/pkgconfig:/software/acedb/lib/pkgconfig
+#PKG_CONFIG_PATH=$GTK_PREFIX/lib/pkgconfig:/software/acedb/lib/pkgconfig
+PKG_CONFIG_PATH=$GTK_PREFIX/lib/pkgconfig
 
 # Make sure we look in the right place of autoconf macros...
 ACLOCAL_FLAGS="-I $GTK_PREFIX/share/aclocal"
