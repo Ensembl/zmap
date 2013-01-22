a5563fc01c15238c39741373b40c8e8f04abcec4
Ed Griffiths <edgrif@sanger.ac.uk>
2013-01-22 16:50:30 +0000

    "use new wrapper functions for setting pixels_xy."

--- src/zmapWindow/zmapWindowDrawFeatures.c
+++ src/zmapWindow/zmapWindowDrawFeatures.c
@@ -278,13 +278,14 @@ void zmapWindowDrawFeatures(ZMapWindow window, ZMapFeatureContext full_context,
   h_adj = gtk_scrolled_window_get_hadjustment(GTK_SCROLLED_WINDOW(window->scrolled_window)) ;
 
   if(!window->scroll_initialised)
-  {
-	  /* MH17: draw features does not change the zoom factor, so we only set if first time round
-	   * re-using scroll intialised flag but that should be safe
-	   */
-	foo_canvas_set_pixels_per_unit_xy(window->canvas, 1.0, zMapWindowGetZoomFactor(window)) ;
-	zmapWindowScaleCanvasSetPixelsPerUnit(window->ruler, 1.0, zMapWindowGetZoomFactor(window)) ;
-  }
+    {
+      /* MH17: draw features does not change the zoom factor, so we only set if first time round
+       * re-using scroll intialised flag but that should be safe
+       */
+      zmapWindowSetPixelxy(window, 1.0, zMapWindowGetZoomFactor(window)) ;
+
+      zmapWindowScaleCanvasSetPixelsPerUnit(window->ruler, 1.0, zMapWindowGetZoomFactor(window)) ;
+    }
 
 #if 0
   {
