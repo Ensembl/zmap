965f902a305ff154f3a3051785dd362efca35c60
Ed Griffiths <edgrif@sanger.ac.uk>
2013-10-04 10:42:36 +0100

    "fix memory leak in quark str func., add func to remove quarks from list. Tidy up."

--- src/include/ZMap/zmapGLibUtils.h
+++ src/include/ZMap/zmapGLibUtils.h
@@ -34,15 +34,8 @@
 
 
 
-
-/*! @addtogroup zmapGLibutils
- * @{
- *  */
-
-/*!
- * Represents a quark set. */
+/* Represents a quark set. */
 typedef struct _ZMapQuarkSetStruct *ZMapQuarkSet ;
-
 
 
 typedef enum
@@ -56,36 +49,46 @@ typedef gboolean (*ZMapGFuncCond)(gpointer data, gpointer user_data) ;
 
 
 
-/*! @} end of zmapGLibutils docs. */
+typedef struct ZMapGHashIterStructType
+{
+  gpointer key;
+  gpointer value;
+} ZMapGHashIterStruct, *ZMapGHashIter;
+
+
+
 
 
 
 gchar *zMap_g_remove_char(char *string, char ch) ;
 gchar *zMap_g_ascii_strstrcasecmp(const gchar *haystack, const gchar *needle) ;
+gboolean zMap_g_string_replace(GString *string, char *target, char *source) ;
 
 void zMap_g_list_foreach_reverse(GList *list, GFunc func, gpointer user_data);
 void zMap_g_list_foreach_directional(GList *list, GFunc func, gpointer user_data,
                                      ZMapGListDirection forward);
 gboolean zMap_g_list_cond_foreach(GList *list, ZMapGFuncCond func, gpointer user_data) ;
 GList *zMap_g_list_move(GList *list, gpointer user_data, gint new_index) ;
-void zMap_g_list_quark_print(GList *quark_list, char *list_name, gboolean new_line) ;
-GList *zMap_g_list_find_quark(GList *list, GQuark str_quark) ;
 GList *zMap_g_list_grep(GList **list_inout, gpointer data, GCompareFunc func);
-GList *zMap_g_list_insert_list_after(GList *recipient, GList *donor, int point);
+GList *zMap_g_list_insert_list_after(GList *recipient, GList *donor, int point, gboolean copy_donor) ;
 GList *zMap_g_list_lower(GList *move, int positions);
 GList *zMap_g_list_raise(GList *move, int positions);
 GList *zMap_g_list_split(GList *list, GList *new_list_head) ;
-
 GList *zMap_g_list_append_unique(GList *list, gpointer data);
-
-gchar *zMap_g_list_quark_to_string(GList *l);
 GList *zMap_g_list_merge(GList *a, GList *b);
 
+void zMap_g_list_quark_print(GList *quark_list, char *list_name, gboolean new_line) ;
+GList *zMap_g_list_find_quark(GList *list, GQuark str_quark) ;
+gchar *zMap_g_list_quark_to_string(GList *l, char *delimiter);
+GList *zMap_g_list_remove_quarks(GList *quark_target_list, GList *quarks) ;
+
 gpointer zMap_g_hash_table_nth(GHashTable *hash_table, int nth) ;
-
 void zMap_g_hash_table_print(GHashTable *hash_table, char *data_format_str) ;
-
-gboolean zMap_g_string_replace(GString *string, char *target, char *source) ;
+void zMap_g_hash_table_iter_init(GList **iter, GHashTable *h);
+gboolean zMap_g_hash_table_iter_next(GList **iter,gpointer *key, gpointer *value);
+void zMap_g_hash_table_iter_free(GList **list);       // not implemented
+void  zMap_g_hash_table_get_keys(GList **iter, GHashTable *h);
+void  zMap_g_hash_table_get_data(GList **iter, GHashTable *h);
 
 GHashTable *zMap_g_hashlist_create(void) ;
 void zMap_g_hashlist_insert(GHashTable *hashlist, GQuark key, gpointer value) ;
@@ -95,28 +98,13 @@ void zMap_g_hashlist_merge(GHashTable *in_out, GHashTable *in) ;
 void zMap_g_hashlist_print(GHashTable *hashlist) ;
 void zMap_g_hashlist_destroy(GHashTable *hashlist) ;
 
+gpointer zMap_g_datalist_first(GData **datalist);
+gint zMap_g_datalist_length(GData **datalist);
 
-typedef struct
-{
-      gpointer key;
-      gpointer value;
-} ZMapGHashIterStruct, *ZMapGHashIter;
-
-void zMap_g_hash_table_iter_init(GList **iter, GHashTable *h);
-gboolean zMap_g_hash_table_iter_next(GList **iter,gpointer *key, gpointer *value);
-void zMap_g_hash_table_iter_free(GList **list);       // not implemented
-void  zMap_g_hash_table_get_keys(GList **iter, GHashTable *h);
-void  zMap_g_hash_table_get_data(GList **iter, GHashTable *h);
 
 /* Returns a pointer to an element of the array instead of the element itself. */
 #define zMap_g_array_index_ptr(a, t, i)      (&(((t*) (a)->data) [(i)]))
-
-
-
-GArray *zMap_g_array_element(GArray           *array,
-			     guint             index,
-			     gpointer *element
-			     );
+GArray *zMap_g_array_element(GArray *array, guint index, gpointer *element) ;
 
 
 /* This is our version of the Glib Quark code (see www.gtk.org for more information).
@@ -128,8 +116,5 @@ GQuark zMap_g_quark_try_string(ZMapQuarkSet quark_set, gchar *string);
 GQuark zMap_g_quark_from_string(ZMapQuarkSet quark_set, gchar *string);
 gchar *zMap_g_quark_to_string(ZMapQuarkSet quark_set, GQuark quark) ;
 void zMap_g_quark_destroy_set(ZMapQuarkSet quark_set) ;
-
-gpointer zMap_g_datalist_first(GData **datalist);
-gint zMap_g_datalist_length(GData **datalist);
 
 #endif /* !ZMAP_GLIBUTILS_H */
