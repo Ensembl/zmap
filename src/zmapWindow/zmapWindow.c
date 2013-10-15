Ensembl/zmap

commit a05cf86cf652bdc5a9baceff3258e4460d2a3175

"add call to tell layer above us we have finished displaying/loading features."

src/zmapWindow/zmapWindow.c
@@ -214,7 +214,7 @@ static gboolean windowGeneralEventCB(GtkWidget *wigdet, GdkEvent *event, gpointe
 
 static void resetCanvas(ZMapWindow window, gboolean free_child_windows, gboolean keep_revcomp_safe_windows) ;
 static gboolean getConfiguration(ZMapWindow window) ;
-static void sendClientEvent(ZMapWindow window, FeatureSetsState feature_sets) ;
+static void sendClientEvent(ZMapWindow window, FeatureSetsState feature_sets, gpointer loaded_cb_user_data) ;
 
 static void moveWindow(ZMapWindow window, GdkEventKey *key_event) ;
 static void scrollWindow(ZMapWindow window, GdkEventKey *key_event) ;
@@ -331,6 +331,8 @@ static void printWindowSizeDebug(char *prefix, ZMapWindow window,
  * because the callback routines are set just once for the lifetime of the
  * process. */
 static ZMapWindowCallbacks window_cbs_G = NULL ;
+
+
 static gboolean window_rev_comp_save_state_G = TRUE;
 static gboolean window_rev_comp_save_bumped_G = TRUE;
 static gboolean window_rev_comp_save_zoom_G = TRUE;
@@ -512,7 +514,7 @@ ZMapWindow zMapWindowCopy(GtkWidget *parent_widget, ZMapFeatureSequenceMap seque
     /* should we be passing in a copy of the full set of original styles ? */
     zMapWindowDisplayData(new_window, state, feature_context, feature_context,
 			  original_window->context_map,
-			  NULL, NULL) ;
+			  NULL, NULL, NULL) ;
   }
 
   zmapWindowBusy(new_window, FALSE) ;
@@ -534,21 +536,6 @@ void zMapWindowBusyFull(ZMapWindow window, gboolean busy, const char *file, cons
 }
 
 
-
-#ifdef ED_G_NEVER_INCLUDE_THIS_CODE
-/* Tells zmapwindow that there is an external client that can be queried. */
-gboolean zMapWindowXRemoteRegister(ZMapWindow window)
-{
-  gboolean result = FALSE ;
-
-  result = window->xremote_client = TRUE ;
-
-  return result ;
-}
-#endif /* ED_G_NEVER_INCLUDE_THIS_CODE */
-
-
-
 /* This routine is called by the code in zmapView.c that manages the slave threads.
  * It has to determine whether or not the canvas has got far enough along in its
  * creation to have a valid height.  If so, it calls the code to notify the GUI
@@ -561,13 +548,17 @@ gboolean zMapWindowXRemoteRegister(ZMapWindow window)
  *
  * Rob
  *
- *    No, that's not the problem, it is realised, it just hasn't got sized properly yet. Ed
+ *    Yes...if you read the docs then you'll know that realised != sized.
+ *    It's the map-event signal that is needed....though whether that's really what's wanted 
+ *    I'm not sure....better revisit this.... Ed
+ * 
  *
  *  */
 void zMapWindowDisplayData(ZMapWindow window, ZMapWindowState state,
 			   ZMapFeatureContext current_features, ZMapFeatureContext new_features,
 			   ZMapFeatureContextMap context_map,
-			   GList *masked, ZMapFeature highlight_feature)
+			   GList *masked, ZMapFeature highlight_feature,
+			   gpointer loaded_cb_user_data)
 {
   FeatureSetsState feature_sets ;
 
@@ -592,7 +583,7 @@ void zMapWindowDisplayData(ZMapWindow window, ZMapWindowState state,
    * again in zmapWindowDrawFeatures(). */
   if (GTK_WIDGET(window->canvas)->allocation.height > 1 && GTK_WIDGET(window->canvas)->window)
     {
-      sendClientEvent(window, feature_sets) ;
+      sendClientEvent(window, feature_sets, loaded_cb_user_data) ;
     }
   else if (!window->exposeHandlerCB)
     {
@@ -728,11 +719,16 @@ void zMapWindowUnDisplayData(ZMapWindow window,
 
 void zMapWindowRemoveFeatureset(ZMapWindow window, ZMapFeatureSet featureset)
 {
+#warning: "what's going on here, this function takes a feature, not a feature set..."
+
   FooCanvasItem *item = zmapWindowFToIFindFeatureItem(window, window->context_to_item,
-                                                              ZMAPSTRAND_NONE, ZMAPFRAME_NONE, featureset) ;
+						      ZMAPSTRAND_NONE, ZMAPFRAME_NONE, featureset) ;
+
 
   if (item)
     zMapWindowFeaturesetItemRemoveSet(item, featureset, TRUE) ;
+
+  return ;
 }
 
 
@@ -914,7 +910,7 @@ void zMapWindowFeatureRedraw(ZMapWindow window, ZMapFeatureContext feature_conte
    * realised by the time we draw into it. */
   zMapWindowDisplayData(window, window->state, feature_context, feature_context,
 			window->context_map,
-			NULL, NULL) ;
+			NULL, NULL, NULL) ;
 
   window->state = NULL;	/* see comment in zmapWindowFeatureReset() above */
 
@@ -2925,7 +2921,7 @@ static gboolean exposeHandlerCB(GtkWidget *widget, GdkEventExpose *expose, gpoin
    * sequence as the previous one, will trigger another call to this function.  */
   g_signal_handler_disconnect(G_OBJECT(widget), realiseData->window->exposeHandlerCB);
 
-  sendClientEvent(realiseData->window, realiseData->feature_sets) ;
+  sendClientEvent(realiseData->window, realiseData->feature_sets, NULL) ;
 
   if(realiseData->window->curr_locking == ZMAP_WINLOCK_VERTICAL)
     {
@@ -2946,7 +2942,7 @@ static gboolean exposeHandlerCB(GtkWidget *widget, GdkEventExpose *expose, gpoin
 
 /* This routine sends a synthesized event to alert the GUI that it needs to
  * do some work and supplies the data for the GUI to process via the event struct. */
-static void sendClientEvent(ZMapWindow window, FeatureSetsState feature_sets)
+static void sendClientEvent(ZMapWindow window, FeatureSetsState feature_sets, gpointer loaded_cb_user_data)
 {
   GdkEventClient event ;
   zmapWindowData window_data ;
@@ -2956,6 +2952,7 @@ static void sendClientEvent(ZMapWindow window, FeatureSetsState feature_sets)
   window_data = g_new0(zmapWindowDataStruct, 1) ;
   window_data->window = window ;
   window_data->data = feature_sets ;
+  window_data->loaded_cb_user_data = loaded_cb_user_data ;
 
   event.type         = GDK_CLIENT_EVENT ;
   event.window       = window->toplevel->window ;  /* no window generates this event. */
@@ -2990,9 +2987,18 @@ static gboolean dataEventCB(GtkWidget *widget, GdkEventClient *event, gpointer c
   int i ;
 
   if (event->type != GDK_CLIENT_EVENT)
-    zMapLogFatal("%s", "dataEventCB() received non-GdkEventClient event") ;
-
-  if (event->send_event == TRUE && event->message_type == gdk_atom_intern(ZMAP_ATOM, TRUE))
+    {
+      zMapLogCritical("%s", "received non-GdkEventClient event in dataEventCB() handler") ;
+    }
+  else if (event->send_event != TRUE)
+    {
+      zMapLogCritical("%s", "received event not sent by \"client\" in dataEventCB() handler") ;
+    }
+  else if (event->message_type != gdk_atom_intern(ZMAP_ATOM, TRUE))
+    {
+      zMapLogCritical("%s", "unknown client event in dataEventCB() handler") ;
+    }
+  else
     {
       zmapWindowData window_data = NULL ;
       ZMapWindow window = NULL ;
@@ -3024,11 +3030,7 @@ static gboolean dataEventCB(GtkWidget *widget, GdkEventClient *event, gpointer c
 	diff_context = feature_sets->current_features ;
 
       /* Draw the features on the canvas */
-
       zmapWindowDrawFeatures(window, feature_sets->current_features, diff_context, feature_sets->masked) ;
-
-      (*(window_cbs_G->drawn_data))(window, window->app_data, diff_context);
-
 
       if (feature_sets->state != NULL)
 	{
@@ -3102,23 +3104,19 @@ static gboolean dataEventCB(GtkWidget *widget, GdkEventClient *event, gpointer c
       g_free(feature_sets) ;
       g_free(window_data) ;				    /* Free the WindowData struct. */
 
-      event_handled = TRUE ;
 
-
-
-      /* TRY FEATURE HIGHLIGHT HERE............. */
+      /* If features were already displayed and one was highlighted then rehighlight it. */
       if (highlight_feature)
 	zMapWindowFeatureSelect(window, highlight_feature) ;
 
-
+      /* Tell layer above that we have finished loading/displaying features. */
+      (*(window_cbs_G->drawn_data))(window,
+				    window->app_data, window_data->loaded_cb_user_data, diff_context) ;
 
       zmapWindowBusy(window, FALSE) ;
-    }
-  else
-    {
-      zMapLogCritical("%s", "unknown client event in dataEventCB() handler") ;
 
-      event_handled = FALSE ;
+
+      event_handled = TRUE ;
     }
 
   return event_handled ;

commit 698dd2c77ebe42a6770517b924b078e98c8dea6d

Fix a crash in dataEventCB
Regression introduced in a05cf86

src/zmapWindow/zmapWindow.c
@@ -3125,10 +3125,6 @@ static gboolean dataEventCB(GtkWidget *widget, GdkEventClient *event, gpointer c
       else
 //	zMapLogMessage("%s", "event handler for canvas already registered.");
 
-      g_free(feature_sets) ;
-      g_free(window_data) ;				    /* Free the WindowData struct. */
-
-
       /* If features were already displayed and one was highlighted then rehighlight it. */
       if (highlight_feature)
 	zMapWindowFeatureSelect(window, highlight_feature) ;
@@ -3136,6 +3132,10 @@ static gboolean dataEventCB(GtkWidget *widget, GdkEventClient *event, gpointer c
       /* Tell layer above that we have finished loading/displaying features. */
       (*(window_cbs_G->drawn_data))(window,
 				    window->app_data, window_data->loaded_cb_user_data, diff_context) ;
+
+
+      g_free(feature_sets) ;
+      g_free(window_data) ;				    /* Free the WindowData struct. */
 
       zmapWindowBusy(window, FALSE) ;
 