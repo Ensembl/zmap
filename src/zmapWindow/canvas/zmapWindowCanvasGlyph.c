33bc38c784eb9fa91ca2a875dac7717a1d4e06f3
Jeremy Henty <jh13@sanger.ac.uk>
2014-02-14 16:26:59 +0000

    Fix search-replace-mangled GPL licenses.

--- src/zmapWindow/canvas/zmapWindowCanvasGlyph.c
+++ src/zmapWindow/canvas/zmapWindowCanvasGlyph.c
@@ -2,12 +2,12 @@
  *  Author: malcolm hinsley (mh17@sanger.ac.uk)
  *  Copyright (c) 2006-2014: Genome Research Ltd.
  *-------------------------------------------------------------------
- * ZMap is free software; you can refeaturesetstribute it and/or
- * mofeaturesetfy it under the terms of the GNU General Public License
+ * ZMap is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * This program is featuresetstributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
