0a910aeb8ff470ae7614e20026fcae095ef1bdae
Steve Miller <sm23@sanger.ac.uk>
2014-02-14 16:35:45 +0000

    update to drawing for truncation of features

--- src/zmapWindow/canvas/zmapWindowCanvasBasic.c
+++ src/zmapWindow/canvas/zmapWindowCanvasBasic.c
@@ -35,12 +35,107 @@
 #include <string.h>
 
 #include <ZMap/zmapFeature.h>
+#include <ZMap/zmapStyle.h>
 #include <zmapWindowCanvasDraw.h>
 #include <zmapWindowCanvasFeatureset_I.h>
+#include <zmapWindowCanvasGlyph_I.h>
+
+/*
+ * A glyph to represent the trunction of a feature at the ZMap boundary. This one just draws a
+ * square with the diagonal oriented in the direction of the feature.
+ */
+static ZMapStyleGlyphShapeStruct truncation_shape_instance01 =
+{
+  {
+    5, 0,      0, 5,        -5, 0,       0, -5,      5, 0,          0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  },                                                                               /* length 32 coordinate array */
+  5,                                                                               /* number of coordinates */
+  10, 10,                                                                          /* width and height */
+  0,                                                                               /* quark ID */
+  GLYPH_DRAW_LINES                                                                 /* ZMapStyleGlyphDrawType */
+}  ;
+
+static ZMapStyleGlyphShapeStruct * truncation_shape01 = &truncation_shape_instance01 ;
+static ZMapWindowCanvasGlyph truncation_glyph = NULL ;
+static const int truncation_glyph_type = 999 ;
 
 
+
+/* draw a box */
 static void basicPaintFeature(ZMapWindowFeaturesetItem featureset, ZMapWindowCanvasFeature feature,
-                              GdkDrawable *drawable, GdkEventExpose *expose) ;
+                              GdkDrawable *drawable, GdkEventExpose *expose)
+{
+  gulong fill,outline ;
+  int colours_set = 0, fill_set = 0, outline_set = 0;
+  double x1 = 0.0, x2 = 0.0, col_width = 0.0 ;
+  gboolean truncated_start = FALSE,
+    truncated_end = FALSE ;
+  zMapReturnIfFail(featureset && feature && drawable && expose ) ;
+  ZMapFeatureTypeStyle style = *feature->feature->style;
+  FooCanvasItem *foo = (FooCanvasItem *) featureset ;
+
+  /* colours are not defined for the CanvasFeatureSet
+   * as we can have several styles in a column
+   * but they are cached by the calling function
+   * and also the window focus code
+   */
+  colours_set = zMapWindowCanvasFeaturesetGetColours(featureset, feature, &fill, &outline) ;
+  fill_set = colours_set & WINDOW_FOCUS_CACHE_FILL ;
+  outline_set = colours_set & WINDOW_FOCUS_CACHE_OUTLINE ;
+
+  if (fill_set && feature->feature->population)
+    {
+
+      if((zMapStyleGetScoreMode(style) == ZMAPSCORE_HEAT) || (zMapStyleGetScoreMode(style) == ZMAPSCORE_HEAT_WIDTH))
+        {
+          fill = (fill << 8) | 0xff;	/* convert back to RGBA */
+          fill = foo_canvas_get_color_pixel(foo->canvas,
+                                            zMapWindowCanvasFeatureGetHeatColour(0xffffffff,fill,feature->score));
+        }
+    }
+
+  /*
+   * Decide about truncation.
+   */
+
+  /*
+   * Draw the basic box.
+   */
+  if (zMapWindowCanvasCalcHorizCoords(featureset, feature, &x1, &x2))
+    {
+
+      zMapCanvasFeaturesetDrawBoxMacro(featureset, x1, x2, feature->y1, feature->y2, drawable,
+                                       fill_set, outline_set, fill, outline) ;
+
+    }
+
+  /*
+   * Construct glyph object from shape given.
+   */
+  if (truncation_glyph == NULL)
+    {
+      truncation_glyph = g_new0(zmapWindowCanvasGlyphStruct, 1) ;
+      truncation_glyph->which = truncation_glyph_type ;
+      truncation_glyph->sub_feature = TRUE ;
+      truncation_glyph->shape = truncation_shape01 ;
+    }
+  col_width = zMapStyleGetWidth(featureset->style) ;
+  zmap_window_canvas_set_glyph(foo, truncation_glyph, style, feature->feature, col_width, feature->score ) ;
+
+  /*
+   * Draw the glyph subfeatures.
+   */
+  zMapWindowCanvasGlyphPaintSubFeature(featureset, feature, truncation_glyph, drawable) ;
+
+  return ;
+}
+
+
+
+
+
+
 
 
 
@@ -54,44 +149,3 @@ void zMapWindowCanvasBasicInit(void)
 
   return ;
 }
-
-
-/* draw a box */
-static void basicPaintFeature(ZMapWindowFeaturesetItem featureset, ZMapWindowCanvasFeature feature,
-                              GdkDrawable *drawable, GdkEventExpose *expose)
-{
-  gulong fill,outline ;
-  int colours_set, fill_set, outline_set ;
-  double x1,x2 ;
-
-  /* colours are not defined for the CanvasFeatureSet
-   * as we can have several styles in a column
-   * but they are cached by the calling function
-   * and also the window focus code
-   */
-  colours_set = zMapWindowCanvasFeaturesetGetColours(featureset, feature, &fill, &outline) ;
-  fill_set = colours_set & WINDOW_FOCUS_CACHE_FILL ;
-  outline_set = colours_set & WINDOW_FOCUS_CACHE_OUTLINE ;
-
-  if (fill_set && feature->feature->population)
-    {
-      FooCanvasItem *foo = (FooCanvasItem *) featureset ;
-      ZMapFeatureTypeStyle style = *feature->feature->style;
-
-      if((zMapStyleGetScoreMode(style) == ZMAPSCORE_HEAT) || (zMapStyleGetScoreMode(style) == ZMAPSCORE_HEAT_WIDTH))
-        {
-          fill = (fill << 8) | 0xff;	/* convert back to RGBA */
-          fill = foo_canvas_get_color_pixel(foo->canvas,
-                                            zMapWindowCanvasFeatureGetHeatColour(0xffffffff,fill,feature->score));
-        }
-    }
-
-  if (zMapWindowCanvasCalcHorizCoords(featureset, feature, &x1, &x2))
-    zMapCanvasFeaturesetDrawBoxMacro(featureset, x1, x2, feature->y1, feature->y2, drawable,
-                                     fill_set, outline_set, fill, outline) ;
-
-  return ;
-}
-
-
-
