5e046bdbbf721df4f8ad866ba96779c794f3201c
Gemma Barson <gb10@sanger.ac.uk>
2014-03-03 17:10:45 +0000

    Remove requirement to have sequence details when creating parser

--- src/zmapGFF/zmapGFFGeneral.c
+++ src/zmapGFF/zmapGFFGeneral.c
@@ -85,7 +85,6 @@ static gboolean removeTranscriptFeature(gpointer key,gpointer value, gpointer us
 ZMapGFFParser zMapGFFCreateParser(int iGFFVersion, char *sequence, int features_start, int features_end)
 {
   ZMapGFFParser pParser = NULL ;
-  zMapReturnValIfFail(sequence && *sequence && features_start && (features_start <= features_end), pParser) ;
 
   if (iGFFVersion == ZMAPGFF_VERSION_2 )
     {
