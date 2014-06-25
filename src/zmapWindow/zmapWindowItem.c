Ensembl/zmap

commit 568ff7de672fe96c6f507f520a441f7a3b15a57a

Fix compiler warnings

src/zmapWindow/zmapWindowItem.c
@@ -429,7 +429,7 @@ ZMapFeatureAny zmapWindowItemGetFeatureAnyType(FooCanvasItem *item, ZMapFeatureL
       zMapLogMessage("Unexpected item [%s]", (item ? G_OBJECT_TYPE_NAME(item) : "null")) ;
     }
 
-  if (feature_any && expected_type != -1)
+  if (feature_any && expected_type != ZMAPFEATURE_STRUCT_INVALID)
     {
       if (feature_any->struct_type != expected_type)
         {

src/zmapApp/zmapAppRemoteControl.c
@@ -456,7 +456,7 @@ static void handleZMapRequestsCB(gpointer caller_data,
   if (remote)
     {
       gboolean result = TRUE ;
-      GArray *request_stack ;
+      GArray *request_stack = NULL ;
       char *request ;
       char *err_msg = NULL ;
       char *view = NULL ;    /* to be filled in later..... */

src/zmapView/zmapViewRemoteControl.c
@@ -1804,12 +1804,14 @@ static gboolean xml_feature_start_cb(gpointer user_data, ZMapXMLElement feature_
 	  if ((attr = zMapXMLElementGetAttributeByName(feature_element, "start_not_found")))
 	    {
 	      /* I don't understand what this ghastliness is....how can phase be -ve ?? */
+              /* gb10: perhaps the strand and phase are mixed up together in this attribute?? It
+               * looks like the sign is ignored, anyway */
+              long int start_phase_int = strtol((char *)g_quark_to_string(zMapXMLAttributeGetValue(attr)),
+                                                (char **)NULL, 10);
 
-	      start_phase = strtol((char *)g_quark_to_string(zMapXMLAttributeGetValue(attr)),
-				   (char **)NULL, 10);
 	      start_not_found = TRUE;
 
-	      switch (start_phase)
+              switch (start_phase_int)
 		{
 		case 1:
 		case -1:

src/zmapWindow/canvas/zmapWindowCanvasFeatureset.c
@@ -463,7 +463,7 @@ gboolean zMapWindowCanvasFeaturesetGetFeatureExtent(ZMapWindowCanvasFeature feat
 
   void (*func) (ZMapWindowCanvasFeature feature, ZMapSpan span, double *width) = NULL;
 
-  if(!feature || feature->type < 0 || feature->type >= FEATURE_N_TYPE)
+  if(!feature || feature->type == FEATURE_INVALID || feature->type >= FEATURE_N_TYPE)
     return FALSE;
 
   func = _featureset_extent_G[feature->type];

src/zmapWindow/zmapWindowSeqDisp.c
@@ -209,7 +209,7 @@ void zmapWindowItemHighlightTranslationRegions(ZMapWindow window, gboolean item_
                                                ZMapSequenceType coords_type,
                                                int region_start, int region_end, int flanking)
 {
-  ZMapFrame frame ;
+  int frame ;
 
   for (frame = ZMAPFRAME_0 ; frame < ZMAPFRAME_2 + 1 ; frame++)
     {
@@ -232,7 +232,7 @@ void zmapWindowItemHighlightTranslationRegions(ZMapWindow window, gboolean item_
 
 void zmapWindowItemUnHighlightTranslations(ZMapWindow window, FooCanvasItem *item)
 {
-  ZMapFrame frame ;
+  int frame ;
 
   for (frame = ZMAPFRAME_0 ; frame < ZMAPFRAME_2 + 1 ; frame++)
     {

commit 6498d989b59a03024e5bde40b39bfa7da0d94981

Fix regression in looking up features caused by 568ff7d

src/zmapWindow/zmapWindowItem.c
@@ -407,7 +407,7 @@ ZMapFeatureAny zmapWindowItemGetFeatureAny(FooCanvasItem *item)
 }
 
 
-ZMapFeatureAny zmapWindowItemGetFeatureAnyType(FooCanvasItem *item, ZMapFeatureLevelType expected_type)
+ZMapFeatureAny zmapWindowItemGetFeatureAnyType(FooCanvasItem *item, const int expected_type)
 {
   ZMapFeatureAny feature_any = NULL ;
   ZMapWindowCanvasItem feature_item ;
@@ -429,7 +429,7 @@ ZMapFeatureAny zmapWindowItemGetFeatureAnyType(FooCanvasItem *item, ZMapFeatureL
       zMapLogMessage("Unexpected item [%s]", (item ? G_OBJECT_TYPE_NAME(item) : "null")) ;
     }
 
-  if (feature_any && expected_type != ZMAPFEATURE_STRUCT_INVALID)
+  if (feature_any && expected_type != -1)
     {
       if (feature_any->struct_type != expected_type)
         {

src/zmapWindow/zmapWindow_P.h
@@ -1161,7 +1161,7 @@ void zmapWindowCallBlixem(ZMapWindow window, FooCanvasItem *item,
 ZMapFeatureBlock zmapWindowItemGetFeatureBlock(FooCanvasItem *item) ;
 ZMapFeature zmapWindowItemGetFeature(FooCanvasItem *item) ;
 ZMapFeatureAny zmapWindowItemGetFeatureAny(FooCanvasItem *item) ;
-ZMapFeatureAny zmapWindowItemGetFeatureAnyType(FooCanvasItem *item, ZMapFeatureLevelType expected_type) ;
+ZMapFeatureAny zmapWindowItemGetFeatureAnyType(FooCanvasItem *item, const int expected_type) ;
 
 FooCanvasItem *zmapWindowItemGetShowTranslationColumn(ZMapWindow window, FooCanvasItem *item) ;
 void zmapWindowItemShowTranslation(ZMapWindow window, FooCanvasItem *feature_to_translate) ;