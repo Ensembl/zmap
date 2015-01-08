315c02670e5b1be45fe628108c03df3639895d2d
Jeremy Henty <jh13@sanger.ac.uk>
2015-01-08 11:09:42 +0000

    Update the copyright declarations to the year 2015.

--- src/zmapServer/pipe/pipeServer_P.h
+++ src/zmapServer/pipe/pipeServer_P.h
@@ -1,6 +1,6 @@
 /*  File: pipeServer_P.h
  *  Author: Malcolm Hinsley (mh17@sanger.ac.uk), Ed Griffiths (edgrif@sanger.ac.uk)
- *  Copyright (c) 2006-2014: Genome Research Ltd.
+ *  Copyright (c) 2006-2015: Genome Research Ltd.
  *-------------------------------------------------------------------
  * ZMap is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
