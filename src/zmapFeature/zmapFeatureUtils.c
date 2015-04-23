Ensembl/zmap

commit cf575da43ed3b6c730b78a4a6dd9c2a0567a7f3b

Fix compiler warnings - BUILD

src/zmapFeature/zmapFeatureUtils.c
@@ -1498,12 +1498,8 @@ ZMapPhase zMapFeaturePhase(ZMapFeature feature)
 char *zMapFeatureTranscriptTranslation(ZMapFeature feature, int *length, gboolean pad)
 {
   char *pep_str = NULL ;
-  ZMapFeatureContext context ;
   ZMapPeptide peptide ;
   char *dna_str, *name, *free_me ;
-
-  context = (ZMapFeatureContext)(zMapFeatureGetParentGroup((ZMapFeatureAny)feature,
-   ZMAPFEATURE_STRUCT_CONTEXT));
 
   if ((dna_str = zMapFeatureGetTranscriptDNA(feature, TRUE, feature->feature.transcript.flags.cds)))
     {
@@ -1800,7 +1796,7 @@ static ZMapFullExon feature_find_closest_exon_at_coord(ZMapFeature feature, int 
   zMapReturnValIfFail(feature && feature->mode == ZMAPSTYLE_MODE_SEQUENCE, result) ;
 
   gboolean first_time = TRUE;
-  gboolean found = FALSE;
+  /*gboolean found = FALSE;*/
   int closest_dist = 0;
   GList *exon_item = feature->feature.sequence.exon_list;
 
@@ -1821,7 +1817,7 @@ static ZMapFullExon feature_find_closest_exon_at_coord(ZMapFeature feature, int 
               /* Inside the exon - done */
               //zMapDebugPrintf("Coord %d found in exon %d,%d", y, x1, x2) ;
               result = current_exon ;
-              found = TRUE ;
+              /*found = TRUE ;*/
               break ;
             }
           else if (y < x1)

src/zmapApp/zmapAppManager.c
@@ -583,7 +583,6 @@ static void destroyedCB(ZMap zmap, ZMapView view, void *cb_data)
 static void quitReqCB(ZMap zmap, void *cb_data)
 {
   ZMapAppContext app_context = (ZMapAppContext)cb_data ;
-  ZMapManager zmaps = app_context->zmap_manager ;
 
   zmapAppQuitReq(app_context) ;
 
@@ -781,7 +780,6 @@ static ZMapManagerAddResult addNewView(ZMapManager zmaps,
   ZMapManagerAddResult result = ZMAPMANAGER_ADD_FAIL ;
   ZMap zmap ;
   ZMapView view = NULL ;
-  ZMapWindow window = NULL ;
   ZMapAppContext app_context = (ZMapAppContext)(zmaps->gui_data) ;
 
 
@@ -829,8 +827,6 @@ static ZMapManagerAddResult addNewView(ZMapManager zmaps,
                       && (view = zMapViewGetView(view_window))
               && zMapConnectView(zmap, view, NULL))
             {
-              window = zMapViewGetWindow(view_window) ;
-
               result = ZMAPMANAGER_ADD_OK ;
             }
           else

src/zmapApp/zmapAppmanagepanel.c
@@ -266,11 +266,10 @@ static void viewOnDoubleClickCB(GtkTreeView        *treeview,
                                 gpointer            userdata)
 {
   ZMapAppContext app_context = (ZMapAppContext)userdata ;
-  GtkTreeModel *model;
 
   /* probably will need to change later, but for now, seems
      reasonable. */
-  model = gtk_tree_view_get_model(treeview);
+  gtk_tree_view_get_model(treeview);
 
   zMapManagerRaise(app_context->selected_zmap);
   

src/zmapApp/zmapAppwindow.c
@@ -343,9 +343,9 @@ int zmapMainMakeAppWindow(int argc, char *argv[])
   {
     /* Check locale setting, vital for message handling, text parsing and much else. */
     char *default_locale = "POSIX";
-    char *locale_in_use, *user_req_locale, *new_locale;
+    char *user_req_locale, *new_locale;
 
-    locale_in_use = setlocale(LC_ALL, NULL);
+    setlocale(LC_ALL, NULL);
 
     if (!(user_req_locale = app_context->locale))
       user_req_locale = app_context->locale = g_strdup(default_locale);
@@ -588,13 +588,12 @@ void zmapAppExit(ZMapAppContext app_context)
 
 void zmapAppPingStart(ZMapAppContext app_context)
 {
-  guint timeout_func_id ;
   int interval = app_context->ping_timeout * 1000 ;    /* glib needs time in milliseconds. */
 
   app_context->stop_pinging = FALSE ;
 
   /* try setting a timer to ping periodically.... */
-  timeout_func_id = g_timeout_add(interval, pingHandler, (gpointer)app_context) ;
+  g_timeout_add(interval, pingHandler, (gpointer)app_context) ;
 
   return ;
 }
@@ -809,7 +808,6 @@ static void topLevelDestroy(ZMapAppContext app_context)
 /* Signal all views to die (including their widgets). */
 static void killZMaps(ZMapAppContext app_context)
 {
-  guint timeout_func_id ;
   int interval = app_context->exit_timeout * 1000 ;    /* glib needs time in milliseconds. */
 
   consoleLogMsg(TRUE, EXIT_FORMAT, "Issuing requests to all ZMaps to disconnect from servers and quit.") ;
@@ -820,7 +818,7 @@ static void killZMaps(ZMapAppContext app_context)
                      GTK_JUSTIFY_CENTER, 2, FALSE) ;
 
   /* time out func makes sure that we exit if threads fail to report back. */
-  timeout_func_id = g_timeout_add(interval, timeoutHandler, (gpointer)app_context) ;
+  g_timeout_add(interval, timeoutHandler, (gpointer)app_context) ;
 
   /* Tell all our zmaps to die, they will tell all their threads to die, once they
    * are all gone we will be called back to exit. */

src/zmapConfig/zmapConfigLoader.c
@@ -167,9 +167,9 @@ ZMapConfigIniContext zMapConfigIniContextProvide(char *config_file)
       if (context->error_message)
         err_msg = context->error_message ;
       else if (context->config && context->config->sys_key_error)
-        err_msg = context->config->sys_key_error ;
+        err_msg = context->config->sys_key_error->message ;
       else if (context->config && context->config->zmap_key_error)
-        err_msg = context->config->zmap_key_error ;
+        err_msg = context->config->zmap_key_error->message ;
 
       zMapCritical("Error reading config file: %s", err_msg) ;
     }
@@ -1708,10 +1708,6 @@ static gpointer create_config_style()
       {NULL}
     } ;
 
-
-  int dummy ;
-
-  dummy = sizeof(style_conf) ;
 
 
   /* Needs to return a block copy of this.... */

src/zmapControl/zmapControl.c
@@ -766,10 +766,8 @@ void zmapControlResetCB(ZMap zmap)
   if (zmap->state == ZMAP_VIEWS)
     {
       ZMapView curr_view ;
-      ZMapViewState view_state ;
 
       curr_view = zMapViewGetView(zmap->focus_viewwindow) ;
-      view_state = zMapViewGetStatus(curr_view) ;
 
       /* for now we are just doing the current view but this will need to change to allow a kind
        * of global load of all views if there is no current selected view, or perhaps be an error

src/zmapControl/zmapControlImportFile.c
@@ -281,6 +281,9 @@ static void importGetConfig(MainFrame main_frame, char *config_file)
               s->args = argp;
               s->allocd = args;*/
             }
+
+          if (freethis)
+            g_strfreev(freethis) ;
         }
     }
 

src/zmapControl/zmapControlPreferences.c
@@ -87,7 +87,6 @@ void zmapControlShowPreferences(ZMap zmap)
 {
   ZMapGuiNotebook note_book ;
   char *notebook_title ;
-  GtkWidget *notebook_dialog ;
   /* ZMapGuiNotebookChapter chapter ; */
 
   zMapReturnIfFailSafe(!(zmap->preferences_note_book)) ;
@@ -111,7 +110,7 @@ void zmapControlShowPreferences(ZMap zmap)
       zMapViewBlixemGetConfigChapter(zmap_view, note_book) ;
 
       /* Display the preferences. */
-      notebook_dialog = zMapGUINotebookCreateDialog(note_book, help_title_G, help_text_G) ;
+      zMapGUINotebookCreateDialog(note_book, help_title_G, help_text_G) ;
 
       zmap->preferences_note_book = note_book ;
     }
@@ -151,7 +150,6 @@ static ZMapGuiNotebookChapter addControlPrefsChapter(ZMapGuiNotebook note_book_p
   ZMapGuiNotebookPage page ;
   ZMapGuiNotebookSubsection subsection ;
   ZMapGuiNotebookParagraph paragraph ;
-  ZMapGuiNotebookTagValue tagvalue ;
 
   chapter = zMapGUINotebookCreateChapter(note_book_parent, CONTROL_CHAPTER, &user_CBs) ;
 
@@ -164,17 +162,17 @@ static ZMapGuiNotebookChapter addControlPrefsChapter(ZMapGuiNotebook note_book_p
 					     ZMAPGUI_NOTEBOOK_PARAGRAPH_TAGVALUE_TABLE,
 					     NULL, NULL) ;
 
-  tagvalue = zMapGUINotebookCreateTagValue(paragraph, CONTROL_SHRINKABLE, "Make the ZMap window shrinkable beyond the default minimum size",
-					   ZMAPGUI_NOTEBOOK_TAGVALUE_CHECKBOX,
-					   "bool", zmap->shrinkable) ;
+  zMapGUINotebookCreateTagValue(paragraph, CONTROL_SHRINKABLE, "Make the ZMap window shrinkable beyond the default minimum size",
+                                ZMAPGUI_NOTEBOOK_TAGVALUE_CHECKBOX,
+                                "bool", zmap->shrinkable) ;
 
-  tagvalue = zMapGUINotebookCreateTagValue(paragraph, CONTROL_FILTERED, "Highlight the background of columns that have features that are not visible due to filtering",
-                                           ZMAPGUI_NOTEBOOK_TAGVALUE_CHECKBOX,
-                                           "bool", zMapViewGetFlag(view, ZMAPFLAG_HIGHLIGHT_FILTERED_COLUMNS)) ;
+  zMapGUINotebookCreateTagValue(paragraph, CONTROL_FILTERED, "Highlight the background of columns that have features that are not visible due to filtering",
+                                ZMAPGUI_NOTEBOOK_TAGVALUE_CHECKBOX,
+                                "bool", zMapViewGetFlag(view, ZMAPFLAG_HIGHLIGHT_FILTERED_COLUMNS)) ;
 
-  tagvalue = zMapGUINotebookCreateTagValue(paragraph, CONTROL_ANNOTATION, "Enable feature editing within ZMap via the Annotation column. This enables the Annotation sub-menu when you right-click a feature.",
-                                           ZMAPGUI_NOTEBOOK_TAGVALUE_CHECKBOX,
-                                           "bool", zMapViewGetFlag(view, ZMAPFLAG_ENABLE_ANNOTATION)) ;
+  zMapGUINotebookCreateTagValue(paragraph, CONTROL_ANNOTATION, "Enable feature editing within ZMap via the Annotation column. This enables the Annotation sub-menu when you right-click a feature.",
+                                ZMAPGUI_NOTEBOOK_TAGVALUE_CHECKBOX,
+                                "bool", zMapViewGetFlag(view, ZMAPFLAG_ENABLE_ANNOTATION)) ;
 
   return chapter ;
 }

commit 0d9b4d7ab5fa9b7fd464d810209666cef9f02749

Fix bug with revcomp coords introduced in cf575da - REGRESSION FIX

src/zmapFeature/zmapFeatureUtils.c
@@ -2033,13 +2033,15 @@ static void printChildCB(gpointer key, gpointer value, gpointer user_data_unused
 }
 
 
-
+/* Invert the coordinates within the given range. Also converts them to 1-based coordinates. */
 void zmapFeatureInvert(int *coord, const int seq_start, const int seq_end)
 {
-  *coord = seq_end - *coord + seq_start ;
+  /* Subtract 1 here instead of seq_start in order to make the coord 1-based */
+  *coord = seq_end - *coord + 1 ;
 }
 
 
+/* Revcomp the feature coords (swops, inverts, and makes them 1-based) */
 void zmapFeatureRevComp(const int seq_start, const int seq_end, int *coord1, int *coord2)  
 { 
   int tmp = *coord1;