2352b1c699ecb49f4ac06511f93bce8aab7c734e
Steve Miller <sm23@sanger.ac.uk>
2015-06-19 11:41:53 +0100

    update to deal with transcript drawing bug - BUG FIX

--- src/zmapWindow/canvas/zmapWindowCanvasDraw.h
+++ src/zmapWindow/canvas/zmapWindowCanvasDraw.h
@@ -6,12 +6,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -75,6 +75,8 @@ gboolean zMapWindowCanvasCalcHorizCoords(ZMapWindowFeaturesetItem featureset, ZM
                                          double *x1_out, double *x2_out) ;
 int zMap_draw_line(GdkDrawable *drawable, ZMapWindowFeaturesetItem featureset,
                    gint cx1, gint cy1, gint cx2, gint cy2) ;
+void zMap_draw_line_hack(GdkDrawable* drawable, ZMapWindowFeaturesetItem featureset,
+                                gint x1, gint y1, gint x2, gint y2) ;
 int zMap_draw_broken_line(GdkDrawable *drawable, ZMapWindowFeaturesetItem featureset,
                           gint cx1, gint cy1, gint cx2, gint cy2) ;
 int zMap_draw_rect(GdkDrawable *drawable, ZMapWindowFeaturesetItem featureset,
