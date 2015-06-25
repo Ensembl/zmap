Ensembl/zmap

commit e01b11a3d7a246305bdda5fcef3c7b61812ae739

Avoid potential null access - TRIVIAL

src/zmapConfig/zmapConfigLoader.c
@@ -1324,7 +1324,10 @@ static void fetch_referenced_stanzas(gpointer list_data, gpointer user_data)
 
   full_data->current_stanza_name = stanza_name;
 
-  if (zMapConfigIniHasStanza(full_data->context->config, stanza_name,NULL) && (full_data->object_create_func))
+  if (full_data && 
+      zMapConfigIniHasStanza(full_data->context->config, stanza_name,NULL) &&
+      full_data->object_create_func &&
+      full_data->stanza)
     {
       if ((full_data->current_object = (full_data->object_create_func)()))
         {
@@ -1334,8 +1337,10 @@ static void fetch_referenced_stanzas(gpointer list_data, gpointer user_data)
           full_data->object_list_out = g_list_append(full_data->object_list_out, full_data->current_object);
         }
       else
-        zMapLogWarning("Object Create Function for stanza '%s'"
-               " failed to return anything", stanza_name);
+        {
+          zMapLogWarning("Object Create Function for stanza '%s'"
+                         " failed to return anything", stanza_name);
+        }
     }
 
   return ;

commit 56edb091dd039b4de437eb0e07109cc8ab193dff

Fix crash when importing files - REGRESSION FIX
Regression caused by e01b11a3

src/zmapConfig/zmapConfigLoader.c
@@ -123,11 +123,13 @@ static void stylesFreeList(GList *config_styles_list) ;
  */
 
 
+/* Note that config_file can be null here - this returns a new context which has  
+ * the config_read flag as false. */
 ZMapConfigIniContext zMapConfigIniContextProvide(char *config_file)
 {
   ZMapConfigIniContext context = zMapConfigIniContextCreate(config_file) ;
 
-  if(context && context->config_read)
+  if(context)
     {
       ZMapConfigIniContextKeyEntry stanza_group = NULL;
       char *stanza_name, *stanza_type;
@@ -1322,6 +1324,7 @@ static void fetch_referenced_stanzas(gpointer list_data, gpointer user_data)
 {
   FetchReferencedStanzas full_data = (FetchReferencedStanzas)user_data;
   char *stanza_name = (char *)list_data;
+  gboolean found = FALSE ;
 
   full_data->current_stanza_name = stanza_name;
 