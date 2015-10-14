42af70f8f566b976c884137e2ca9c51332cf842c
Ed Griffiths <edgrif@sanger.ac.uk>
2015-10-14 15:32:04 +0100

    swop from precise to trusty as there's a space issue on the precise machine. - BUILD

--- scripts/build_config.sh
+++ scripts/build_config.sh
@@ -243,7 +243,7 @@ ZMAP_CLUSTER_CONFIG_FILE=~zmap/cluster.config.sh
 
 # temporary until we've sorted out why the build fail on windows.....
 #ZMAP_BUILD_MACHINES="tviewsrv lucid-dev64 precise-dev64 mac106229i t119-win-build"
-ZMAP_BUILD_MACHINES="tviewsrv lucid-dev64 precise-dev64 mac106229i"
+ZMAP_BUILD_MACHINES="tviewsrv lucid-dev64 trusty-dev64 mac106229i"
 
 
 
